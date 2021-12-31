7519e65ea2fb9d80ad35688843ece906f62b8b69
kirkeasterson <kirk.easterson@gmail.com>
2021-12-31 12:51:07 +0100

    Changed ls -a to ls -^C to avoid having . and .. in the ign_files array

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@
 # TODO: use a map instead of an array
 # contents to ignore when making sym links
 ign_files=(
-		"."
-		".."
 		".git"
 		"README.md"
 )
@@ -15,7 +13,7 @@ ign_files=(
 ign_files+=(`basename "$0"`)
 
 # iterate through contents of current directory
-for entry in `ls -a`; do
+for entry in `ls -aA`; do
 
 		# if current entry is not in the ignore list
 		if ! [[ ${ign_files[*]} =~ (^|[[:space:]])"$entry"($|[[:space:]]) ]]; then
