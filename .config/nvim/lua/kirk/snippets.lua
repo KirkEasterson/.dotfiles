525cf1edef0004799767e818672e08f293d75c60
Kirk Easterson <kirk.easterson@gmail.com>
2022-03-16 14:29:03 +0100

    Added more go snippets

--- .config/nvim/lua/kirk/snippets.lua
+++ .config/nvim/lua/kirk/snippets.lua
@@ -90,13 +90,19 @@ ls.snippets = {
 	go = {
 		ls.parser.parse_snippet("mn", "package main\n\nfunc main() {\n\t$0\n}"),
 		ls.parser.parse_snippet("fn", "func $1($2) $3{\n\t$0\n}"),
+		ls.parser.parse_snippet("gfn", "go func() {\n\t$0\n}()"),
+		ls.parser.parse_snippet("test", "func $1(t *testing.T) {\n\twant := $2\n\tcontains := $3\n\n\tif want != contains {\n\t\tt.Fatalf(`$0`, contains, want)\n\t}\n}"),
 		ls.parser.parse_snippet("stru", "type $1 struct {\n\t$0\n}"),
 		ls.parser.parse_snippet("in", "type $1 interface {\n\t$0\n}"),
 		ls.parser.parse_snippet("err", "if err != nil {\n\t$0\n}"),
-		ls.parser.parse_snippet("for", "for i := 0; i < $1; i++ {\n\t$2\n}"),
+		ls.parser.parse_snippet("for", "for {\n\t$0\n}"),
+		ls.parser.parse_snippet("forw", "for $1 {\n\t$0\n}"),
+		ls.parser.parse_snippet("fori", "for i := 0; i < $1; i++ {\n\t$0\n}"),
+		ls.parser.parse_snippet("mu", "mu sync.Mutex"),
+		ls.parser.parse_snippet("ch", "$1 := make(chan $0)"),
 		ls.parser.parse_snippet("pr", "fmt.Println($0)"),
 		ls.parser.parse_snippet("prf", "fmt.Printf($1, $0)"),
-		ls.parser.parse_snippet("test", "func $1(t *testing.T) {\n\twant := $2\n\tcontains := $3\n\n\tif want != contains {\n\t\tt.Fatalf(`$0`, contains, want)\n\t}\n}"),
+		ls.parser.parse_snippet("wg", "var wg sync.WaitGroup"),
 	},
 
 	-- Lua specific snippets
