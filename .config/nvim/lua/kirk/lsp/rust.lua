64901ed95fa083a8d8ff41c2877d6cb6a1f2000a
Kirk Easterson <kirk.easterson@gmail.com>
2022-05-21 11:58:59 +0200

    Added lsp config for rust, but it needs work since I'm using both a plugin and an lsp config

---
+++ .config/nvim/lua/kirk/lsp/rust.lua
@@ -0,0 +1,25 @@
+-- local nvim_lsp = require'lspconfig'
+
+-- local on_attach = function(client)
+--     require'completion'.on_attach(client)
+-- end
+
+-- nvim_lsp.rust_analyzer.setup({
+--     on_attach=on_attach,
+--     settings = {
+--         ["rust-analyzer"] = {
+--             assist = {
+--                 importGranularity = "module",
+--                 importPrefix = "self",
+--             },
+--             cargo = {
+--                 loadOutDirsFromCheck = true
+--             },
+--             procMacro = {
+--                 enable = true
+--             },
+--         }
+--     }
+-- })
+
+require('rust-tools').setup({})
