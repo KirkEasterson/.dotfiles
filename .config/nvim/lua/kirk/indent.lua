f7743e79e19d669a2532f6b06761ee29bd8fd650
Kirk Easterson <kirk.easterson@gmail.com>
2022-05-29 22:42:12 +0200

    Fixed issue where indenting with treesitter wouldn't show the indent line

--- .config/nvim/lua/kirk/indent.lua
+++ .config/nvim/lua/kirk/indent.lua
@@ -5,9 +5,8 @@ vim.opt.listchars:append("space:⋅")
 
 require("indent_blankline").setup {
 	use_treesitter = true,
-    show_current_context = true,
-    show_current_context_start = true,
+	show_current_context = true,
+	show_current_context_start = true,
 	max_indent_increase = 1,
 	show_trailing_blankline_indent = false,
-	context_patterns = true,
 }
