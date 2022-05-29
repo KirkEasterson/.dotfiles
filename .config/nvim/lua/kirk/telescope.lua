282fc6ef359e19bf110c9d9bc9e61f5cd7d645b3
Kirk Easterson <kirk.easterson@gmail.com>
2022-05-29 23:15:50 +0200

    Added more config to telescope

--- .config/nvim/lua/kirk/telescope.lua
+++ .config/nvim/lua/kirk/telescope.lua
@@ -1,19 +1,25 @@
+local previewers = require("telescope.previewers")
+local sorters = require("telescope.sorters")
+
 local map = require("util").map
 
 
 map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
 map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
+map("n", "<leader>fG", "<cmd>lua require('telescope.builtin').grep_string()<CR>")
 map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>")
 map("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
 
 require('telescope').setup{
 	defaults = {
-		file_sorter = require("telescope.sorters").get_fzy_sorter,
+		file_sorter = sorters.get_fzy_sorter,
+		file_previewer = previewers.vim_buffer_cat.new,
+		grep_previewer = previewers.vim_buffer_vimgrep.new,
+		qflist_previewer = previewers.vim_buffer_qflist.new,
+
 		color_devicons = true,
 
-		file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-		grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-		qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+		file_ignore_patterns = {"./node%_modules/*", "node%_modules", "^node%_modules/*", "node%_modules/*"},
 	},
 	extensions = {
 		fzy_native = {
@@ -23,7 +29,8 @@ require('telescope').setup{
 	},
 	pickers = {
 		find_files = {
-			hidden = true
+			hidden = true,
+			theme = "dropdown",
 		}
 	}
 }
