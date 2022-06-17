c06fac6bdebb545b68071dafe08f56448e500193
Kirk Easterson <kirk.easterson@gmail.com>
2022-06-17 06:42:23 +0200

    Removed 5 keyword length on autocompletion

--- .config/nvim/lua/kirk/lsp/completion.lua
+++ .config/nvim/lua/kirk/lsp/completion.lua
@@ -44,7 +44,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'luasnip' },
-	{ name = 'buffer', keyword_length = 5 },
+	-- { name = 'buffer', keyword_length = 5 },
   }, {
     { name = 'buffer' },
   }),
