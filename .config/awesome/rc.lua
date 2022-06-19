76d5ebfda1271d5c225dd996316f8c60b76f64f6
Kirk Easterson <kirk.easterson@gmail.com>
2022-06-19 12:13:14 +0200

    Added more changes and a TODO for awesomerc

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,3 +1,13 @@
+-- TODO:
+--	- change key bindings for moving windows to that of i3
+--	- get scratchpad terminals working
+--	- customize top bar
+--	- figure out how gaps work
+--	- resize windows with mouse
+--	- screen locking
+--	- changing screens/moving windows to other screens
+--		- how to tags work with other windows?
+
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
@@ -47,25 +57,20 @@ do
 end
 -- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- THEME
+beautiful.init("~/.config/awesome/theme.lua")
 
--- This is used later as the default terminal and editor to run.
+-- DEFAULT EDITOR
 terminal = "x-terminal-emulator"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
+-- MOD KEY
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- AVAILABLE LAYOUTS
+-- TODO: Reduce this to ones that I actually want
 awful.layout.layouts = {
-	awful.layout.suit.floating,
 	awful.layout.suit.tile,
 	awful.layout.suit.tile.left,
 	awful.layout.suit.tile.bottom,
@@ -78,11 +83,8 @@ awful.layout.layouts = {
 	awful.layout.suit.max.fullscreen,
 	awful.layout.suit.magnifier,
 	awful.layout.suit.corner.nw,
-	-- awful.layout.suit.corner.ne,
-	-- awful.layout.suit.corner.sw,
-	-- awful.layout.suit.corner.se,
+	awful.layout.suit.floating,
 }
--- }}}
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
