0f43199c9100a60e3e2b41e53861b54575052d4b
Kirk Easterson <kirk.easterson@gmail.com>
2022-06-20 21:42:09 +0200

    Changed height of bar in awesomerc

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -207,7 +207,7 @@ awful.screen.connect_for_each_screen(function(s)
 	}
 
 	-- Create the wibox
-	s.mywibox = awful.wibar({ position = "top", screen = s })
+	s.mywibox = awful.wibar({ position = "top", screen = s, height = 16 })
 
 	-- Add widgets to the wibox
 	s.mywibox:setup {
