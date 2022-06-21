6c00ede7df848afbd34fc6558d4705b85ac19d27
Kirk Easterson <kirk.easterson@gmail.com>
2022-06-21 23:11:50 +0200

    Added more funciton key bindings and bindings for gaps

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,6 +1,5 @@
 -- TODO:
 --	- get scratchpad terminals working
---	- customize top bar
 --	- figure out how gaps work
 --	- screen locking
 --	- get a battery module
@@ -111,60 +110,9 @@ else
 	})
 end
 
-
-mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
-	menu = mymainmenu })
-
 -- Menubar configuration
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
--- {{{ Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock('%a %d %b %Y %T')
-
--- Create a wibox for each screen and add it
-local taglist_buttons = gears.table.join(
-	awful.button({}, 1, function(t) t:view_only() end),
-	awful.button({ modkey }, 1, function(t)
-		if client.focus then
-			client.focus:move_to_tag(t)
-		end
-	end),
-	awful.button({}, 3, awful.tag.viewtoggle),
-	awful.button({ modkey }, 3, function(t)
-		if client.focus then
-			client.focus:toggle_tag(t)
-		end
-	end),
-	awful.button({}, 4, function(t) awful.tag.viewnext(t.screen) end),
-	awful.button({}, 5, function(t) awful.tag.viewprev(t.screen) end)
-)
-
-local tasklist_buttons = gears.table.join(
-	awful.button({}, 1, function(c)
-		if c == client.focus then
-			c.minimized = true
-		else
-			c:emit_signal(
-				"request::activate",
-				"tasklist",
-				{ raise = true }
-			)
-		end
-	end),
-	awful.button({}, 3, function()
-		awful.menu.client_list({ theme = { width = 250 } })
-	end),
-	awful.button({}, 4, function()
-		awful.client.focus.byidx(1)
-	end),
-	awful.button({}, 5, function()
-		awful.client.focus.byidx(-1)
-	end))
 
 local function set_wallpaper(s)
 	-- Wallpaper
@@ -188,58 +136,6 @@ awful.screen.connect_for_each_screen(function(s)
 	-- Each screen has its own tag table.
 	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
-	-- Create a promptbox for each screen
-	s.mypromptbox = awful.widget.prompt()
-	-- Create an imagebox widget which will contain an icon indicating which layout we're using.
-	-- We need one layoutbox per screen.
-	s.mylayoutbox = awful.widget.layoutbox(s)
-	s.mylayoutbox:buttons(gears.table.join(
-		awful.button({}, 1, function() awful.layout.inc(1) end),
-		awful.button({}, 3, function() awful.layout.inc(-1) end),
-		awful.button({}, 4, function() awful.layout.inc(1) end),
-		awful.button({}, 5, function() awful.layout.inc(-1) end)))
-	-- Create a taglist widget
-	s.mytaglist = awful.widget.taglist {
-		screen  = s,
-		filter  = awful.widget.taglist.filter.all,
-		buttons = taglist_buttons
-	}
-
-	-- Create a tasklist widget
-	s.mytasklist = awful.widget.tasklist {
-		screen  = s,
-		filter  = awful.widget.tasklist.filter.currenttags,
-		buttons = tasklist_buttons
-	}
-
-	-- Create the wibox
-	s.mywibox = awful.wibar({ position = "top", screen = s, height = 16 })
-
-	-- Add widgets to the wibox
-	s.mywibox:setup {
-		layout = wibox.layout.align.horizontal,
-		{ -- Left widgets
-			layout = wibox.layout.fixed.horizontal,
-			mylauncher,
-			s.mytaglist,
-			s.mypromptbox,
-		},
-		s.mytasklist, -- Middle widget
-		{ -- Right widgets
-			layout = wibox.layout.fixed.horizontal,
-			mykeyboardlayout,
-			ram_widget({}),
-			cpu_widget({}),
-			battery_widget({
-				path_to_icons = "/usr/share/icons/Papirus-Dark/symbolic/status/",
-				show_current_level = true,
-			}),
-			volume_widget({}),
-			wibox.widget.systray(),
-			mytextclock,
-			s.mylayoutbox,
-		},
-	}
 end)
 -- }}}
 
@@ -337,26 +233,50 @@ globalkeys = gears.table.join(
 		end,
 		{ description = "restore minimized", group = "client" }),
 
-	-- Prompt
-	awful.key({ modkey }, "r", function()
-		awful.util.spawn("dmenu_run")
-	end,
-		{ description = "launch dmenu", group = "launcher" }),
-
-	awful.key({ modkey }, "x",
-		function()
-			awful.prompt.run {
-				prompt       = "Run Lua code: ",
-				textbox      = awful.screen.focused().mypromptbox.widget,
-				exe_callback = awful.util.eval,
-				history_path = awful.util.get_cache_dir() .. "/history_eval"
-			}
-		end,
-		{ description = "lua execute prompt", group = "awesome" }),
-
 	-- Menubar
 	awful.key({ modkey }, "p", function() menubar.show() end,
-		{ description = "show the menubar", group = "launcher" })
+		{ description = "show the menubar", group = "launcher" }),
+
+	-- gaps
+	-- TODO: eventually add bindings for resetting gaps and setting to some nice number
+	awful.key({ modkey, "Control" }, "h", function() awful.tag.incgap(-2) end,
+		{ description = "decrease gaps", group = "gaps" }),
+
+	awful.key({ modkey, "Control" }, "l", function() awful.tag.incgap(2) end,
+		{ description = "increase gaps", group = "gaps" }),
+
+	-- volume keys
+	awful.key({}, "XF86AudioRaiseVolume", function() awful.util.spawn("pactl set-sink-volume @DEFAULT_SINK@ +10%", false) end,
+		{ description = "raise volume", group = "volume" }),
+
+	awful.key({}, "XF86AudioLowerVolume", function() awful.util.spawn("pactl set-sink-volume @DEFAULT_SINK@ -10%", false) end,
+		{ description = "lower volume", group = "volume" }),
+
+	awful.key({}, "XF86AudioMute", function() awful.util.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle", false) end,
+		{ description = "mute audio", group = "volume" }),
+
+	awful.key({}, "XF86AudioMicMute", function() awful.util.spawn("pactl set-source-mute @DEFAULT_SOURCE@ toggle", false) end,
+		{ description = "mute mic", group = "volume" }),
+
+	-- media keys
+	awful.key({}, "XF86AudioPlay", function() awful.util.spawn("playerctl play-pause", false) end,
+		{ description = "play/pause media", group = "media" }),
+
+	awful.key({}, "XF86AudioStop", function() awful.util.spawn("playerctl stop", false) end,
+		{ description = "stop media", group = "media" }),
+
+	awful.key({}, "XF86AudioNext", function() awful.util.spawn("playerctl next", false) end,
+		{ description = "next in media", group = "media" }),
+
+	awful.key({}, "XF86AudioPrev", function() awful.util.spawn("playerctl previous", false) end,
+		{ description = "previous in media", group = "media" }),
+
+	-- brightness keys
+	awful.key({}, "XF86MonBrightnessDown", function() awful.util.spawn("brightnessctl --min-val=2 -q set 3%-", false) end,
+		{ description = "decrease monitor brightness", group = "brightness" }),
+
+	awful.key({}, "XF86MonBrightnessUp", function() awful.util.spawn("brightnessctl -q set 3%+", false) end,
+		{ description = "increase monitor brightness", group = "brightness" })
 )
 
 clientkeys = gears.table.join(
@@ -572,3 +492,4 @@ awful.spawn.with_shell("compton")
 awful.spawn.with_shell("$HOME/.fehbg")
 awful.spawn.with_shell("flameshot")
 awful.spawn.with_shell("setxkbmap -option caps:escape")
+awful.spawn.with_shell("$HOME/.config/polybar/launch.sh")
