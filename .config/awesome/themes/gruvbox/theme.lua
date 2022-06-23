c81bad823be1d10f26d0dcc958005e1037b8fc95
Kirk Easterson <kirk.easterson@gmail.com>
2022-06-23 14:36:54 +0200

    Changed font from 8.5 to 8 in awesomewm theme

--- .config/awesome/themes/gruvbox/theme.lua
+++ .config/awesome/themes/gruvbox/theme.lua
@@ -11,7 +11,7 @@ theme.awesome_icon = theme.wd .. "awesome.png"
 
 -- fonts
 theme.base_font     = "JetBrains Mono Nerd Font"
-theme.font          = theme.base_font .. " 8.5"
+theme.font          = theme.base_font .. " 8"
 theme.hotkeys_font  = theme.base_font .. " 10"
 theme.taglist_font  = theme.base_font .. "10"
 theme.icon_font     = theme.base_font .. " 13"
