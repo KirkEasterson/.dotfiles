87ed733b94fb58e7b24d79d9a430df3f97b2ef7b
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-17 16:19:55 +0200

    Added indent to tree-sitter

--- .config/nvim/lua/kirk/treesitter.lua
+++ .config/nvim/lua/kirk/treesitter.lua
@@ -1,25 +1,36 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-	  "go",
-	  "gomod",
-	  "gowork",
-	  "typescript",
-	  "python",
-	  "lua",
-	  "rust",
-	  "bash",
-	  "bibtex",
-	  "css",
-	  "dockerfile",
-	  "html",
-	  "make",
-	  "latex",
-	  "vim",
-	  "javascript"
-  },
-  sync_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+-- local ts_utils = require 'nvim-treesitter.ts_utils'
+
+require 'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+		"bash",
+		"bibtex",
+		"css",
+		"dockerfile",
+		"go",
+		"gomod",
+		"gowork",
+		"html",
+		"javascript",
+		"json",
+		"json5",
+		"latex",
+		"lua",
+		"make",
+		"markdown",
+		"python",
+		"regex",
+		"rust",
+		"tsx",
+		"typescript",
+		"vim",
+		"yaml",
+	},
+	sync_install = false,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enable = true,
+	}
 }
