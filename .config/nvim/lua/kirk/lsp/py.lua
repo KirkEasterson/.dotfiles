a6be7b9ac2058d14496af94f1180249b1baaebb8
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-18 12:29:59 +0200

    Added autopep8 formatting for py.lua

--- .config/nvim/lua/kirk/lsp/py.lua
+++ .config/nvim/lua/kirk/lsp/py.lua
@@ -1,8 +1,24 @@
 require('kirk.lsp.on_attach')
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local py_on_attach = function(client, bufnr)
+	client.server_capabilities.documentFormattingProvider = false
+	client.server_capabilities.document_range_formatting = false
+	on_attach(client, bufnr)
+end
+
 -- https://github.com/microsoft/pyright
-require'lspconfig'.pyright.setup{
-  capabilities=capabilities,
-  on_attach = on_attach,
+require('lspconfig').pyright.setup {
+	capabilities = capabilities,
+	on_attach = py_on_attach,
 }
 
+local null_ls = require("null-ls")
+null_ls.setup({
+	sources = {
+		null_ls.builtins.formatting.autopep8
+	},
+	on_attach = on_attach,
+})
