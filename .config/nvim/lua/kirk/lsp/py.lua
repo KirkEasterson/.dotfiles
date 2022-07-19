c02d20d25082f36e23d7bb5b62d22c1d879e5bbf
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-19 14:33:55 +0200

    Added diagnostics for python

--- .config/nvim/lua/kirk/lsp/py.lua
+++ .config/nvim/lua/kirk/lsp/py.lua
@@ -18,6 +18,8 @@ require('lspconfig').pyright.setup {
 local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
+		null_ls.builtins.diagnostics.pylint,
+		-- null_ls.builtins.code_actions.pyright, -- TODO: find code_actions for python
 		null_ls.builtins.formatting.autopep8
 	},
 	on_attach = on_attach,
