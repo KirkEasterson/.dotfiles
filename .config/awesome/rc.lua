6e4c87fa368e483ac31697239ef2a08ccd87d210
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-22 12:46:36 +0200

    Added rounded corners to windows

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -686,6 +686,12 @@ awful.spawn.with_shell("flameshot")
 awful.spawn.with_shell("setxkbmap -option caps:escape")
 -- awful.spawn("pcmanfm -d") -- TODO: figure out how to start the daemon with spawning a window
 
+-- rounded corners for all windows
+client.connect_signal("manage", function(c)
+	c.shape = gears.shape.rounded_rect
+end)
+
+-- make the garbage collector collect more often
 collectgarbage("setpause", 160)
 collectgarbage("setstepmul", 400)
 
