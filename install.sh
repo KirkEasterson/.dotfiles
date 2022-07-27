b88bfef414d24673a2e3b6994f63fafeb4ed55cd
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-27 17:19:09 +0200

    Fixed formatting in install script

--- install.sh
+++ install.sh
@@ -18,12 +18,10 @@ ign_files+=(`basename "$0"`)
 # iterate through contents of current directory
 # TODO: make this work for already existing directories
 for entry in `ls -aA`; do
-
-		# if current entry is not in the ignore list
-		if ! [[ ${ign_files[*]} =~ (^|[[:space:]])"$entry"($|[[:space:]]) ]]; then
-
-				# create sym link
-				ln -sfn $(pwd)/"$entry" $HOME/"$entry"
-		fi
+	# if current entry is not in the ignore list
+	if ! [[ ${ign_files[*]} =~ (^|[[:space:]])"$entry"($|[[:space:]]) ]]; then
+		# create sym link
+		ln -sfn $(pwd)/"$entry" $HOME/"$entry"
+	fi
 done
 
