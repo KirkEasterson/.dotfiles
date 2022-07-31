f0a1036646073160b94cb16487007b47f22eaf05
Kirk Easterson <kirk.easterson@gmail.com>
2022-07-31 08:46:18 +0000

    Reformatted compeltion file

--- .config/nvim/lua/kirk/lsp/completion.lua
+++ .config/nvim/lua/kirk/lsp/completion.lua
@@ -1,77 +1,78 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local cmp = require'cmp'
+local cmp = require('cmp')
 
 local ok, lspkind = pcall(require, "lspkind")
 if not ok then
-  return
+	return
 end
 
 lspkind.init()
 
 cmp.setup({
 
-  mapping = {
-	["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<c-y>"] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { "i", "c" }
-    ),
+	mapping = {
+		["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+		["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<c-y>"] = cmp.mapping(
+			cmp.mapping.confirm {
+				behavior = cmp.ConfirmBehavior.Insert,
+				select = true,
+			},
+			{ "i", "c" }
+		),
 
-    ["<c-space>"] = cmp.mapping {
-      i = cmp.mapping.complete(),
-      c = function(
-        _ --[[fallback]]
-      )
-        if cmp.visible() then
-          if not cmp.confirm { select = true } then
-            return
-          end
-        else
-          cmp.complete()
-        end
-      end,
-    },
-  },
+		["<c-space>"] = cmp.mapping {
+			i = cmp.mapping.complete(),
+			c = function(
+			_ --[[fallback]]
+			)
+				if cmp.visible() then
+					if not cmp.confirm { select = true } then
+						return
+					end
+				else
+					cmp.complete()
+				end
+			end,
+		},
+	},
 
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'luasnip' },
-	-- { name = 'buffer', keyword_length = 5 },
-  }, {
-    { name = 'buffer' },
-  }),
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp' },
+		{ name = 'path' },
+		{ name = 'luasnip' },
+		{ name = 'spell' },
+		-- { name = 'buffer', keyword_length = 5 },
+	}, {
+		{ name = 'buffer' },
+	}),
 
-  snippet = {
-    expand = function(args)
-  	require('luasnip').lsp_expand(args.body)
-    end,
-  },
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
 
-  formatting = {
-    format = lspkind.cmp_format {
-      with_text = true,
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]",
-        gh_issues = "[issues]",
-        tn = "[TabNine]",
-      },
-    },
-  },
+	formatting = {
+		format = lspkind.cmp_format {
+			with_text = true,
+			menu = {
+				buffer = "[buf]",
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[api]",
+				path = "[path]",
+				luasnip = "[snip]",
+				gh_issues = "[issues]",
+				tn = "[TabNine]",
+			},
+		},
+	},
 
-  experimental = {
-	  native_menu = false,
-	  ghost_text = true,
-  }
+	experimental = {
+		native_menu = false,
+		ghost_text = true,
+	}
 })
