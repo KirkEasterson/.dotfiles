c9e0b10f4e86dbc3fbb706ce7cd117c76bbf1201
Kirk Easterson <kirk.easterson@gmail.com>
2022-08-31 00:48:57 +0200

    Removed some windows from floating rules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -608,14 +608,6 @@ awful.rules.rules = {
 			class = {
 				"Arandr",
 				"Blueman-manager",
-				"Gpick",
-				"Kruler",
-				"MessageWin", -- kalarm.
-				"Sxiv",
-				"Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-				"Wpa_gui",
-				"veromix",
-				"xtightvncviewer",
 			},
 
 			-- Note that the name property shown in xprop might be set slightly after creation of the client
