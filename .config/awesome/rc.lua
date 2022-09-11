b37dcc4035edc4dddd918cb368a1204b37d6ad55
Kirk Easterson <kirk.easterson@gmail.com>
2022-09-11 11:27:40 +0200

    More changes to awesomewm titlebar height

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -801,7 +801,7 @@ client.connect_signal("request::titlebars", function(c)
 	layout:set_right(right_layout)
 
 	local margin_layout = wibox.container.margin(layout, 5, 5, 5, 5)
-	local titlebar_height = math.max(awful.screen.focused().workarea.height * 0.015, 24)
+	local titlebar_height = math.max(math.floor(awful.screen.focused().workarea.height * 0.015), 22)
 
 	awful.titlebar(c, { size = titlebar_height }):set_widget(margin_layout)
 end)
