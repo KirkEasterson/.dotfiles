6f614d0be6fc73c6c1e6cd8294cd44adec2dd06f
Kirk Easterson <kirk.easterson@gmail.com>
2022-09-11 11:19:05 +0200

    Added a config for spaceship

---
+++ .spaceshiprc.zsh
@@ -0,0 +1,15 @@
+SPACESHIP_PROMPT_SEPARATE_LINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+
+SPACESHIP_PROMPT_ORDER=(
+  dir           # Current directory section
+  git           # Git section (git_branch + git_status)
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  async         # Async jobs indicator
+  jobs          # Background jobs indicator
+  line_sep      # Line break
+  # char          # Prompt character
+  vi_mode
+)
