ad68819cd3931a8283ef6d2cb3168983bdf612f7
Kirk Easterson <kirk.easterson@gmail.com>
2022-10-16 20:38:10 +0200

    Cleaned up telescope config

--- .config/nvim/lua/kirk/telescope.lua
+++ .config/nvim/lua/kirk/telescope.lua
@@ -1,30 +1,6 @@
 local previewers = require("telescope.previewers")
 local sorters = require("telescope.sorters")
 
-local util = require("util")
-
-util.map("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
-util.map("n", "<leader>FF", "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>")
-util.map("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
-util.map("n", "<leader>fG", "<cmd>lua require('telescope.builtin').grep_string()<CR>")
-util.map("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>")
--- util.map("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>")
-util.map("n", "<leader>fr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>")
-util.map("n", "<leader>fs", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
-util.map("n", "<leader>ft", "<cmd>lua require('telescope.builtin').treesitter()<CR>")
-
-util.map("n", "<leader>gb", "<cmd>lua require('telescope.builtin').git_branches()<CR>")
-util.map("n", "<leader>gc", "<cmd>lua require('telescope.builtin').git_commits()<CR>")
-util.map("n", "<leader>bc", "<cmd>lua require('telescope.builtin').git_bcommits()<CR>")
-
-util.map("n", "<leader>rr", "<cmd>lua require('telescope.builtin').registers()<CR>")
-
-util.map("n", "<leader>dd", "<cmd>lua require('telescope.builtin').diagnostics()<CR>")
-
-util.map("n", "<leader>gw", "<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>")
-util.map("n", "<leader>gW", "<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>")
-
-
 require('telescope').setup {
 	defaults = {
 		file_sorter = sorters.get_fzy_sorter,
@@ -32,9 +8,24 @@ require('telescope').setup {
 		grep_previewer = previewers.vim_buffer_vimgrep.new,
 		qflist_previewer = previewers.vim_buffer_qflist.new,
 
-		color_devicons = true,
+		color_devicons = false,
 
-		file_ignore_patterns = { "./node%_modules/*", "node%_modules", "^node%_modules/*", "node%_modules/*" },
+		file_ignore_patterns = {
+			"%.a",
+			"%.class",
+			"%.mkv",
+			"%.mp4",
+			"%.o",
+			"%.out",
+			"%.pdf",
+			"%.zip",
+			"./node%_modules/*",
+			".cache",
+			".git/",
+			"^node%_modules/*",
+			"node%_modules",
+			"node%_modules/*",
+		},
 	},
 	extensions = {
 		fzy_native = {
@@ -51,7 +42,10 @@ require('telescope').setup {
 		frecency = {
 			show_scores = false,
 			show_unindexed = true,
-			ignore_patterns = { "*.git/*", "*/tmp/*" },
+			ignore_patterns = {
+				"*.git/*",
+				"*/tmp/*"
+			},
 		},
 	},
 	pickers = {
