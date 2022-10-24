31a14f8e42d04892f492e65e9e1e2e700d13f4a6
Kirk Easterson <kirk.easterson@gmail.com>
2022-10-24 03:28:36 +0200

    Removed most xcompmgr settings so windows are fast

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -847,7 +847,7 @@ local autorunApps =
 	"nm-applet",
 	"package-update-indicator",
 	-- "pcmanfm -d", -- TODO: figure out how to start the daemon with spawning a window
-	"xcompmgr -f -F -D3",
+	"xcompmgr -a",
 	-- "picom",
 	"setxkbmap -option caps:escape",
 	"unclutter",
