3cdcd43d7d67ab0c84f4e6b957c39caf2cb47b09
Kirk Easterson <kirk.easterson@gmail.com>
2022-10-31 21:16:21 +0100

    Removed old code from tmux-nvim conf

--- .config/nvim/lua/kirk/tmux-nvim.lua
+++ .config/nvim/lua/kirk/tmux-nvim.lua
@@ -3,16 +3,4 @@ require('tmux').setup({
 		enable = false,
 		redirect_to_clipboard = true,
 	},
-
-
-
-	-- disable_when_zoomed = true, -- defaults to false
-	-- keybindings = {
-	-- 	left = "<C-h>",
-	-- 	down = "<C-j>",
-	-- 	up = "<C-k>",
-	-- 	right = "<C-l>",
-	-- 	last_active = "<C-\\>",
-	-- 	next = "<C-Space>",
-	-- },
 })
