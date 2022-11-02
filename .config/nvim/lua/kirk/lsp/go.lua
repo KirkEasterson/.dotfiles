8393ef7234cec410319bdb541690107ea4d16f77
Kirk Easterson <kirk.easterson@gmail.com>
2022-11-02 08:36:40 +0100

    Re-enabled gofumpt

--- .config/nvim/lua/kirk/lsp/go.lua
+++ .config/nvim/lua/kirk/lsp/go.lua
@@ -5,8 +5,8 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local go_on_attach = function(client, bufnr)
-	-- client.server_capabilities.documentFormattingProvider = false
-	-- client.server_capabilities.document_range_formatting = false
+	client.server_capabilities.documentFormattingProvider = false
+	client.server_capabilities.document_range_formatting = false
 	on_attach(client, bufnr)
 end
 
