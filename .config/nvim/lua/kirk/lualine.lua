c7b53dbb19b9ea029dc5374b276315da44507213
Kirk Easterson <kirk.easterson@gmail.com>
2022-11-13 19:13:22 +0100

    Made Lualine more sleek

--- .config/nvim/lua/kirk/lualine.lua
+++ .config/nvim/lua/kirk/lualine.lua
@@ -1,10 +1,23 @@
 require('lualine').setup {
-	sections = {
-		lualine_c = {},
-		lualine_x = { 'filetype' },
-		lualine_y = {},
-	},
 	options = {
 		globalstatus = true,
+		component_separators = { left = '|', right = '|' },
+		section_separators = { left = '', right = '' },
+		refresh = {
+			statusline = 50,
+			-- tabline = 50,
+			-- winbar = 50,
+		}
+	},
+	sections = {
+		lualine_c = {},
+
+		-- DEFAULTS
+		-- lualine_a = { 'mode' },
+		-- lualine_b = { 'branch', 'diff', 'diagnostics' },
+		-- lualine_c = { 'filename' },
+		-- lualine_x = { 'encoding', 'fileformat', 'filetype' },
+		-- lualine_y = { 'progress' },
+		-- lualine_z = { 'location' }
 	},
 }
