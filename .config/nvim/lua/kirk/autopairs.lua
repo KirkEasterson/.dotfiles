f8ba84eaf8170c0e1d00eb653982afaff2de7e8c
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-03 22:41:08 +0100

    Refactored autopairs conf

--- .config/nvim/lua/kirk/autopairs.lua
+++ .config/nvim/lua/kirk/autopairs.lua
@@ -1,20 +1,10 @@
 local autopairs = require("nvim-autopairs")
 local Rule = require('nvim-autopairs.rule')
+local ts_conds = require('nvim-autopairs.ts-conds')
 
 autopairs.setup({
 	check_ts = true,
-	ts_config = {
-		lua = {
-			'string',
-		},
-		javascript = {
-			'template_string',
-		},
-	}
 })
-
-local ts_conds = require('nvim-autopairs.ts-conds')
-
 
 autopairs.add_rules({
 	Rule("%", "%", "lua")
