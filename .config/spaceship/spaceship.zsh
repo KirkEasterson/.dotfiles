6af5c49ef9d1e4ecb0cce0dcafa87510fd9a5687
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-04 21:12:05 +0100

    Cleaned up spaceship config

--- .config/spaceship/spaceship.zsh
+++ .config/spaceship/spaceship.zsh
@@ -1,3 +1,9 @@
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_USER_SHOW=always
+SPACESHIP_DIR_TRUNC_REPO=false
+spaceship add ember
+spaceship add --before char vi_mode
+
 SPACESHIP_PROMPT_SEPARATE_LINE=false
 SPACESHIP_CHAR_SYMBOL="❯"
 SPACESHIP_CHAR_SUFFIX=" "
--- .config/spaceship/spaceshiprc.zsh
+++
@@ -1,5 +0,0 @@
-SPACESHIP_TIME_SHOW=true
-SPACESHIP_USER_SHOW=always
-SPACESHIP_DIR_TRUNC_REPO=false
-spaceship add ember
-spaceship add --before char vi_mode
