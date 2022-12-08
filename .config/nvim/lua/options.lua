019c70dce08ef2555e6c946c9aaa899cf2aebaa9
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-08 09:29:14 +0100

    Added option to hide the tilde character at EOB

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -36,6 +36,8 @@ vim.opt.wildmenu = true -- visual autocompletion for command menu
 vim.opt.wildmode = { 'longest', 'list', 'full' }
 vim.opt.clipboard:append('unnamedplus') -- use system clipboard when yanking
 
+vim.opt.fillchars = { eob = " "} -- no tilde (~) at the end of the buffer
+
 vim.opt.swapfile = false -- don't create swap files
 
 vim.opt.splitright = true --  splits open to the right
