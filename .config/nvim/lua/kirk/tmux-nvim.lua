71f6cc56d6b74224771e9fe8a6f2b57ade112861
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-13 07:30:18 +0100

    Added steps for resizing tmux-nvim

--- .config/nvim/lua/kirk/tmux-nvim.lua
+++ .config/nvim/lua/kirk/tmux-nvim.lua
@@ -3,4 +3,9 @@ require('tmux').setup({
 		enable = false,
 		redirect_to_clipboard = true,
 	},
+	resize = {
+		enable_default_keybindings = true,
+		resize_step_x = 8,
+		resize_step_y = 4,
+	},
 })
