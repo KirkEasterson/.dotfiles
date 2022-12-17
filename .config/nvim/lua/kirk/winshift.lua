f323186ec50556dadd840f087dd5de411c5bd635
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-17 13:16:00 +0100

    Removed definition of bindings for winshift from config file

--- .config/nvim/lua/kirk/winshift.lua
+++ .config/nvim/lua/kirk/winshift.lua
@@ -1,10 +1,1 @@
-local util = require("util")
-
-util.map("n", "<leader><leader>w", "<cmd>WinShift<CR>")
-util.map("n", "<leader><leader>s", "<cmd>WinShift swap<CR>")
-util.map("n", "<leader><leader>h", "<cmd>WinShift left<CR>")
-util.map("n", "<leader><leader>j", "<cmd>WinShift down<CR>")
-util.map("n", "<leader><leader>k", "<cmd>WinShift up<CR>")
-util.map("n", "<leader><leader>l", "<cmd>WinShift right<CR>")
-
 require("winshift").setup({})
