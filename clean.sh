5ce257384a62187b0d14d243347ee4ab93de0423
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-22 00:02:59 +0100

    Removed old directories

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO: use environment variable $DOTFILES
-# TODO: find a more elegant way to redirect output
-
-git stash > /dev/null 2>&1
-git checkout master > /dev/null 2>&1
-./install.sh
