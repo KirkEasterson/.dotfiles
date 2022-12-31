8888c7e878c2e25e8e838df703fc6f805706e284
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-31 07:43:48 +0100

    Removed mini.animate

--- nvim/.config/nvim/lua/kirk/mini.lua
+++ nvim/.config/nvim/lua/kirk/mini.lua
@@ -1,15 +1,3 @@
-require('mini.animate').setup({
-	cursor = {
-		enable = false,
-	},
-	open = {
-		enable = false,
-	},
-	close = {
-		enable = false,
-	},
-})
-
 require('mini.surround').setup({})
 
 require('mini.jump').setup({
