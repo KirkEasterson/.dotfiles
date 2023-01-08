0ad8bded6bf8eba80b468651686b103339f875cc
Kirk Easterson <kirk.easterson@gmail.com>
2023-01-08 20:37:54 +0100

    Added more mini plugins

--- nvim/.config/nvim/lua/kirk/mini.lua
+++ nvim/.config/nvim/lua/kirk/mini.lua
@@ -1,7 +1,27 @@
 require('mini.surround').setup({})
 
+require('mini.ai').setup()
+
 require('mini.jump').setup({
 	mappings = {
 		repeat_jump = '', -- TODO: find another mapping for this
+	},
+})
+
+require('mini.animate').setup({
+	cursor = {
+		enable = false,
+	},
+	scroll = {
+		enable = true,
+	},
+	resize = {
+		enable = false,
+	},
+	open = {
+		enable = false,
+	},
+	close = {
+		enable = false,
 	},
 })
