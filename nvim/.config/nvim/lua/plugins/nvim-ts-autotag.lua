7bd44d5969704344fae6b2e3388f3a09c7edd82f
Kirk Easterson <kirk.easterson@gmail.com>
2023-03-01 19:31:46 +0100

    Added nvim-ts-autotag

---
+++ nvim/.config/nvim/lua/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,12 @@
+return {
+	'windwp/nvim-ts-autotag',
+	events = {
+		'FileType html',
+		'FileType xml',
+		'FileType javascriptreact',
+		'FileType jsx',
+		'FileType typescriptreact',
+		'FileType tsx',
+	},
+	config = true,
+}
