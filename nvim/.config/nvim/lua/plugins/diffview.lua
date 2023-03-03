b34c504c98077ee459bedb41560a09952dd0aded
Kirk Easterson <kirk.easterson@gmail.com>
2023-03-03 09:25:01 +0100

    Added diffview

---
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+	'sindrets/diffview.nvim',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+	},
+	cmd = {
+		'DiffviewClose',
+		'DiffviewFileHistory',
+		'DiffviewFocusFiles',
+		'DiffviewOpen',
+		'DiffviewRefresh',
+		'DiffviewToggleFiles',
+	},
+	config = true,
+}
