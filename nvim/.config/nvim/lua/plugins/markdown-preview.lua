e44aeb75f9a6bdcfc55218a3c01843d4a08643b1
Kirk Easterson <kirk.easterson@gmail.com>
2023-03-10 07:35:04 +0100

    Added key binding for markdown preview

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,13 @@
 return {
 	"iamcco/markdown-preview.nvim",
-	cmd = 'MarkdownPreview', -- TODO: make this work somehow
+	event = "FileType markdown",
+	cmd = "MarkdownPreview",
+	keys = {
+		{ "<leader>md", "<Cmd>MarkdownPreview<CR>" },
+	},
 	build = function() vim.fn["mkdp#util#install"]() end,
 	config = function()
 		-- vim.api.nvim_set_var("mkdp_auto_start", 1)
-		vim.api.nvim_set_var("mkdp_echo_preview_url", 1)
+		-- vim.api.nvim_set_var("mkdp_echo_preview_url", 1)
 	end,
 }
