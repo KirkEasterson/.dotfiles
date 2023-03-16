fe6169ef0e0819e5f92803b64305796715f6aee5
Kirk Easterson <kirk.easterson@gmail.com>
2023-03-16 08:27:29 +0100

    Set higher priority for gruvbox

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -1,7 +1,7 @@
 return {
 	'luisiacc/gruvbox-baby',
 	lazy = false,
-	priority = 50,
+	priority = 1000,
 	config = function()
 		-- vim.g.gruvbox_baby_transparent_mode = true
 		vim.cmd [[colorscheme gruvbox-baby]]
