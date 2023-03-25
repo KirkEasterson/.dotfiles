55369d6c613eaad72362d96a2cde913ff264a576
Kirk Easterson <kirk.easterson@gmail.com>
2023-03-25 14:22:15 +0100

    Re-enabled auto session starting

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,15 +1,16 @@
 return {
 	'rmagatti/auto-session',
 	cmd = {
-		"SaveSession",
-		"RestoreSession",
-		"DeleteSession",
 		"Autosession",
+		"DeleteSession",
+		"RestoreSession",
+		"SaveSession",
 	},
+	lazy = false,
 	opts = {
 		log_level = 'error',
 		auto_save_enabled = true,
-		auto_restore_enabled = false,
+		auto_restore_enabled = true,
 		auto_session_use_git_branch = true,
 		pre_save_cmds = {
 			"NvimTreeClose",
