707b33199defc3c11a907fd0875fda778dd85789
Kirk Easterson <kirk.easterson@gmail.com>
2023-03-26 12:07:54 +0200

    Added borders to mason

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -3,11 +3,12 @@ return {
 	event = "VimEnter",
 	opts = {
 		ui = {
+			border = "rounded",
 			icons = {
 				package_installed = "✓",
 				package_pending = "➜",
 				package_uninstalled = "✗"
-			}
-		}
-	}
+			},
+		},
+	},
 }
