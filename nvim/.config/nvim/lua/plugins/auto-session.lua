bcce266cbe7ec774223c05568b5bf908e06cf902
Kirk Easterson <kirk.easterson@gmail.com>
2023-04-02 09:19:28 +0200

    Re-disabled auto-session

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -10,7 +10,7 @@ return {
 	opts = {
 		log_level = 'error',
 		auto_save_enabled = true,
-		auto_restore_enabled = true,
+		auto_restore_enabled = false,
 		auto_session_use_git_branch = true,
 		pre_save_cmds = {
 			"NvimTreeClose",
