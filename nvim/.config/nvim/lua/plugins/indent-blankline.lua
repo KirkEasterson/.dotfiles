b9bb80b60f9a8903b2ef5c996aec66478bcd7ff8
Kirk Easterson <kirk.easterson@gmail.com>
2023-04-07 18:10:22 +0200

    Removed start of context in idnent blankline

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -4,7 +4,6 @@ return {
 	opts = {
 		use_treesitter = true,
 		show_current_context = true,
-		show_current_context_start = true,
 		max_indent_increase = 1,
 		show_trailing_blankline_indent = false,
 	},
