824eefe311fc6908999479bea1972a4fa4bbde5f
Kirk Easterson <kirk.easterson@gmail.com>
2023-04-07 22:59:00 +0200

    Added better chars for indent blanklind

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -2,9 +2,21 @@ return {
 	'lukas-reineke/indent-blankline.nvim',
 	event = "VeryLazy",
 	opts = {
+		char = "▏",
+		context_char = "▏",
 		use_treesitter = true,
 		show_current_context = true,
 		max_indent_increase = 1,
 		show_trailing_blankline_indent = false,
+		filetype_exclude = {
+			"help",
+			"startify",
+			"dashboard",
+			"packer",
+			"neogitstatus",
+			"NvimTree",
+			"Trouble",
+			"Alpha",
+		},
 	},
 }
