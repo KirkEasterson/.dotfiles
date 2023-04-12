5a4f689552f8cae719fd7781c3809b0dbbc31dbf
Kirk Easterson <kirk.easterson@gmail.com>
2023-04-12 08:25:24 +0200

    Added incline.nvim

---
+++ nvim/.config/nvim/lua/plugins/incline.lua
@@ -0,0 +1,23 @@
+return {
+	"b0o/incline.nvim",
+	dependencies = {
+		'ellisonleao/gruvbox.nvim',
+		'luisiacc/gruvbox-baby',
+		'nvim-tree/nvim-web-devicons',
+	},
+	event = "VeryLazy",
+	opts = {
+		highlight = {
+			groups = {
+				InclineNormal = { guibg = require('gruvbox.palette').colors.dark2 },
+				InclineNormalNC = { guibg = require('gruvbox.palette').colors.dark1 },
+			},
+		},
+		window = { margin = { vertical = 0, horizontal = 1 } },
+		render = function(props)
+			local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+			local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+			return { { icon, guifg = color }, { " " }, { filename } }
+		end,
+	},
+}
