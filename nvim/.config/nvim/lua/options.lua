deb9671aefa147e228c38371f121e93fed01c15d
Kirk Easterson <kirk.easterson@gmail.com>
2023-04-12 08:26:47 +0200

    Changed formatting for fill and list chars

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -41,8 +41,20 @@ vim.opt.wildmenu = true -- visual autocompletion for command menu
 vim.opt.wildmode = { 'longest', 'list', 'full' }
 
 vim.opt.list = true
-vim.opt.listchars:append({ lead = '·', trail = '▒', extends = '', precedes = '', tab = null })
-vim.opt.fillchars:append({ eob = " ", fold = " ", foldopen = "󰅀", foldsep = " ", foldclose = "󰅂" })
+vim.opt.listchars:append({
+	lead = '·',
+	trail = '▒',
+	extends = '',
+	precedes = '',
+	tab = null
+})
+vim.opt.fillchars:append({
+	eob = " ",
+	fold = " ",
+	foldopen = "󰅀",
+	foldsep = " ",
+	foldclose = "󰅂"
+})
 
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
