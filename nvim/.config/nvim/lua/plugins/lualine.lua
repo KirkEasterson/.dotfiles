5ce2034acad58ec2ac60454c32e4618f10447cc4
Kirk Easterson <kirk.easterson@gmail.com>
2023-04-12 08:25:39 +0200

    Changed lazy event for lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,7 @@
 return {
 	'nvim-lualine/lualine.nvim',
 	dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-	event = 'VeryLazy',
+	event = 'VimEnter',
 	opts = {
 		disabled_filetypes = {
 			statusline = {
