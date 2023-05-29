f76989828f7c8b567e6b7ce898650b4b746cc5c7
Kirk Easterson <kirk.easterson@gmail.com>
2023-05-29 17:28:04 +0200

    Added lsp file operations

---
+++ nvim/.config/nvim/lua/plugins/lsp-file-operations.lua
@@ -0,0 +1,10 @@
+return {
+	'antosha417/nvim-lsp-file-operations',
+	enabled = false,
+	event = 'VeryLazy',
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ 'nvim-tree/nvim-tree.lua' },
+	},
+	opts = {},
+}
