9907825a2d6cebbecaae3c2e6225e60f6196f98c
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-25 21:28:31 +0200

    Changed install colorscheme for lazy

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -28,6 +28,9 @@ require('lazy').setup('plugins', {
 	change_detection = {
 		notify = false,
 	},
+	install = {
+		colorscheme = { 'gruvbox' },
+	},
 	performance = {
 		rtp = {
 			disabled_plugins = {
