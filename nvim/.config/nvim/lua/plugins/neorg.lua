2772af8391fc01cdc3af0345010c00360e63c797
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-26 19:38:54 +0200

    Updated config for neorg

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -3,23 +3,55 @@ return {
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"nvim-cmp",
 	},
 	cmd = {
 		"Neorg",
 	},
-	event = "FileType *.norg",
+	ft = {
+		"norg",
+	},
 	build = ":Neorg sync-parsers",
 	opts = {
 		load = {
-			["core.defaults"] = {}, -- Loads default behaviour
-			["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-			["core.norg.dirman"] = { -- Manages Neorg workspaces
+			["core.defaults"] = {},
+			["core.completion"] = {
+				config = {
+					engine = "nvim-cmp",
+					name = "[Norg]",
+				},
+			},
+			["core.integrations.nvim-cmp"] = {},
+			["core.concealer"] = {},
+			["core.export"] = {},
+			["core.keybinds"] = {
+				-- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+				config = {
+					default_keybinds = true,
+					neorg_leader = "<Leader><Leader>",
+				},
+			},
+			["core.dirman"] = {
 				config = {
 					workspaces = {
 						notes = "~/Documents/notes",
 					},
+					default_workspace = "notes",
 				},
 			},
 		},
 	},
+	config = function(_, opts)
+		-- set conceal only for norg files
+		vim.api.nvim_create_autocmd({ "FileType" }, {
+			pattern = { 'norg' },
+			callback = function()
+				-- TODO: experiment to see if I don't need all modes
+				vim.opt.concealcursor = 'nvic'
+				vim.opt.conceallevel = 3
+			end,
+		})
+		require('neorg').setup(opts)
+	end
 }
