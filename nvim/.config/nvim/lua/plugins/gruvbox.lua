12ab124329014043d8e5f5e6a39408c5b299b7ab
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-26 19:38:03 +0200

    Added bold and italic highlighting for some treesitter groups in gruvbox

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -2,8 +2,18 @@ return {
 	'luisiacc/gruvbox-baby',
 	lazy = false,
 	priority = 1000,
-	config = function()
+	config = function(_, opts)
 		-- vim.g.gruvbox_baby_transparent_mode = true
+		vim.g.gruvbox_baby_highlights = {
+			["@text.strong"] = {
+				fg = require('gruvbox-baby.colors').config(opts).soft_yellow,
+				style = "bold",
+			},
+			["@text.emphasis"] = {
+				fg = require('gruvbox-baby.colors').config(opts).soft_yellow,
+				style = "italic",
+			},
+		}
 		vim.cmd [[colorscheme gruvbox-baby]]
 	end,
 }
