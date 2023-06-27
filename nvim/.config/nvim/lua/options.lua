7a713228d19874de6b813908ab337dbf90edbdd7
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-27 14:14:42 +0200

    Defined fold options in UFO config

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -56,10 +56,6 @@ vim.opt.fillchars:append({
 	foldclose = "󰅂"
 })
 
-vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
 
 
 vim.opt.swapfile = false  -- don't create swap files
--- nvim/.config/nvim/lua/plugins/ufo.lua
+++ nvim/.config/nvim/lua/plugins/ufo.lua
@@ -25,7 +25,7 @@ return {
 		end
 	},
 	config = function(_, opts)
-		-- these are redundant, since they're also defined in `mappings.lua`
+
 		vim.o.foldcolumn = '1' -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 		vim.o.foldlevelstart = 99
