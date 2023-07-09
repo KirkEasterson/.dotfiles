f467b7f094ceb201d3c84d7ed323aff7d14c42b4
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-09 05:16:54 +0200

    Cleaned up gitsigns, and added a keybinding to toggle line blame

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -4,25 +4,20 @@ return {
 	cmd = {
 		"Gitsigns",
 	},
-	config = function()
-		require('gitsigns').setup({
-			current_line_blame      = true,
-			signs                   = {
-				add          = { text = "▌" },
-				change       = { text = "▌" },
-				topdelete    = { text = "▔" },
-				delete       = { text = "▁" },
-				changedelete = { text = "▁" },
-				untracked    = { text = "▌" }
-			},
-			current_line_blame_opts = {
-				delay = 10,
-			},
-		})
-
-		vim.cmd([[highlight SignColumn guibg=none]])
-		vim.cmd([[highlight GitSignsAdd guibg=none]])
-		vim.cmd([[highlight GitSignsChange guibg=none]])
-		vim.cmd([[highlight GitSignsDelete guibg=none]])
-	end,
+	keys = {
+		{ '<leader>gb', function() require('gitsigns').toggle_current_line_blame() end },
+	},
+	opts = {
+		signs = {
+			add = { text = "▌" },
+			change = { text = "▌" },
+			topdelete = { text = "▔" },
+			delete = { text = "▁" },
+			changedelete = { text = "▁" },
+			untracked = { text = "▌" }
+		},
+		current_line_blame_opts = {
+			delay = 10,
+		},
+	},
 }
