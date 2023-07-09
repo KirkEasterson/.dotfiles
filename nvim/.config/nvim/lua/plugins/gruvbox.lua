7554fbe18a95cf6389fa6481ba940230c5f39b4b
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-09 03:52:06 +0200

    Removed manuall specification of styling for gruvbox since my PR was merged

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -3,25 +3,6 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function(_, opts)
-		-- vim.g.gruvbox_baby_transparent_mode = true
-		vim.g.gruvbox_baby_highlights = {
-			["@text.strong"] = {
-				fg = require('gruvbox-baby.colors').config(opts).soft_yellow,
-				style = "bold",
-			},
-			["@text.emphasis"] = {
-				fg = require('gruvbox-baby.colors').config(opts).soft_yellow,
-				style = "italic",
-			},
-			["@text.underline"] = {
-				fg = require('gruvbox-baby.colors').config(opts).soft_yellow,
-				style = "underline",
-			},
-			["@text.strike"] = {
-				fg = require('gruvbox-baby.colors').config(opts).medium_gray,
-				style = "strikethrough",
-			},
-		}
 		vim.cmd [[colorscheme gruvbox-baby]]
 	end,
 }
