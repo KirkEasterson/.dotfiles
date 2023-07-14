38ecd3cabe37ecd8617198ee940377b148ff055a
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-14 02:45:43 +0200

    Removed `config` for rust-tools

--- nvim/.config/nvim/lua/plugins/rust-tools.lua
+++ nvim/.config/nvim/lua/plugins/rust-tools.lua
@@ -4,12 +4,9 @@ return {
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 	},
-	config = function()
-		require('lsp.on_attach')
-		require('rust-tools').setup({
-			server = {
-				on_attach = on_attach,
-			}
-		})
-	end,
+	opts = {
+		server = {
+			on_attach = require('lsp.on_attach').on_attach,
+		}
+	},
 }
