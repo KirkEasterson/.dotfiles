84a50999d34e5d66281b1edd3e75918abbb6fbc6
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-15 09:58:36 +0200

    Updated config for `auto-session` and removed the pinned commit

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,26 +1,46 @@
 return {
-	'rmagatti/auto-session',
-	commit = "63984ed9c0fb7eae61eb1c2982bc1147e202d23e", -- temp fix until telescope fix is merged
+	"rmagatti/auto-session",
+	dependencies = {
+		-- "nvim-telescope/telescope.nvim",
+	},
 	cmd = {
 		"Autosession",
-		"DeleteSession",
-		"RestoreSession",
-		"SaveSession",
+		"SessionDelete",
+		"SessionRestore",
+		"SessionRestoreFromFile",
+		"SessionSave",
+	},
+	keys = {
+		-- telescope file helpers
+		-- {
+		-- 	"<leader>sl",
+		-- 	function ()
+		-- 		require("auto-session.session-lens").search_session()
+		-- 	end,
+		-- 	desc = "Find files",
+		-- },
 	},
 	opts = {
-		log_level = 'error',
+		log_level = "error",
 		auto_save_enabled = true,
 		auto_restore_enabled = false,
 		auto_session_use_git_branch = true,
 		pre_save_cmds = {
 			"NvimTreeClose",
 		},
+		-- session_lens = {
+		-- 	load_on_setup = true,
+		-- 	theme_conf = { border = true, },
+		-- 	previewer = false,
+		-- },
 	},
-	init = function()
+	init = function ()
 		local session_opts = {
+			"blank",
 			"buffers",
 			"curdir",
 			"folds",
+			"help",
 			"localoptions",
 			"tabpages",
 			"terminal",
