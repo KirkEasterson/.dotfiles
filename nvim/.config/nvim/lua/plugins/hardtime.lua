0090389fa3bf8823dc2894fafdf2cd05796a4fd5
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-15 10:21:50 +0200

    Added `hardtime`

---
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,14 @@
+return {
+	"m4xshen/hardtime.nvim",
+	cmd = "Hardtime",
+	keys = {
+		{
+			"<leader>ht",
+			function ()
+				require("hardtime").toggle()
+			end,
+			desc = "Toggle hardtime",
+		},
+	},
+	opts = {},
+}
