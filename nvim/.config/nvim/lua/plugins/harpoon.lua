1303c087868d7051ba6d70ffb15922ccaff5c1e0
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-15 10:22:29 +0200

    Changed keybindings in harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,48 +1,49 @@
 return {
-	'ThePrimeagen/harpoon',
+	"ThePrimeagen/harpoon",
 	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'nvim-telescope/telescope.nvim',
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope.nvim",
 	},
 	keys = {
 		{
-			'<leader>ha',
-			function() require('harpoon.mark').add_file() end,
+			"<leader>ha",
+			function () require("harpoon.mark").add_file() end,
 			desc = "Add harpoon mark",
 		},
 		{
-			'<leader>hm',
-			function() require('harpoon.ui').toggle_quick_menu() end,
+			"<leader>hq",
+			function () require("harpoon.ui").toggle_quick_menu() end,
 			desc = "Toggle harpoon UI",
 		},
 		{
-			'<leader>h1',
-			function() require('harpoon.ui').nav_file(1) end,
+			"<leader>h1",
+			function () require("harpoon.ui").nav_file(1) end,
 			desc = "Harpoon file 1",
 		},
 		{
-			'<leader>h2',
-			function() require('harpoon.ui').nav_file(2) end,
+			"<leader>h2",
+			function () require("harpoon.ui").nav_file(2) end,
 			desc = "Harpoon file 2",
 		},
 		{
-			'<leader>h3',
-			function() require('harpoon.ui').nav_file(3) end,
+			"<leader>h3",
+			function () require("harpoon.ui").nav_file(3) end,
 			desc = "Harpoon file 3",
 		},
 		{
-			'<leader>h4',
-			function() require('harpoon.ui').nav_file(4) end,
+			"<leader>h4",
+			function () require("harpoon.ui").nav_file(4) end,
 			desc = "Harpoon file 4",
 		},
 		{
-			"<leader>ht",
+			"<leader>hm",
 			"<Cmd>Telescope harpoon marks<CR>",
 			desc = "Harpoon marks",
 		},
 	},
 	opts = {},
-	init = function()
+	config = function ()
+		require("harpoon").setup(opts)
 		require("telescope").load_extension("harpoon")
-	end
+	end,
 }
