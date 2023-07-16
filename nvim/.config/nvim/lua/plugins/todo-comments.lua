aa0dd1c434df6f016871bf6ca27bcadb6b5c0388
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-17 00:53:50 +0200

    Removed dependencies for `todo-comments`

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -2,8 +2,8 @@ return {
 	"folke/todo-comments.nvim",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"folke/trouble.nvim",
-		"nvim-telescope/telescope.nvim",
+		-- "folke/trouble.nvim",
+		-- "nvim-telescope/telescope.nvim",
 	},
 	event = "VeryLazy",
 	cmd = {
