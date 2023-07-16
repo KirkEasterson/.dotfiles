7763a7798b98d21abe0a58dafab1c35439137bb5
Kirk Easterson <kirk.easterson@gmail.com>
2023-07-17 00:46:10 +0200

    Added `block`

---
+++ nvim/.config/nvim/lua/plugins/block.lua
@@ -0,0 +1,16 @@
+return {
+	"HampusHauffman/block.nvim",
+	cmd = {
+		"Block",
+		"BlockOff",
+		"BlockOn",
+	},
+	keys = {
+		{
+			"<leader>bb",
+			"<Cmd>Block<CR>",
+			desc = "Toggle blocks",
+		},
+	},
+	opts = {},
+}
