b2146f2af18027abab3677ce277aa7e5d9f37a11
Kirk Easterson <kirk.easterson@gmail.com>
2023-08-03 18:30:24 +0200

    Changed bindings for bufferline to use ctrl instead

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -9,79 +9,79 @@ return {
 	version = "v4.*",
 	keys = {
 		{
-			'<S-Tab>',
+			'<C-S-Tab>',
 			function() require('bufferline').cycle(-1) end,
 			desc = "Go to previous buffer",
 		},
 		{
-			'<Tab>',
+			'<C-Tab>',
 			function() require('bufferline').cycle(1) end,
 			desc = "Go to next buffer",
 		},
 		{
-			'<A-1>',
+			'<C-1>',
 			function() require('bufferline').go_to_buffer(1, true) end,
 			desc = "Go to buffer 1",
 		},
 		{
-			'<A-2>',
+			'<C-2>',
 			function() require('bufferline').go_to_buffer(2, true) end,
 			desc = "Go to buffer 2",
 		},
 		{
-			'<A-3>',
+			'<C-3>',
 			function() require('bufferline').go_to_buffer(3, true) end,
 			desc = "Go to buffer 3",
 		},
 		{
-			'<A-4>',
+			'<C-4>',
 			function() require('bufferline').go_to_buffer(4, true) end,
 			desc = "Go to buffer 4",
 		},
 		{
-			'<A-5>',
+			'<C-5>',
 			function() require('bufferline').go_to_buffer(5, true) end,
 			desc = "Go to buffer 5",
 		},
 		{
-			'<A-6>',
+			'<C-6>',
 			function() require('bufferline').go_to_buffer(6, true) end,
 			desc = "Go to buffer 6",
 		},
 		{
-			'<A-7>',
+			'<C-7>',
 			function() require('bufferline').go_to_buffer(7, true) end,
 			desc = "Go to buffer 7",
 		},
 		{
-			'<A-8>',
+			'<C-8>',
 			function() require('bufferline').go_to_buffer(8, true) end,
 			desc = "Go to buffer 8",
 		},
 		{
-			'<A-9>',
+			'<C-9>',
 			function() require('bufferline').go_to_buffer(9, true) end,
 			desc = "Go to buffer 9",
 		},
+		-- { -- conflicts with resetting font size in terminals
+		-- 	'<C-0>',
+		-- 	function() require('bufferline').go_to_buffer(10, true) end,
+		-- 	desc = "Go to buffer 10",
+		-- },
 		{
-			'<A-0>',
-			function() require('bufferline').go_to_buffer(10, true) end,
-			desc = "Go to buffer 10",
-		},
-		{
-			'<A-<>',
+			'<C-<>',
 			function() require('bufferline').move(-1) end,
 			desc = "Move buffer to the left",
 		},
 		{
-			'<A->>',
+			'<C->>',
 			function() require('bufferline').move(1) end,
 			desc = "Move buffer to the right",
 		},
 		{
 			'<A-w>',
 			function() require('bufferline').close_others() end,
-			desc = "Move buffer to the right",
+			desc = "Close other buffers",
 		},
 	},
 	opts = {
