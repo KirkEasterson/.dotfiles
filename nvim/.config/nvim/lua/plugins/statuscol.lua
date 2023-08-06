c3e7a246b6d01e9023bc99a1ea9ef886c904a1b4
Kirk Easterson <kirk.easterson@gmail.com>
2023-08-06 23:00:21 +0200

    Added marks to statuscol

--- nvim/.config/nvim/lua/plugins/statuscol.lua
+++ nvim/.config/nvim/lua/plugins/statuscol.lua
@@ -1,4 +1,4 @@
-local gitsigns_bar = '│'
+local gitsigns_bar = "│"
 
 local gitsigns_hl_pool = {
 	GitSignsAdd          = "DiagnosticOk",
@@ -51,19 +51,19 @@ local function get_sign_name(cur_sign)
 end
 
 local function mk_hl(group, sym)
-	return table.concat({ "%#", group, "#", sym, "%*" })
+	return table.concat({ "%#", group, "#", sym, "%*", })
 end
 
 local function get_name_from_group(bufnum, lnum, group)
 	local cur_sign_tbl = vim.fn.sign_getplaced(bufnum, {
 		group = group,
-		lnum = lnum
+		lnum = lnum,
 	})
 
 	return get_sign_name(cur_sign_tbl)
 end
 
-local get_statuscol_gitsign = function(bufnr, lnum)
+local get_statuscol_gitsign = function (bufnr, lnum)
 	local cur_sign_nm = get_name_from_group(bufnr, lnum, "gitsigns_vimfn_signs_")
 
 	if cur_sign_nm ~= nil then
@@ -73,7 +73,7 @@ local get_statuscol_gitsign = function(bufnr, lnum)
 	end
 end
 
-local get_statuscol_diag = function(bufnum, lnum)
+local get_statuscol_diag = function (bufnum, lnum)
 	local cur_sign_nm = get_name_from_group(bufnum, lnum, "*")
 
 	if cur_sign_nm ~= nil and vim.startswith(cur_sign_nm, "DiagnosticSign") then
@@ -83,17 +83,28 @@ local get_statuscol_diag = function(bufnum, lnum)
 	end
 end
 
-local get_statuscol_debug = function(bufnum, lnum)
-	local cur_sign_nm = get_name_from_group(bufnum, lnum, "dap_breakpoints")
+local get_statuscol_marks = function (bufnum, lnum)
+	local cur_sign_nm = get_name_from_group(bufnum, lnum, "MarkSigns")
 
-	if cur_sign_nm ~= nil and vim.startswith(cur_sign_nm, "Dap") then
-		return mk_hl(cur_sign_nm, debug_signs_icons[cur_sign_nm] .. " ")
+	print(vim.inspect(cur_sign_nm))
+	if cur_sign_nm ~= nil and vim.startswith(cur_sign_nm, "Marks") then
+		return mk_hl(cur_sign_nm, string.gsub(cur_sign_nm, "Marks_", ""))
 	else
-		return "  "
+		return " "
 	end
 end
 
-local filter_table = function(t, filterIter)
+local get_statuscol_debug = function (bufnum, lnum)
+	local cur_sign_nm = get_name_from_group(bufnum, lnum, "dap_breakpoints")
+
+	if cur_sign_nm ~= nil and vim.startswith(cur_sign_nm, "Dap") then
+		return mk_hl(cur_sign_nm, debug_signs_icons[cur_sign_nm] .. "")
+	else
+		return " "
+	end
+end
+
+local filter_table = function (t, filterIter)
 	local out = {}
 	for k, v in pairs(t) do
 		if filterIter(v) then
@@ -111,8 +122,8 @@ return {
 		-- 'kevinhwang91/nvim-ufo',
 	},
 	cond = not vim.g.started_by_firenvim,
-	event = 'VeryLazy',
-	config = function()
+	event = "VeryLazy",
+	config = function ()
 		local builtin = require("statuscol.builtin")
 		require("statuscol").setup({
 			setopt = true,
@@ -130,48 +141,63 @@ return {
 				{
 					-- diagnostics
 					text = {
-						function()
+						function ()
 							return get_statuscol_diag(vim.fn.bufnr(), vim.v.lnum)
-						end
+						end,
 					},
-					condition = { function()
-						local cur_sign_tbl = vim.fn.sign_getplaced(vim.fn.bufnr(), {
-							group = "*",
-						})
-						local filtered_table = filter_table(cur_sign_tbl[1].signs, function(v)
-							return string.find(v.name, "Diagnostic", 0, true)
-						end)
+					condition = { function ()
+						local cur_sign_tbl = vim.fn.sign_getplaced(
+							vim.fn.bufnr(), {
+								group = "*",
+							})
+						local filtered_table = filter_table(
+							cur_sign_tbl[1].signs, function (v)
+								return string.find(v.name, "Diagnostic", 0, true)
+							end)
 						return next(filtered_table) ~= nil
-					end
+					end,
 					},
 				},
 				{
-					-- dap
-					text = { function()
-						return get_statuscol_debug(vim.fn.bufnr(), vim.v.lnum)
-					end },
-					condition = { function()
-						local cur_sign_tbl = vim.fn.sign_getplaced(vim.fn.bufnr(), {
-							group = "dap_breakpoints",
-						})
+					-- marks
+					text = {
+						function ()
+							return get_statuscol_marks(vim.fn.bufnr(), vim.v
+								.lnum)
+						end,
+					},
+					-- condition = { true, },
+					condition = { function ()
+						local cur_sign_tbl = vim.fn.sign_getplaced(
+							vim.fn.bufnr(), {
+								group = "MarkSigns",
+							})
 						return next(cur_sign_tbl[1].signs) ~= nil
-					end },
+					end, },
+				},
+				{
+					-- dap
+					text = { function ()
+						return get_statuscol_debug(vim.fn.bufnr(), vim.v.lnum)
+					end, },
+					condition = { true, },
 					-- click = "v:lua.ScLa",
 				},
 				{
 					-- line numbers
-					text = { builtin.lnumfunc },
-					condition = { true },
+					text = { builtin.lnumfunc, },
+					condition = { true, },
 					click = "v:lua.ScLa",
 				},
 				{
 					-- git signs
 					text = {
-						function()
-							return get_statuscol_gitsign(vim.fn.bufnr(), vim.v.lnum)
-						end
+						function ()
+							return get_statuscol_gitsign(vim.fn.bufnr(),
+								vim.v.lnum)
+						end,
 					},
-					condition = { true },
+					condition = { true, },
 				},
 				-- { -- fold
 				-- 	text = { builtin.foldfunc },
@@ -180,5 +206,5 @@ return {
 				-- },
 			},
 		})
-	end
+	end,
 }
