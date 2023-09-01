ac49f18febbf442c020c34209d281d627455257e
Kirk Easterson <kirk.easterson@gmail.com>
2023-09-02 01:59:29 +0200

    Added opts for diffview

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -13,5 +13,24 @@ return {
 		'DiffviewRefresh',
 		'DiffviewToggleFiles',
 	},
-	opts = {},
+	config = function()
+		require('diffview').setup({
+			enhanced_diff_hl = true,
+			view = {
+				merge_tool = {
+					winbar_info = false,
+				},
+			},
+			keymaps = {
+				file_panel = {
+					{ "n", "<c-u>", require("diffview.actions").scroll_view(-0.1), { desc = "Scroll the view up" } },
+					{ "n", "<c-d>", require("diffview.actions").scroll_view(0.1),  { desc = "Scroll the view down" } },
+				},
+				file_history_panel = {
+					{ "n", "<c-u>", require("diffview.actions").scroll_view(-0.1), { desc = "Scroll the view up" } },
+					{ "n", "<c-d>", require("diffview.actions").scroll_view(0.1),  { desc = "Scroll the view down" } },
+				},
+			},
+		})
+	end,
 }
