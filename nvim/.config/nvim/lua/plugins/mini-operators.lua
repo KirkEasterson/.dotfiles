a7d003c69101b5a5a23f62e94a1f7c28114c9ce0
Kirk Easterson <kirk.easterson@gmail.com>
2023-09-11 08:22:06 +0200

    Added mini operators

---
+++ nvim/.config/nvim/lua/plugins/mini-operators.lua
@@ -0,0 +1,12 @@
+return {
+	'echasnovski/mini.operators',
+	version = '*',
+	keys = {
+		{ "g=", mode = { 'v' }, }, -- evaluate
+		{ "gm", mode = { 'v' }, }, -- multiply
+		{ "gr", mode = { 'v' }, }, -- replace
+		{ "gs", mode = { 'v' }, }, -- sort
+		{ "gx", mode = { 'v' }, }, -- exchange
+	},
+	opts = {},
+}
