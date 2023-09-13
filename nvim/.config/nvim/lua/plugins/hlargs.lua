21f7c43f835b8021ec47ba76c612015f2726e6f1
Kirk Easterson <kirk.easterson@gmail.com>
2023-09-13 14:09:55 +0200

    Added a condition for hlargs

--- nvim/.config/nvim/lua/plugins/hlargs.lua
+++ nvim/.config/nvim/lua/plugins/hlargs.lua
@@ -1,7 +1,6 @@
 return {
 	'm-demare/hlargs.nvim',
-	cond = not vim.g.started_by_firenvim,
-	enabled = false,
+	cond = vim.version().api_level < 10 and not vim.g.started_by_firenvim,
 	event = 'BufEnter',
 	dependencies = {
 		'nvim-treesitter/nvim-treesitter',
