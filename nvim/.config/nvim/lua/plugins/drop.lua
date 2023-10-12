645da12dba8f668958a0b51112b62dc0e13a623f
Kirk Easterson <kirk.easterson@gmail.com>
2023-10-12 22:13:04 +0200

    added some custom themes

--- nvim/.config/nvim/lua/plugins/drop.lua
+++ nvim/.config/nvim/lua/plugins/drop.lua
@@ -1,15 +1,15 @@
 local month_themes = {
 	[1] = "snow",
-	[2] = "snow",
-	[3] = "spring",
+	[2] = "valentines",
+	[3] = "st_pats",
 	[4] = "spring",
 	[5] = "spring",
 	[6] = "summer",
 	[7] = "summer",
 	[8] = "summer",
 	[9] = "leaves",
-	[10] = "leaves",
-	[11] = "leaves",
+	[10] = "halloween",
+	[11] = "thanksgiving",
 	[12] = "xmas",
 }
 
@@ -24,4 +24,28 @@ return {
 		-- screensaver = 1000 * 60 * 10, -- show after 10 minutes
 		screensaver = false,
 	},
+	config = function(_, opts)
+		local themes = require("drop.config").themes
+		themes["valentines"] = {
+			symbols = { "💘", "❤️", "💓", "💕", "💙", "💚", "💛",
+				"💜", "💝", "💖", "♥️", "🤎",
+				"🧡", "❣️", "😍", },
+			colors = {},
+		}
+		themes["st_pats"] = {
+			symbols = { "🍻", "🍺", "🌈", "☘️", "🍀", "🪙", },
+			colors = {},
+		}
+		themes["halloween"] = {
+			symbols = { "🎃", "👻", "💀", "😱", "🍬", "🍫", "👺",
+				"👹", },
+			colors = {},
+		}
+		themes["thanksgiving"] = {
+			symbols = { "🦃", "🌽", "🍗", "🥕", "🍁", "🥧", },
+			colors = {},
+		}
+
+		require("drop").setup(opts)
+	end,
 }
