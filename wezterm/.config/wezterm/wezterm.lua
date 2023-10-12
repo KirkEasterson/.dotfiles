347e6405d68da79b11323cbe8effd0064d9ea7c4
Kirk Easterson <kirk.easterson@gmail.com>
2023-10-12 22:09:06 +0200

    went back to comic code

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -54,11 +54,12 @@ end
 
 return {
 	font = wezterm.font_with_fallback({
-		'JetBrains Mono Nerd Font',
 		'ComicCodeLigatures Nerd Font',
+		'JetBrains Mono Nerd Font',
 	}),
 	font_size = 13,
-	-- line_height = 1.2,
+	line_height = 1.05,
+	cell_width = 0.9,
 	freetype_load_target = "Normal",
 	warn_about_missing_glyphs = false, -- TODO: find which glyphs are missing
 	underline_position = -3,
