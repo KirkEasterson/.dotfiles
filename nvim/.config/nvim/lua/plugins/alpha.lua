be0bdc8fc5dc80c6c6b549fb7a9801aff097cf35
Kirk Easterson <kirk.easterson@gmail.com>
2023-11-07 19:59:19 +0100

    disable lazy for alpha

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -287,7 +287,8 @@ return {
 		'BlakeJC94/alpha-nvim-fortune',
 	},
 	cond = not vim.g.started_by_firenvim,
-	event = "VimEnter",
+	-- event = "VimEnter",
+	lazy = false,
 	priority = 999, -- 1 less than gruvbox
 	config = function(_, opts)
 		local dashboard = require("alpha.themes.dashboard")
