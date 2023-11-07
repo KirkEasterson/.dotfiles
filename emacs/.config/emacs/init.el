d1fecf78bdae0ae2b885575252ede9bc646ec384
Kirk Easterson <kirk.easterson@gmail.com>
2023-11-07 20:05:52 +0100

    update emacs config. WIP

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,171 @@
+(setq inhibit-startup-message t) ;; no startup screen
+
+(scroll-bar-mode -1) ;; disable scrollbar
+(tool-bar-mode -1) ;; disable toolbar
+(menu-bar-mode -1) ;; disable menubar
+(tooltip-mode -1) ;; disable tooltips
+(set-fringe-mode 10) ;; some breathing room
+
+(setq visible-bell t) ;; enable visual bell
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(set-face-attribute 'default nil :font "ComicCodeLigatures Nerd Font" :height 150)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+		 term-mode-hook
+		 eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(require 'package)
+(setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
+			 ("org" . "https://prgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; initialize use-package on non-linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+
+
+(use-package magit)
+(use-package swiper)
+(use-package undo-tree
+  :ensure t
+  :config
+  (global-undo-tree-mode))
+
+(use-package which-key
+	     :init (which-key-mode)
+	     :diminish which-key-mode
+	     :config
+	     (setq which-key-idle-delay 0.3))
+
+(use-package ivy
+	     :diminish
+	     :config
+	     (ivy-mode 1))
+
+(use-package counsel
+	     :bind (("M-x" . counsel-M-x)
+		    ("C-x b" . counsel-ibuffer)
+		    ("C-x C-f" . counsel-find-file)
+		    :map minibuffer-local-map
+		    ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package ivy-rich
+	     :init
+	     (ivy-rich-mode 1))
+
+(use-package helpful
+	     :custom
+	     (counsel-describe-function-function #'helpful-callable)
+	     (counsel-describe-variable-function #'helpful-variable)
+	     :bind
+	     ([remap describe-function] . counsel-describe-function)
+	     ([remap describe-command] . helpful-command)
+	     ([remap describe-variable] . counsel-describe-variable)
+	     ([remap describe-key] . helpful-key))
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (XXX-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+;; optionally
+(use-package lsp-ui :commands lsp-ui-mode)
+;; if you are ivy user
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
+(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+;; ;; optionally if you want to use debugger
+;; (use-package dap-mode)
+;; ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    '(evil-numbers helm-lsp which-key magit lsp-ui lsp-ivy ivy-rich helpful evil-surround doom-themes doom-modeline dap-mode counsel)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+
+(use-package evil
+	     :ensure t
+	     :config
+	     (evil-mode 1))
+	     ;; (evil-want-fine-undo 1))
+(setq evil-undo-system 'undo-tree)
+
+(use-package evil-surround
+	     :ensure t
+	     :config
+	     (global-evil-surround-mode 1))
+
+;; (use-package evil-numbers
+;; 	     :config
+;; 	     (global-evil-surround-mode 1)
+;; 	     :bind (
+;; 		:map evil-normal-state-map
+;; 		("C-a" . 'evil-numbers/inc-at-pt)
+;; 		("C-x" . 'evil-numbers/dec-at-pt)))
+
+(use-package evil-commentary
+	     :ensure t
+	     :config
+	     (evil-commentary-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(evil-commentary which-key magit lsp-ui lsp-ivy ivy-rich helpful evil-surround evil-numbers doom-themes doom-modeline dap-mode counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
