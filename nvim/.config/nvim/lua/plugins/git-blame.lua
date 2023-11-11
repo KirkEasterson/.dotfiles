bc7413a85195cef5b3ba7b979485706acfc5d31d
Kirk Easterson <kirk.easterson@gmail.com>
2023-11-11 12:22:12 +0100

    fix virtual-text highlighting issue

--- nvim/.config/nvim/lua/plugins/git-blame.lua
+++ nvim/.config/nvim/lua/plugins/git-blame.lua
@@ -18,6 +18,12 @@ return {
 			desc = "Toggle git line blame",
 		},
 	},
+	init = function()
+		-- for git-blame.nvim https://github.com/f-person/git-blame.nvim/issues/32#issuecomment-1124506718
+		vim.g.gitblame_set_extmark_options = {
+			hl_mode = "combine",
+		}
+	end,
 	opts = {
 		enabled = false,
 		delay = 1,
