e79983c3c9d505d9b5297901437d1118992449e6
Kirk Easterson <kirk.easterson@gmail.com>
2023-11-17 09:34:04 +0100

    updated marks keys

--- nvim/.config/nvim/lua/plugins/marks.lua
+++ nvim/.config/nvim/lua/plugins/marks.lua
@@ -5,15 +5,41 @@ return {
 	cmd = {
 		"BookmarksList",
 		"BookmarksListAll",
+		"BookmarksQFList",
+		"BookmarksQFListAll",
 		"MarksListAll",
 		"MarksListBuf",
 		"MarksListGlobal",
-		"MarksQFList",
-		"MarksQFListAll",
 		"MarksQFListAll",
 		"MarksQFListBuf",
 		"MarksQFListGlobal",
 		"MarksToggleSigns",
+	},
+	keys = {
+		{
+			'm,',
+			desc = "Set next available mark",
+		},
+		{
+			'm;',
+			desc = "Toggle next available mark",
+		},
+		{
+			'm', -- And then a character
+			desc = "Add mark",
+		},
+		{
+			'dm', -- And then a character
+			desc = "Delete mark",
+		},
+		{
+			'dm-',
+			desc = "Delete all marks in current line",
+		},
+		{
+			'dm<Space>',
+			desc = "Delete all marks in current buffer",
+		},
 	},
 	opts = {
 		-- default_mappings = false,
