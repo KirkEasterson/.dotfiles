a06b37ae33bdea776dc81d63c283067fe673d952
Kirk Easterson <kirk.easterson@gmail.com>
2023-11-20 12:15:58 +0100

    added dependencies for neodev

--- nvim/.config/nvim/lua/plugins/neodev.lua
+++ nvim/.config/nvim/lua/plugins/neodev.lua
@@ -1,6 +1,10 @@
 return {
 	"folke/neodev.nvim",
 	cond = not vim.g.started_by_firenvim,
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/neotest",
+	},
 	opts = {
 		library = {
 			plugins = {
