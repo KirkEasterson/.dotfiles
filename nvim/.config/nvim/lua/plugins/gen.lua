5c06dea50b794e76e94afd915b0b343508c3e1fb
Kirk Easterson <kirk.easterson@gmail.com>
2023-11-22 11:50:38 +0100

    added config for gen.lua

---
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -0,0 +1,23 @@
+return {
+	'David-Kunz/gen.nvim',
+	cond = not vim.g.started_by_firenvim,
+	-- dev = true,
+	cmd = {
+		"Gen",
+	},
+	keys = {
+		{
+			"<leader>\\",
+			"<cmd>Gen<cr>",
+			desc = "Ollama",
+			mode = { "n", "v" },
+		},
+	},
+	config = function(_, opts)
+		local gen = require('gen')
+
+		-- gen.model = 'codellama' -- default 'mistral:instruct'
+		-- gen.container = 'ollama' -- if running in docker container
+		-- gen.command = 'docker exec -it ollama ollama run $model $prompt'
+	end,
+}
