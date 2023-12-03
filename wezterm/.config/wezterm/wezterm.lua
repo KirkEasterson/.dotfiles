24d8bb5e4608f0d592c72ae8da1ddb089e2b0e72
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-03 15:53:54 +0100

    added `stylua.toml`

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,14 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- for nvim zen mode
-wezterm.on('user-var-changed', function(window, pane, name, value)
+wezterm.on("user-var-changed", function(window, pane, name, value)
 	local overrides = window:get_config_overrides() or {}
 	if name == "ZEN_MODE" then
 		local incremental = value:find("+")
 		local number_value = tonumber(value)
 		if incremental ~= nil then
-			while (number_value > 0) do
+			while number_value > 0 do
 				window:perform_action(wezterm.action.IncreaseFontSize, pane)
 				number_value = number_value - 1
 			end
@@ -25,6 +25,7 @@ wezterm.on('user-var-changed', function(window, pane, name, value)
 	window:set_config_overrides(overrides)
 end)
 
+-- TODO: get this to adjust to system theme
 -- wezterm.on("window-config-reloaded", function(window, pane)
 -- 	local overrides = window:get_config_overrides() or {}
 -- 	local scheme = scheme_for_appearance(get_appearance())
@@ -38,13 +39,13 @@ function get_appearance()
 	if wezterm.gui then
 		return wezterm.gui.get_appearance()
 	end
-	return 'dark'
+	return "dark"
 end
 
 function scheme_for_appearance(appearance)
-	local mode = "dark"
-	os.execute("notify-send " .. appearance:lower())
-	if appearance:lower():find('dark') then
+	os.execute("notify-send " .. appearance:lower()) -- debugging
+	local mode = ""
+	if appearance:lower():find("dark") then
 		mode = "dark"
 	else
 		mode = "light"
@@ -54,14 +55,14 @@ end
 
 return {
 	font = wezterm.font_with_fallback({
-		'ComicCodeLigatures Nerd Font',
-		'JetBrains Mono Nerd Font',
+		"ComicCodeLigatures Nerd Font",
+		"JetBrains Mono Nerd Font",
 	}),
-	font_size = 12,
-	-- line_height = 1.05,
+	font_size = 16,
+	line_height = 0.89,
 	cell_width = 0.9,
 	freetype_load_target = "Normal",
-	warn_about_missing_glyphs = false, -- TODO: find which glyphs are missing
+	-- warn_about_missing_glyphs = false, -- TODO: find which glyphs are missing
 	underline_position = -3,
 	strikethrough_position = 8,
 	adjust_window_size_when_changing_font_size = false,
@@ -72,8 +73,7 @@ return {
 	},
 	-- color_scheme = scheme_for_appearance(get_appearance()),
 	color_scheme = "Gruvbox dark, medium (base16)",
-	-- front_end = "Software",
-	front_end = "OpenGL", -- this causes wezterm to freeze randomly
+	front_end = "OpenGL",
 	hide_tab_bar_if_only_one_tab = true,
 	window_padding = {
 		left = 0,
@@ -86,8 +86,8 @@ return {
 	pane_focus_follows_mouse = true,
 	enable_wayland = false,
 	max_fps = 120,
-	window_close_confirmation = 'NeverPrompt',
-	audible_bell = 'SystemBeep',
+	window_close_confirmation = "NeverPrompt",
+	audible_bell = "SystemBeep",
 	visual_bell = {
 		fade_in_function = "EaseIn",
 		fade_out_function = "EaseOut",
@@ -101,47 +101,47 @@ return {
 	check_for_updates = false,
 	disable_default_key_bindings = true,
 	keys = {
-		{ key = '+',        mods = 'CTRL',       action = act.IncreaseFontSize },
-		{ key = '+',        mods = 'SHIFT|CTRL', action = act.IncreaseFontSize },
-		{ key = '=',        mods = 'CTRL',       action = act.IncreaseFontSize },
-		{ key = '=',        mods = 'SHIFT|CTRL', action = act.IncreaseFontSize },
-		{ key = '=',        mods = 'SUPER',      action = act.IncreaseFontSize },
-		{ key = '-',        mods = 'CTRL',       action = act.DecreaseFontSize },
-		{ key = '-',        mods = 'SHIFT|CTRL', action = act.DecreaseFontSize },
-		{ key = '-',        mods = 'SUPER',      action = act.DecreaseFontSize },
-		{ key = '_',        mods = 'CTRL',       action = act.DecreaseFontSize },
-		{ key = '_',        mods = 'SHIFT|CTRL', action = act.DecreaseFontSize },
-		{ key = '0',        mods = 'CTRL',       action = act.ResetFontSize },
-		{ key = '0',        mods = 'ALT',        action = act.ResetFontSize },
-		{ key = '0',        mods = 'SUPER',      action = act.ResetFontSize },
-		{ key = 'Copy',     mods = 'NONE',       action = act.CopyTo 'Clipboard' },
-		{ key = 'c',        mods = 'SUPER',      action = act.CopyTo 'Clipboard' },
-		{ key = 'C',        mods = 'SHIFT|CTRL', action = act.CopyTo 'Clipboard' },
-		{ key = 'c',        mods = 'SHIFT|CTRL', action = act.CopyTo 'Clipboard' },
-		{ key = 'Paste',    mods = 'NONE',       action = act.PasteFrom 'Clipboard' },
-		{ key = 'v',        mods = 'SUPER',      action = act.PasteFrom 'Clipboard' },
-		{ key = 'v',        mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
-		{ key = 'V',        mods = 'SHIFT|CTRL', action = act.PasteFrom 'Clipboard' },
-		{ key = 'PageUp',   mods = 'SHIFT',      action = act.ScrollByPage(-1) },
-		{ key = 'PageDown', mods = 'SHIFT',      action = act.ScrollByPage(1) },
+		{ key = "+", mods = "CTRL", action = act.IncreaseFontSize },
+		{ key = "+", mods = "SHIFT|CTRL", action = act.IncreaseFontSize },
+		{ key = "=", mods = "CTRL", action = act.IncreaseFontSize },
+		{ key = "=", mods = "SHIFT|CTRL", action = act.IncreaseFontSize },
+		{ key = "=", mods = "SUPER", action = act.IncreaseFontSize },
+		{ key = "-", mods = "CTRL", action = act.DecreaseFontSize },
+		{ key = "-", mods = "SHIFT|CTRL", action = act.DecreaseFontSize },
+		{ key = "-", mods = "SUPER", action = act.DecreaseFontSize },
+		{ key = "_", mods = "CTRL", action = act.DecreaseFontSize },
+		{ key = "_", mods = "SHIFT|CTRL", action = act.DecreaseFontSize },
+		{ key = "0", mods = "CTRL", action = act.ResetFontSize }, -- this isn't working 🤔
+		{ key = "0", mods = "ALT", action = act.ResetFontSize },
+		{ key = "0", mods = "SUPER", action = act.ResetFontSize },
+		{ key = "Copy", mods = "NONE", action = act.CopyTo("Clipboard") },
+		{ key = "c", mods = "SUPER", action = act.CopyTo("Clipboard") },
+		{ key = "C", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
+		{ key = "c", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
+		{ key = "Paste", mods = "NONE", action = act.PasteFrom("Clipboard") },
+		{ key = "v", mods = "SUPER", action = act.PasteFrom("Clipboard") },
+		{ key = "v", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
+		{ key = "V", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
+		{ key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-1) },
+		{ key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(1) },
 
 		-- for pane control in tmux/nvim
-		{ key = 'h',        mods = 'SHIFT|CTRL', action = act.SendString("\x1b[27;6;72~") },
-		{ key = 'j',        mods = 'SHIFT|CTRL', action = act.SendString("\x1b[27;6;74~") },
-		{ key = 'k',        mods = 'SHIFT|CTRL', action = act.SendString("\x1b[27;6;75~") },
-		{ key = 'l',        mods = 'SHIFT|CTRL', action = act.SendString("\x1b[27;6;76~") },
+		{ key = "h", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;72~") },
+		{ key = "j", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;74~") },
+		{ key = "k", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;75~") },
+		{ key = "l", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;76~") },
 
 		-- for tab selection in nvim
-		{ key = '1',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;49~") },
-		{ key = '2',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;50~") },
-		{ key = '3',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;51~") },
-		{ key = '4',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;52~") },
-		{ key = '5',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;53~") },
-		{ key = '6',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;54~") },
-		{ key = '7',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;55~") },
-		{ key = '8',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;56~") },
-		{ key = '9',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;57~") },
-		{ key = '0',        mods = 'CTRL',       action = act.SendString("\x1b[27;5;48~") },
+		{ key = "1", mods = "CTRL", action = act.SendString("\x1b[27;5;49~") },
+		{ key = "2", mods = "CTRL", action = act.SendString("\x1b[27;5;50~") },
+		{ key = "3", mods = "CTRL", action = act.SendString("\x1b[27;5;51~") },
+		{ key = "4", mods = "CTRL", action = act.SendString("\x1b[27;5;52~") },
+		{ key = "5", mods = "CTRL", action = act.SendString("\x1b[27;5;53~") },
+		{ key = "6", mods = "CTRL", action = act.SendString("\x1b[27;5;54~") },
+		{ key = "7", mods = "CTRL", action = act.SendString("\x1b[27;5;55~") },
+		{ key = "8", mods = "CTRL", action = act.SendString("\x1b[27;5;56~") },
+		{ key = "9", mods = "CTRL", action = act.SendString("\x1b[27;5;57~") },
+		-- { key = "0", mods = "CTRL", action = act.SendString("\x1b[27;5;48~") },
 
 		-- -- for pane moving in tmux
 		-- { key = '1',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;49~") },
