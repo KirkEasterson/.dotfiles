c4bf09e509e3bf5fdb773db7ad1eccfb0c1257b9
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-05 07:56:59 +0100

    added commented out fzf config

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,11 +1,11 @@
 return {
 	"NeogitOrg/neogit",
 	cond = not vim.g.started_by_firenvim,
-	-- enabled = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope.nvim",
 		"sindrets/diffview.nvim",
+		-- 'ibhagwan/fzf-lua',
 	},
 	cmd = "Neogit",
 	keys = {
@@ -19,11 +19,8 @@ return {
 		},
 	},
 	opts = {
-		disable_signs = false,
-		disable_hint = true,
 		disable_commit_confirmation = true,
 		disable_insert_on_commit = false,
-		auto_refresh = true,
 		use_per_project_settings = false,
 		kind = "split",
 		signs = {
@@ -35,6 +32,7 @@ return {
 		integrations = {
 			telescope = true,
 			diffview = true,
+			-- fzf_lua = true,
 		},
 		mappings = {
 			status = {
