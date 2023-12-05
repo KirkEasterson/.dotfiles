8687adda9f7beca3032dab3550c60a8af25351e7
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-05 08:14:33 +0100

    removed commented out delve config

--- nvim/.config/nvim/lua/plugins/mason-nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/mason-nvim-dap.lua
@@ -27,16 +27,6 @@ return {
 			function(config)
 				require("mason-nvim-dap").default_setup(config)
 			end,
-			-- delve = function(config)
-			-- 	config.adapters = {
-			-- 		type = "executable",
-			-- 		command = "/usr/bin/python3",
-			-- 		args = {
-			-- 			"-m",
-			-- 			"debugpy.adapter",
-			-- 		},
-			-- 	}
-			-- end,
 		},
 	},
 }
