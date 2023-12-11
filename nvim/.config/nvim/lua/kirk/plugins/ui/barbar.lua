405c978f3ce17ff00be6879a7c5c1158a8cdad6a
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-11 01:31:44 +0100

    added old barbar highlights

--- nvim/.config/nvim/lua/kirk/plugins/ui/barbar.lua
+++ nvim/.config/nvim/lua/kirk/plugins/ui/barbar.lua
@@ -12,4 +12,14 @@ return {
 		maximum_padding = 1,
 		minimum_padding = 1,
 	},
+	config = function(_, opts)
+		local palette = require("gruvbox").palette
+
+		vim.cmd("highlight BufferCurrent guibg=" .. palette.bg0 .. " guifg=none")
+		vim.cmd("highlight BufferCurrentSign guibg=" .. palette.bg0 .. " guifg=none")
+		vim.cmd("highlight BufferVisible guibg=" .. palette.bg0 .. " guifg=" .. palette.neutral_yellow)
+		vim.cmd("highlight BufferVisibleSign guibg=" .. palette.bg0 .. " guifg=" .. palette.neutral_yellow)
+		vim.cmd("highlight BufferInactive guibg=" .. palette.bg0 .. " guifg=" .. palette.gray)
+		vim.cmd("highlight BufferInactiveSign guibg=" .. palette.bg0 .. " guifg=" .. palette.gray)
+	end,
 }
