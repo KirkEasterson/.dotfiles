d05c162421d11fc1240d3bba91fe86f6eabf3fe3
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-11 01:26:35 +0100

    removed old highlight groups

--- nvim/.config/nvim/lua/kirk/plugins/theme/gruvbox.lua
+++ nvim/.config/nvim/lua/kirk/plugins/theme/gruvbox.lua
@@ -12,19 +12,6 @@ return {
 		},
 	},
 	config = function(_, opts)
-		local palette = require("gruvbox").palette
-
-		opts.overrides = {
-
-			--  barbar
-			BufferCurrent = { bg = palette.bg0, fg = nil },
-			BufferCurrentSign = { bg = palette.bg0, fg = nil },
-			BufferVisible = { bg = palette.bg1, fg = palette.neutral_yellow },
-			BufferVisibleSign = { bg = palette.bg1, fg = palette.neutral_yellow },
-			BufferInactive = { bg = palette.bg1, fg = palette.gray },
-			BufferInactiveSign = { bg = palette.bg1, fg = palette.gray },
-		}
-
 		require("gruvbox").setup(opts)
 		vim.cmd({
 			cmd = "colorscheme",
