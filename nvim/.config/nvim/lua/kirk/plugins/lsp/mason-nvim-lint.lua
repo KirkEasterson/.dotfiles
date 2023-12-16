359eedbd13d45e64f115f93bccc69bcd3f6bcede
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-16 11:50:41 +0100

    added mason-nvim-lint

---
+++ nvim/.config/nvim/lua/kirk/plugins/lsp/mason-nvim-lint.lua
@@ -0,0 +1,32 @@
+return {
+	"rshkarin/mason-nvim-lint",
+	version = "*",
+	event = "VeryLazy",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"mfussenegger/nvim-lint",
+	},
+	opts = {
+		ensure_installed = {
+			"actionlint",
+			-- "ansible-lint",
+			"cmakelint",
+			"codespell",
+			-- "commitlint",
+			"cpplint",
+			"editorconfig-checker",
+			"eslint_d",
+			"gitlint",
+			-- "golangci-lint",
+			"jsonlint",
+			"luacheck",
+			"markdownlint",
+			"prettier",
+			"pylint",
+			"shellcheck",
+			-- "stylelint", -- css
+			-- "stylua",
+			"yamllint",
+		},
+	},
+}
