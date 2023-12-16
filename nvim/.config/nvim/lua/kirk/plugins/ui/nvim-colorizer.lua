5ac164c40e5474a6f75993c5a103ed43d09bebb8
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-16 16:01:20 +0100

    changed options for colorizer

--- nvim/.config/nvim/lua/kirk/plugins/ui/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/kirk/plugins/ui/nvim-colorizer.lua
@@ -10,5 +10,16 @@ return {
 		"ColorizerReloadAllBuffers",
 		"ColorizerToggle",
 	},
-	opts = {},
+	opts = {
+		user_default_options = {
+			RGB = true, -- #f0f
+			RRGGBB = true, -- #ff00ff
+			names = false, -- Magenta
+			RRGGBBAA = true, -- #ff00ffff
+			rgb_fn = true, -- rgb(255, 0, 255) and rgba(255, 0, 255, 0.75)
+			hsl_fn = true, -- hsl(300, 100%, 50%) and hsla(300, 100%, 50%, 0.75)
+			mode = "background",
+			always_update = true,
+		},
+	},
 }
