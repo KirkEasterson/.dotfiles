c39d9688829636b035840a97a27321d1b13567fe
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-17 10:58:11 +0100

    cleaned up dap virt text

--- nvim/.config/nvim/lua/kirk/plugins/lsp/dap-virtualtext.lua
+++ nvim/.config/nvim/lua/kirk/plugins/lsp/dap-virtualtext.lua
@@ -1,6 +1,9 @@
 return {
-	'theHamsta/nvim-dap-virtual-text',
+	"theHamsta/nvim-dap-virtual-text",
 	cond = not vim.g.started_by_firenvim,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
 	opts = {
 		commented = true,
 		only_first_definition = false,
