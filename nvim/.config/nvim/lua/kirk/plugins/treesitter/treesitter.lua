a3d73d6eb92947afb5a4e05610c9a04baef2c609
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-18 22:09:58 +0100

    added fsharp parser

--- nvim/.config/nvim/lua/kirk/plugins/treesitter/treesitter.lua
+++ nvim/.config/nvim/lua/kirk/plugins/treesitter/treesitter.lua
@@ -104,4 +104,18 @@ return {
 			"zig",
 		},
 	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.fsharp = {
+			install_info = {
+				url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
+				branch = "main",
+				files = { "src/scanner.cc", "src/parser.c" },
+				generate_requires_npm = true,
+				requires_generate_from_grammar = true,
+			},
+			filetype = "fsharp",
+		}
+	end,
 }
