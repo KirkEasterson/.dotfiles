319fe48b73be99b9bee05bf9e35d2ffe93237f51
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-18 22:10:23 +0100

    added tag closing

--- nvim/.config/nvim/lua/kirk/plugins/util/autopairs.lua
+++ nvim/.config/nvim/lua/kirk/plugins/util/autopairs.lua
@@ -1,6 +1,9 @@
 return {
 	"windwp/nvim-autopairs",
 	cond = not vim.g.started_by_firenvim,
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
 	event = {
 		"InsertEnter",
 		"CmdlineEnter",
@@ -10,15 +13,35 @@ return {
 		enable_check_bracket_line = false,
 		ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
 	},
-	-- config = function(_, opts)
-	-- 	local autopairs = require("nvim-autopairs")
-	-- 	local rule = require("nvim-autopairs.rule")
-	-- 	local ts_conds = require("nvim-autopairs.ts-conds")
-	--
-	-- 	autopairs.setup(opts)
-	-- 	autopairs.add_rules({
-	-- 		rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
-	-- 		rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
-	-- 	})
-	-- end,
+	config = function(_, opts)
+		local autopairs = require("nvim-autopairs")
+		local rule = require("nvim-autopairs.rule")
+		local ts_conds = require("nvim-autopairs.ts-conds")
+
+		autopairs.setup(opts)
+		autopairs.add_rules({
+			rule("<", ">", {
+				"astro",
+				"fsharp",
+				"glimmer",
+				"handlebars",
+				"hbs",
+				"html",
+				"javascript",
+				"javascriptreact",
+				"jsx",
+				"markdown",
+				"ocaml",
+				"php",
+				"rescript",
+				"svelte",
+				"tsx",
+				"typescript",
+				"typescriptreact",
+				"vue",
+				"xml",
+			}),
+			-- }):with_pair(ts_conds.is_ts_node({ "Type" })),
+		})
+	end,
 }
