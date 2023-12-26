0de9ea497eed9649e5777fca7333ebc807404d98
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-26 11:30:20 +0100

    added telescope luasnip

---
+++ nvim/.config/nvim/lua/kirk/plugins/telescope/telescope-luasnip.lua
@@ -0,0 +1,23 @@
+return {
+	"benfowler/telescope-luasnip.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"L3MON4D3/LuaSnip",
+	},
+	keys = {
+		{
+			"<leader>fl",
+			function()
+				require("telescope").extensions.luasnip.luasnip()
+			end,
+			desc = "Telescope luasnip",
+		},
+	},
+	opts = {
+		extensions = {},
+	},
+	config = function(_, opts)
+		require("telescope").setup(opts)
+		require("telescope").load_extension("luasnip")
+	end,
+}
