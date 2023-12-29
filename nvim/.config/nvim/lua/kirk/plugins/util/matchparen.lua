548c1498d21ceade3631ae574e7fd552de5d4d85
Kirk Easterson <kirk.easterson@gmail.com>
2023-12-29 03:34:15 +0100

    added config for match paren

---
+++ nvim/.config/nvim/lua/kirk/plugins/util/matchparen.lua
@@ -0,0 +1,12 @@
+return {
+	"monkoose/matchparen.nvim",
+	event = "VimEnter",
+	cmd = {
+		"MatchParenDisable",
+		"MatchParenEnable",
+	},
+	opts = {},
+	init = function()
+		vim.g.loaded_matchparen = 1
+	end,
+}
