ff351947d0c5de4abfc2b560cfe77fe6b4e990cd
Kirk Easterson <kirk.easterson@gmail.com>
2024-01-02 01:13:10 +0100

    restored regular cell size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -58,9 +58,9 @@ return {
 		"ComicCodeLigatures Nerd Font",
 		"JetBrains Mono Nerd Font",
 	}),
-	font_size = 11,
-	line_height = 0.89,
-	cell_width = 0.95,
+	font_size = 14,
+	-- line_height = 0.89,
+	-- cell_width = 0.95,
 	freetype_load_target = "Normal",
 	-- warn_about_missing_glyphs = false, -- TODO: find which glyphs are missing
 	underline_position = -3,
