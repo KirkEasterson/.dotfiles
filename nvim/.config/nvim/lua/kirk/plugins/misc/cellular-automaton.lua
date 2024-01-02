069b2f038ff339c12daafc9f77350bb2c09dfdd1
Kirk Easterson <kirk.easterson@gmail.com>
2024-01-02 01:12:41 +0100

    formatting

--- nvim/.config/nvim/lua/kirk/plugins/misc/cellular-automaton.lua
+++ nvim/.config/nvim/lua/kirk/plugins/misc/cellular-automaton.lua
@@ -1,29 +1,31 @@
 return {
 	"eandrju/cellular-automaton.nvim",
-	cmd = 'CellularAutomaton',
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+	},
+	cmd = "CellularAutomaton",
 	keys = {
 		{
 			"<leader>fml",
-			function() require("cellular-automaton").start_animation("make_it_rain") end,
+			function()
+				require("cellular-automaton").start_animation("make_it_rain")
+			end,
 			desc = "Make it rain",
 		},
 	},
 	config = function()
-		local config = {
-			fps = 50,
-			name = 'snake',
-		}
-
-		config.update = function(grid)
-			for i = 1, #grid do
-				local prev = grid[i][#(grid[i])]
-				for j = 1, #(grid[i]) do
-					grid[i][j], prev = prev, grid[i][j]
+		require("cellular-automaton").register_animation({
+			name = "slide",
+			fps = 60,
+			update = function(grid)
+				for i = 1, #grid do
+					local prev = grid[i][#grid[i]]
+					for j = 1, #grid[i] do
+						grid[i][j], prev = prev, grid[i][j]
+					end
 				end
-			end
-			return true
-		end
-
-		require("cellular-automaton").register_animation(config)
+				return true
+			end,
+		})
 	end,
 }
