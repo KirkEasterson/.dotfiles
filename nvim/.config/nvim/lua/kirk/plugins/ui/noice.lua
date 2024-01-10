35861ba2494ad874b506bbd9761e3fe5825796ef
Kirk Easterson <kirk.easterson@gmail.com>
2024-01-10 22:57:42 +0100

    added cmp documentation to noice

--- nvim/.config/nvim/lua/kirk/plugins/ui/noice.lua
+++ nvim/.config/nvim/lua/kirk/plugins/ui/noice.lua
@@ -19,6 +19,7 @@ return {
     },
     lsp = {
       override = {
+        ["cmp.entry.get_documentation"] = true,
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
       },
