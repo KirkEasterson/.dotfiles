c34c7a0deb568e13f5f15da070083fbf4b891bdc
Kirk Easterson <kirk.easterson@gmail.com>
2024-01-26 18:40:41 +0100

    add more options to nvim tree

--- nvim/.config/nvim/lua/kirk/plugins/ui/nvim-tree.lua
+++ nvim/.config/nvim/lua/kirk/plugins/ui/nvim-tree.lua
@@ -31,9 +31,11 @@ return {
   opts = {
     sort_by = "case_sensitive",
     sync_root_with_cwd = true,
+    respect_buf_cwd = true,
     disable_netrw = true,
     update_focused_file = {
       enable = true,
+      update_root = true,
     },
     view = {
       adaptive_size = false,
