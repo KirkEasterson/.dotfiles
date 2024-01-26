d7dcda46610e7ad6002cad6a3e31a2aac3ce705b
Kirk Easterson <kirk.easterson@gmail.com>
2024-01-26 18:46:02 +0100

    added TODO for install

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO: overwrite already existing items (or rename to *.old)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 stow -S */
