e3002df8bed405db3e55e0a6340a169abb1336f8
Kirk Easterson <kirk.easterson@gmail.com>
2024-02-04 10:13:25 +0100

    fixed mkdir typo for sqlite

--- nvim/.config/nvim/lua/kirk/plugins/util/sqlite.lua
+++ nvim/.config/nvim/lua/kirk/plugins/util/sqlite.lua
@@ -1,6 +1,6 @@
 return {
   "kkharji/sqlite.lua",
   config = function(_, opts)
-    os.execute("mkdir -p" .. vim.fn.stdpath("data") .. "/databases 2>&1 /dev/null")
+    os.execute("mkdir -p " .. vim.fn.stdpath("data") .. "/databases")
   end,
 }
