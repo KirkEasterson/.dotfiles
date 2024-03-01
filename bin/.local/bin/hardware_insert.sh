ebaf91f31e95a0f6494d18f97de9276c1a58f1a2
Kirk Easterson <kirk.easterson@gmail.com>
2024-03-01 10:56:13 +0100

    clean up hardware_insert.sh

--- bin/.local/bin/hardware_insert.sh
+++ bin/.local/bin/hardware_insert.sh
@@ -1,37 +1,39 @@
 #!/bin/sh
 
 # https://ubuntuforums.org/showthread.php?t=380002
-
+#
 # USB Insert / Remove sound script
 # Created by om3ganet (msn@om3ga.net)
-# --------------------------------------------------
-# This script could probably be cleaned up, and add checking
-# of unrecognised usb sounds (and play fail noise)
 
-insert_sound () {
-    pw-play /usr/share/sounds/freedesktop/stereo/device-added.oga
-}
+sound () {
+  file=$(
+  case ${1} in
+    ("insert")
+      echo "device-added.oga" ;;
+    ("remove")
+      echo "device-removed.oga" ;;
+    (*)
+      return ;;
+  esac)
 
-remove_sound () {
-    pw-play /usr/share/sounds/freedesktop/stereo/device-removed.oga
+    pw-play "/usr/share/sounds/freedesktop/stereo/${file}"
 }
 
 # check frequency (default 0.25 seconds)
 CHECKFREQ=0.25
 
-
-# Do not edit below this line
-# --------------------------------------------------
 DEVCOUNT=$(lsusb | wc -l | awk '{ print $1 }')
 OLDDEVCOUNT=$DEVCOUNT;
 
 while(true) do
   sleep $CHECKFREQ;
+
   DEVCOUNT=$(lsusb | wc -l | awk '{ print $1 }')
-  if [ $DEVCOUNT -gt $OLDDEVCOUNT ]; then
-    insert_sound
-  else if [ $DEVCOUNT -lt $OLDDEVCOUNT ]; then
-    remove_sound
-  fi fi
+  if [ "${DEVCOUNT}" -gt "${OLDDEVCOUNT}" ]; then
+    sound "insert"
+  elif [ "${DEVCOUNT}" -lt "${OLDDEVCOUNT}" ]; then
+    sound "remove"
+  fi
+
   OLDDEVCOUNT=$DEVCOUNT;
 done
