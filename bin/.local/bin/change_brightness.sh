c8e17b675454eadb47da5f91e5e4c3b8cddf68cf
Kirk Easterson <kirk.easterson@gmail.com>
2024-03-22 07:27:54 +0100

    add vol and brightness script

---
+++ bin/.local/bin/change_brightness.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+raise_brightness () {
+  brightnessctl --quiet set 5%+ -n 10
+}
+
+lower_brightness () {
+  brightnessctl --quiet set 5%- -n 10
+}
+
+get_brightness () {
+  brightnessctl i | sed -n 's/.*Current brightness: [0-9]* (\([0-9]*\).*/\1/p'
+}
+
+get_icon () {
+    echo "video-display"
+}
+
+notify_user () {
+  brightness=$(get_brightness)
+  icon=$(get_icon "$brightness")
+
+  notify-send "Brightness" \
+    -u "low" \
+    -i "$icon" \
+    -h "int:value:${brightness}" \
+    -h "string:x-canonical-private-synchronous:sys-notify"
+}
+
+case "${1}" in
+  ("raise")
+    raise_brightness
+    ;;
+  ("lower")
+    lower_brightness
+    ;;
+  (*)
+    exit 0
+esac
+
+notify_user
---
+++ bin/.local/bin/change_vol.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+vol_diff="10"
+
+raise_vol () {
+  wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ ${vol_diff}%+
+}
+
+lower_vol () {
+  wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ ${vol_diff}%-
+}
+
+toggle_mute () {
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+}
+
+toggle_mic_mute () {
+  wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+}
+
+get_volume () {
+  # TODO: optimize this. it shouldn't be hard to convert a decimal to a percent
+  vol_float=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2}')
+  vol_percent=$(echo "$vol_float*100" | bc)
+  vol_int=${vol_percent%.*}
+  echo "${vol_int}"
+}
+
+is_muted () {
+  return "$(wpctl get-volume @DEFAULT_AUDIO_SINK@ |  grep -v -c "MUTED")"
+}
+
+get_icon () {
+  if is_muted; then
+    echo "audio-volume-muted"
+    exit
+  fi
+
+  case $(printf "%02d" "$1") in
+    [0-2][0-9]) echo "audio-volume-low" ;;
+    [3-7][0-9]) echo "audio-volume-medium" ;;
+    *) echo "audio-volume-high" ;;
+  esac
+}
+
+notify_user () {
+  # if ($1 ==  "mic") {
+  #   something else
+  # }
+
+  volume=$(get_volume)
+  icon=$(get_icon "$volume")
+
+  notify-send "Volume" \
+    -u "low" \
+    -i "$icon" \
+    -h "int:value:${volume}" \
+    -h "string:x-canonical-private-synchronous:sys-notify"
+}
+
+case "${1}" in
+  ("raise")
+    raise_vol
+    ;;
+  ("lower")
+    lower_vol
+    ;;
+  ("toggle-mute")
+    toggle_mute
+    ;;
+  ("toggle-mic-mute")
+    toggle_mic_mute
+    ;;
+  (*)
+    exit 0
+esac
+
+notify_user
