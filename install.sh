20c62fc0db6d9a585f663364b4912b3044d09cd3
Kirk Easterson <kirk.easterson@gmail.com>
2024-04-23 22:54:12 +0200

    refactor(install): add quotes to args

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 # TODO: overwrite already existing items (or rename to *.old)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-stow -S */
-cd -
+cd "${SCRIPT_DIR}" || exit
+stow -S "*/"
+cd - || exit
