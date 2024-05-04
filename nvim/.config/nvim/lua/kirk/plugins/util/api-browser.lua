017e138b90e01aa1ef7b2b40d0dc71e2fa45db55
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-05 01:25:55 +0200

    feat(nvim): add api-browser plugin

---
+++ nvim/.config/nvim/lua/kirk/plugins/util/api-browser.lua
@@ -0,0 +1,11 @@
+return {
+  "tlj/api-browser.nvim",
+  enabled = false,
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = "ApiBrowser",
+  config = {},
+}
