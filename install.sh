b030a1640c47c27adceab0d0849476b43839b376
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-07 20:23:38 +0200

    fix(install): fix broken stow command

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 # TODO: overwrite already existing items (or rename to *.old)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit
-stow -S "*/"
+stow -S */ --adopt
 cd - || exit
