ffc5a08f96241ec503c5f36db46b648e5b04d38e
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-11 18:18:47 +0200

    feat(nvim): test of stylua

--- nvim/.config/nvim/lua/kirk/plugins/theme/gruvbox.lua
+++ nvim/.config/nvim/lua/kirk/plugins/theme/gruvbox.lua
@@ -2,7 +2,9 @@ return {
   "ellisonleao/gruvbox.nvim",
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    contrast = "hard,
+  },
   config = function (_, opts)
     require("gruvbox").setup(opts)
     vim.cmd({
