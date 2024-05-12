47de5f2e8874492016946fa8f27476dcb09bf429
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-12 21:59:58 +0200

    feat(install): test failing install script

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ set -e
 # TODO: overwrite already existing items (or rename to *.old)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit
-stow -S */ --adopt
+stow -S */
 cd - || exit
