4b263802955fa4cec3f7e94cbd3f97bc5d05f0ad
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-20 10:08:36 +0200

    feat(nvim): slight update to barbar

--- nvim/.config/nvim/lua/kirk/plugins/ui/barbar.lua
+++ nvim/.config/nvim/lua/kirk/plugins/ui/barbar.lua
@@ -3,22 +3,36 @@ return {
   enabled = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+    "lewis6991/gitsigns.nvim",
+  },
+  event = {
+    "BufNewFile",
+    "BufReadPost",
+  },
+  cmd = {
+    "BufferOrderByBufferNumber",
+    "BufferOrderByDirectory",
+    "BufferOrderByLanguage",
+    "BufferOrderByName",
+    "BufferOrderByWindowNumber",
   },
   opts = {
     animation = false,
-    auto_hide = true,
-    tabpages = false,
+    -- auto_hide = true,
     maximum_padding = 1,
     minimum_padding = 1,
   },
-  config = function(_, opts)
-    local palette = require("gruvbox").palette
-
-    vim.cmd("highlight BufferCurrent guibg=" .. palette.bg0 .. " guifg=none")
-    vim.cmd("highlight BufferCurrentSign guibg=" .. palette.bg0 .. " guifg=none")
-    vim.cmd("highlight BufferVisible guibg=" .. palette.bg0 .. " guifg=" .. palette.neutral_yellow)
-    vim.cmd("highlight BufferVisibleSign guibg=" .. palette.bg0 .. " guifg=" .. palette.neutral_yellow)
-    vim.cmd("highlight BufferInactive guibg=" .. palette.bg0 .. " guifg=" .. palette.gray)
-    vim.cmd("highlight BufferInactiveSign guibg=" .. palette.bg0 .. " guifg=" .. palette.gray)
+  init = function()
+    vim.g.barbar_auto_setup = false
   end,
+  -- config = function(_, opts)
+  --   local palette = require("gruvbox").palette
+  --
+  --   vim.cmd("highlight BufferCurrent guibg=" .. palette.bg0 .. " guifg=none")
+  --   vim.cmd("highlight BufferCurrentSign guibg=" .. palette.bg0 .. " guifg=none")
+  --   vim.cmd("highlight BufferVisible guibg=" .. palette.bg0 .. " guifg=" .. palette.neutral_yellow)
+  --   vim.cmd("highlight BufferVisibleSign guibg=" .. palette.bg0 .. " guifg=" .. palette.neutral_yellow)
+  --   vim.cmd("highlight BufferInactive guibg=" .. palette.bg0 .. " guifg=" .. palette.gray)
+  --   vim.cmd("highlight BufferInactiveSign guibg=" .. palette.bg0 .. " guifg=" .. palette.gray)
+  -- end,
 }
