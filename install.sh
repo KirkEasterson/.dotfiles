b19503abba2b754aee4f2af90fa29497556ee879
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-20 19:16:16 +0000

    fix(install): remove old comment

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# TODO: overwrite already existing items (or rename to *.old)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit
 stow -S */ --adopt
