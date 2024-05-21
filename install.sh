be9d8d75155c4ee3b6260e6e26c2b8dab9a847a3
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-21 17:41:44 +0000

    feat(firefox): first draft of adding firefox config

--- install.sh
+++ install.sh
@@ -4,4 +4,20 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit
 stow -S */ --adopt
-cd - || exit
+
+if [ ! -d "${HOME}/.mozilla/firefox" ]
+then
+	firefox -headless 2>/dev/null & disown
+	sleep 1
+	killall firefox
+fi
+
+FIREFOX_DIR=$(find ${HOME}/.mozilla/firefox/ -type d -regex ".*default-release$")
+if [ ! -d "${FIREFOX_DIR}" ]
+then
+	echo "oh no again"
+fi
+
+stow -S firefox --target="${FIREFOX_DIR}"
+
+cd - > /dev/null || exit
