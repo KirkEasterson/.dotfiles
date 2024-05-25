ff5bea39746b9c2d593d85c07c85462b993bd522
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-25 08:43:04 +0200

    feat(install): add print info

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
+echo "Installing dotfiles"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}" || exit
 stow -S */ --adopt
 
 # firefox files need specific installation
 if [ -x "firefox"]; then
+	echo "Installing firefox config"
+
 	# create firefox dir if it doesn't exists
 	if [ ! -d "${HOME}/.mozilla/firefox" ]; then
 		firefox -headless 2>/dev/null & disown
