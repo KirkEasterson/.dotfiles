734840c73ee794415bf185c78e43aca02be7d211
Kirk Easterson <kirk.easterson@gmail.com>
2024-05-25 08:49:12 +0200

    fix(install): don't run headless firefox

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ -x "firefox" ]; then
 
 	# create firefox dir if it doesn't exists
 	if [ ! -d "${HOME}/.mozilla/firefox" ]; then
-		firefox -headless 2>/dev/null & disown
+		firefox 2>/dev/null & disown
 		sleep 5
 		killall firefox
 	fi
