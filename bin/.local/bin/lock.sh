08349b2bb9141d058353a564bb81ae2c22c9340f
Kirk Easterson <kirk.easterson@gmail.com>
2024-07-07 11:20:05 +0200

    fix(lock): clean up xorg locking script

--- bin/.local/bin/lock.sh
+++ bin/.local/bin/lock.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-LOCK_IMG=${XDG_CONFIG_HOME}/lock.png
-SCREEN_RESOLUTION="$(xdpyinfo | grep dimensions | cut -d' ' -f7)"
-BGCOLOR="#000000"
-# convert "$XDG_CONFIG_HOME/lock.png" -gravity Center -extent "$SCREEN_RESOLUTION" RGB:- | i3lock --ignore-empty-password --show-failed-attempts --beep --raw "$SCREEN_RESOLUTION":rgb -c 000000 -i /dev/stdin
-
 if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
-	convert $LOCK_IMG -gravity Center -background \
-		$BGCOLOR -extent "$SCREEN_RESOLUTION" RGB:- \
-		| i3lock --ignore-empty-password --show-failed-attempts --beep --nofork \
-		--raw "$SCREEN_RESOLUTION":rgb -c $BGCOLOR -i /dev/stdin
+	LOCK_IMG="${XDG_CONFIG_HOME}/lock.png"
+	SCREEN_RESOLUTION="$(xdpyinfo | grep dimensions | cut -d' ' -f7)"
+	BGCOLOR="#000000"
+
+	magick "$LOCK_IMG" \
+		-gravity Center \
+		-background "$BGCOLOR" \
+		-resize "$SCREEN_RESOLUTION"! RGB:- \
+	| i3lock \
+		--ignore-empty-password \
+		--nofork \
+		--beep \
+		--raw "$SCREEN_RESOLUTION":rgb \
+		--image="/dev/stdin" \
+		--color="#000000" \
+		--tiling \
+		--show-failed-attempts \
+		--show-keyboard-layout
 else
 	swaylock
 fi
-
