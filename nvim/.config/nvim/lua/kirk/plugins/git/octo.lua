94b8573c11d07c2ad8cdf17a44a096f5310c42a4
Kirk Easterson <kirk.easterson@gmail.com>
2024-07-09 09:13:57 +0200

    feat(nvim): add gruvbox colors for octo

--- nvim/.config/nvim/lua/kirk/plugins/git/octo.lua
+++ nvim/.config/nvim/lua/kirk/plugins/git/octo.lua
@@ -5,9 +5,36 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
     "nvim-tree/nvim-web-devicons",
+
+    "ellisonleao/gruvbox.nvim",
   },
   init = function()
     vim.treesitter.language.register("markdown", "octo")
   end,
-  opts = {},
+  config = function(_, _)
+    local gruvbox = require("gruvbox")
+    local opts = {
+      default_merge_method = "squash",
+      colors = {
+        white = gruvbox.palette.light0,
+        grey = gruvbox.palette.gray,
+        black = gruvbox.palette.dark0,
+
+        red = gruvbox.palette.light_red_hard,
+        dark_red = gruvbox.palette.dark_red,
+
+        green = gruvbox.palette.light_green_hard,
+        dark_green = gruvbox.palette.dark_green_hard,
+
+        yellow = gruvbox.palette.bright_yellow,
+        dark_yellow = gruvbox.palette.faded_yellow,
+
+        blue = gruvbox.palette.bright_blue,
+        dark_blue = gruvbox.palette.faded_blue,
+
+        purple = gruvbox.palette.bright_purple,
+      },
+    }
+    require("octo").setup(opts)
+  end,
 }
