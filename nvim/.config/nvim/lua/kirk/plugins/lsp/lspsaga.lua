7675b1aba8c60f69905974cc1f241f522d1cde27
Kirk Easterson <kirk.easterson@gmail.com>
2024-07-11 19:28:45 +0200

    feat(nvim): re-enable lspsaga

--- nvim/.config/nvim/lua/kirk/plugins/lsp/lspsaga.lua
+++ nvim/.config/nvim/lua/kirk/plugins/lsp/lspsaga.lua
@@ -1,11 +1,11 @@
 return {
   "nvimdev/lspsaga.nvim",
-  enabled = false,
   dependencies = {
     "VonHeikemen/lsp-zero.nvim",
     "nvim-tree/nvim-web-devicons",
     "nvim-treesitter/nvim-treesitter",
   },
+  event = "LspAttach",
   cmd = "Lspsaga",
   keys = {
     {
@@ -13,70 +13,22 @@ return {
       "<cmd>Lspsaga outline<CR>",
       desc = "Code outline",
     },
-    {
-      "<leader>ga",
-      "<cmd>Lspsaga code_action<CR>",
-      desc = "Code action",
-    },
-    {
-      "<leader>e",
-      "<cmd>Lspsaga show_line_diagnostics<CR>",
-      desc = "Line diagnostics",
-    },
-    {
-      "<leader>gu",
-      "<cmd>Lspsaga finder<CR>",
-      desc = "LSP finder",
-    },
-    {
-      "<leader>rn",
-      "<cmd>Lspsaga rename<CR>",
-      desc = "Rename",
-    },
-    {
-      "K",
-      "<cmd>Lspsaga hover_doc<CR>",
-      desc = "Hover docs",
-    },
-    {
-      "[d",
-      "<cmd>Lspsaga diagnostic_jump_prev<CR>",
-      desc = "Previous diagnostic",
-    },
-    {
-      "]d",
-      "<cmd>Lspsaga diagnostic_jump_next<CR>",
-      desc = "Next diagnostic",
-    },
-    {
-      "gb",
-      "<cmd>Lspsaga show_buf_diagnostics<CR>",
-      desc = "Buffer diagnostics",
-    },
-    {
-      "gd",
-      "<cmd>Lspsaga goto_definition<CR>",
-      desc = "Definition",
-    },
-    {
-      "gt",
-      "<cmd>Lspsaga goto_type_definition<CR>",
-      desc = "Type definition",
-    },
   },
   opts = {
     ui = {
       border = "rounded",
+      title = true,
     },
     rename = {
       auto_save = true,
     },
     lightbulb = {
+      enable = false,
       virtual_text = false,
     },
     symbol_in_winbar = {
-      enable = false, -- disabled to avoid having this plugin load at startup
-      separator = "  ",
+      enable = false,
+      separator = "/ ",
       folder_level = 8,
       respect_root = true,
       delay = 10,
