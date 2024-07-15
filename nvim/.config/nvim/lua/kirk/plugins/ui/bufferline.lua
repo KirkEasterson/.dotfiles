ebfd95be820f7f21da97e24917b5413cf93d28a7
Kirk Easterson <kirk.easterson@gmail.com>
2024-07-15 07:47:05 +0000

    feat(nvim): add whichkey bindings for changing buffers

--- nvim/.config/nvim/lua/kirk/plugins/ui/bufferline.lua
+++ nvim/.config/nvim/lua/kirk/plugins/ui/bufferline.lua
@@ -4,11 +4,11 @@ return {
     -- 'neovim/nvim-lspconfig',
     "nvim-tree/nvim-web-devicons",
   },
+  lazy = false,
   event = {
     "BufNewFile",
     "BufReadPost",
   },
-  branch = "main", -- TODO: remove this when new version is released
   keys = {
     {
       "<C-S-Tab>",
@@ -24,74 +24,6 @@ return {
       end,
       desc = "Go to next buffer",
     },
-    {
-      "<C-1>",
-      function()
-        require("bufferline").go_to_buffer(1, true)
-      end,
-      desc = "Go to buffer 1",
-    },
-    {
-      "<C-2>",
-      function()
-        require("bufferline").go_to_buffer(2, true)
-      end,
-      desc = "Go to buffer 2",
-    },
-    {
-      "<C-3>",
-      function()
-        require("bufferline").go_to_buffer(3, true)
-      end,
-      desc = "Go to buffer 3",
-    },
-    {
-      "<C-4>",
-      function()
-        require("bufferline").go_to_buffer(4, true)
-      end,
-      desc = "Go to buffer 4",
-    },
-    {
-      "<C-5>",
-      function()
-        require("bufferline").go_to_buffer(5, true)
-      end,
-      desc = "Go to buffer 5",
-    },
-    {
-      "<C-6>",
-      function()
-        require("bufferline").go_to_buffer(6, true)
-      end,
-      desc = "Go to buffer 6",
-    },
-    {
-      "<C-7>",
-      function()
-        require("bufferline").go_to_buffer(7, true)
-      end,
-      desc = "Go to buffer 7",
-    },
-    {
-      "<C-8>",
-      function()
-        require("bufferline").go_to_buffer(8, true)
-      end,
-      desc = "Go to buffer 8",
-    },
-    {
-      "<C-9>",
-      function()
-        require("bufferline").go_to_buffer(9, true)
-      end,
-      desc = "Go to buffer 9",
-    },
-    -- { -- conflicts with resetting font size in terminals
-    -- 	'<C-0>',
-    -- 	function() require('bufferline').go_to_buffer(10, true) end,
-    -- 	desc = "Go to buffer 10",
-    -- },
     {
       "<C-<>",
       function()
