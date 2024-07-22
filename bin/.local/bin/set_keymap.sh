703df83b37d332bee4b9ed886f02b72fc88abe46
Kirk Easterson <kirk.easterson@gmail.com>
2024-07-22 12:00:51 -0400

    feat(scripts): extens change_keymap to work on different DEs

--- bin/.local/bin/set_keymap.sh
+++ bin/.local/bin/set_keymap.sh
@@ -5,7 +5,8 @@
 
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
 	# TODO: filter on different compositors
-	swaymsg input type:keyboard xkb_layout "${1}"
+	swaymsg input type:keyboard xkb_layout "$1" || true
+	riverctl keyboard-layout -options "caps:escape" "$1" || true
 else
 	setxkbmap "${1}"
 fi
