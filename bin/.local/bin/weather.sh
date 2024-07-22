e6cad84020679e4a7db35a4734f52152b669fe96
Kirk Easterson <kirk.easterson@gmail.com>
2024-07-22 14:29:05 -0400

    feat(scripts): add error handling to weather.sh

--- bin/.local/bin/weather.sh
+++ bin/.local/bin/weather.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-curl -s 'https://wttr.in/?format=%c%t&m'
+url="https://wttr.in/?format=%c%t&m"
+resp=$(wget --content-on-error --server-response -qO- "$url" 2>&1)
+
+status=$(echo "$resp" | head -n 1 | awk -F" " '{print $2}' )
+content=$(echo "$resp" | tail -n 1)
+
+case "$status" in
+	("200")
+		if [[ "$content" == *"Unknown location"* ]]; then
+			echo ""
+		else
+			echo "$content"
+		fi
+		;;
+	(*)
+		echo ""
+esac
