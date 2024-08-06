e51db120bd24ff7c7ab8cea08beeb4ba8e70c9f9
Kirk Easterson <kirk.easterson@gmail.com>
2024-08-06 04:32:51 +0200

    feat(river): add specififc input configs

--- river/.config/river/modules/06-input.sh
+++ river/.config/river/modules/06-input.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+################################
+# All inputs
+################################
+
 riverctl set-repeat 30 200
 riverctl keyboard-layout -options "caps:escape" "no"
 
@@ -10,3 +14,15 @@ riverctl input pointer-* middle-emulation enabled
 riverctl input pointer-* natural-scroll enabled
 riverctl input pointer-* tap enabled
 riverctl input pointer-* scroll-method two-finger
+
+################################
+# Specific inputs
+################################
+
+riverctl input "*-Kensington_Orbit_Fusion_Wireless_Trackball" scroll-button BTN_MIDDLE
+riverctl input "*-Kensington_Orbit_Fusion_Wireless_Trackball" scroll-method button
+riverctl input "*-Kensington_Orbit_Fusion_Wireless_Trackball" scroll-factor 0.4
+
+riverctl input "*-Getech_HUGE_TrackBall" scroll-button BTN_TASK
+riverctl input "*-Getech_HUGE_TrackBall" scroll-method button
+riverctl input "*-Getech_HUGE_TrackBall" scroll-factor 0.4
