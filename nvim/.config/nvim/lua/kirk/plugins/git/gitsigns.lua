fb540690c87480d6df73c41b89e789053860a371
Kirk Easterson <kirk.easterson@gmail.com>
2024-08-18 09:30:01 +0200

    feat(nvim): remove invalid feields from gitsigns

--- nvim/.config/nvim/lua/kirk/plugins/git/gitsigns.lua
+++ nvim/.config/nvim/lua/kirk/plugins/git/gitsigns.lua
@@ -45,15 +45,6 @@ return {
       topdelete = { text = "┃" },
       untracked = { text = "┃" },
     },
-    signs_staged = {
-      add = { text = "┃" },
-      change = { text = "┃" },
-      changedelete = { text = "┃" },
-      delete = { text = "┃" },
-      topdelete = { text = "┃" },
-      untracked = { text = "┃" },
-    },
-
     current_line_blame = false,
     current_line_blame_formatter = " 󰊢 <author>  <author_time:%d %b %Y>  <summary>",
     current_line_blame_opts = {
