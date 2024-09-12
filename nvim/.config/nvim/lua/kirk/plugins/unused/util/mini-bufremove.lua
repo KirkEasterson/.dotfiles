40a3bebfd416dfc5c3892f71be3ff5ae479ac1e4
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-12 15:54:42 +0200

    feat(nvim): move mini-bufremove to unused

---
+++ nvim/.config/nvim/lua/kirk/plugins/unused/util/mini-bufremove.lua
@@ -0,0 +1,14 @@
+return {
+  "echasnovski/mini.bufremove",
+  enabled = false, -- TODO: replace with a simple binding
+  keys = {
+    {
+      "<C-w>", -- this is causing a delay from window choosing binding
+      function()
+        require("mini.bufremove").delete()
+      end,
+      desc = "Delete buffer",
+    },
+  },
+  opts = {},
+}
