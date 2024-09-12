88bd8bac6625c71cd6c314f7ad71902951ae11e2
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-12 21:22:45 +0200

    feat(qtile): add wayland check for autostart

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-"${XDG_CONFIG_HOME}/polybar/launch.sh" &
-"${HOME}/.fehbg" &
+if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+	"${XDG_CONFIG_HOME}/polybar/launch.sh" &
+	"${HOME}/.fehbg" &
+else
+	"${XDG_CONFIG_HOME}/waybar/start.sh" &
+	swaybg --image "${XDG_CONFIG_HOME}/wall.png"
+fi
--- qtile/.config/qtile/autostart_once.sh
+++ qtile/.config/qtile/autostart_once.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+else
+fi
