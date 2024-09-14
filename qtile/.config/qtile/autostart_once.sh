e6f68f17f69ba2e72860263e6c143043fbb60a6b
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-14 16:14:20 +0200

    feat(qtile): autostart some programs

--- qtile/.config/qtile/autostart_once.sh
+++ qtile/.config/qtile/autostart_once.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+	"${HOME}/.fehbg" &
 else
+	dunst &
+	systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
+	hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=river &
+	/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+	swaybg --image "${XDG_CONFIG_HOME}/wall.png" &
 fi
