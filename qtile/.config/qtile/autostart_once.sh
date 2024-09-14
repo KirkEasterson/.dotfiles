6dde2ed741a3814e02b70f7bf21dc25b80a8cffe
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-14 19:05:26 +0200

    fix(qtile): set qtile and default desktop

--- qtile/.config/qtile/autostart_once.sh
+++ qtile/.config/qtile/autostart_once.sh
@@ -5,7 +5,7 @@ if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
 else
 	dunst &
 	systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP &
-	hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=river &
+	hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=qtile &
 	/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 	swaybg --image "${XDG_CONFIG_HOME}/wall.png" &
 fi
