5096b01c2e7f6c4fe77e22ccee84649f804082e4
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-15 12:26:44 +0200

    feat(river): start bar and background earlier

--- river/.config/river/modules/08-exec-bg.sh
+++ river/.config/river/modules/08-exec-bg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+riverctl spawn "swaybg --image ${XDG_CONFIG_HOME}/wall.png"
+riverctl spawn "${XDG_CONFIG_HOME}/waybar/start.sh"
 riverctl spawn "systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
 riverctl spawn "hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=river"
 riverctl spawn "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
-riverctl spawn "${XDG_CONFIG_HOME}/waybar/start.sh"
-riverctl spawn "swaybg --image ${XDG_CONFIG_HOME}/wall.png"
