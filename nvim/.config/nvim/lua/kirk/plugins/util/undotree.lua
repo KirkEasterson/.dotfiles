e79b15acb7b8683917e17e07f9b17078366f9915
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-19 12:50:12 +0200

    feat(nvim): re-enable undotree

--- nvim/.config/nvim/lua/kirk/plugins/util/undotree.lua
+++ nvim/.config/nvim/lua/kirk/plugins/util/undotree.lua
@@ -1,6 +1,5 @@
 return {
   "mbbill/undotree",
-  enabled = false,
   cmd = {
     "UndotreeFocus",
     "UndotreeHide",
@@ -16,4 +15,11 @@ return {
       desc = "Undo tree",
     },
   },
+  init = function()
+    vim.g.undotree_ShortIndicators = 1
+    vim.g.undotree_SetFocusWhenToggle = 1
+    vim.g.undotree_TreeNodeShape = ""
+    vim.g.undotree_TreeSplitShape = "/"
+    vim.g.undotree_HelpLine = 0
+  end,
 }
