4eedbb3242e8dc6e4b635e84bc6d6955ae8414ea
Kirk Easterson <kirk.easterson@gmail.com>
2024-09-25 09:48:56 +0200

    feat(shell): create var for xkb options

--- bin/.local/bin/set_keymap.sh
+++ bin/.local/bin/set_keymap.sh
@@ -3,19 +3,21 @@
 # This script serves as a wrapper for setting keyboard layouts in different
 # desktop environments.
 
+xkb_options="caps:escape"
+
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
 	case $XDG_CURRENT_DESKTOP in
 	sway)
 		swaymsg input type:keyboard xkb_layout "$1"
 		;;
 	river)
-		riverctl keyboard-layout -options "caps:escape" "$1"
+		riverctl keyboard-layout -options "${xkb_options}" "$1"
 		;;
 	qtile)
-		qtile cmd-obj -o core -f set_keymap -a "$1" "caps:escape"
+		qtile cmd-obj -o core -f set_keymap -a "$1" "${xkb_options}"
 		;;
 	esac
 
 else
-	setxkbmap -option "caps:escape" "$1"
+	setxkbmap -option "${xkb_options}" "$1"
 fi
