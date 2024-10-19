bced705f3127d7bad508d15cf74d107a720d1168
Kirk Easterson <kirk.easterson@gmail.com>
2024-10-19 17:57:03 +0200

    feat(bin): make weather.sh POSIX compliant

--- bin/.local/bin/weather.sh
+++ bin/.local/bin/weather.sh
@@ -3,17 +3,21 @@
 url="https://wttr.in/?format=%c%t&m"
 resp=$(wget --content-on-error --server-response -qO- "$url" 2>&1)
 
-status=$(echo "$resp" | head -n 1 | awk -F" " '{print $2}' )
+status=$(echo "$resp" | head -n 1 | awk -F" " '{print $2}')
 content=$(echo "$resp" | tail -n 1)
 
 case "$status" in
-	("200")
-		if [[ "$content" == *"Unknown location"* ]]; then
-			echo ""
-		else
-			echo "$content"
-		fi
-		;;
-	(*)
+"200")
+	case "$content" in
+	*"Unknown location"*)
 		echo ""
+		;;
+	*)
+		echo "$content"
+		;;
+	esac
+	;;
+*)
+	echo ""
+	;;
 esac
