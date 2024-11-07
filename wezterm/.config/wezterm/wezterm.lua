0f6af4f30e0093bfefdf1e76e0f8ea54264ae1c6
Kirk Easterson <kirk.easterson@gmail.com>
2024-11-07 23:35:12 +0100

    feat(wezterm): remove unnecessary key bindings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -138,10 +138,6 @@ return {
   },
   disable_default_key_bindings = true,
   keys = {
-    -- basic tab management
-    { key = "t", mods = "SHIFT|CTRL", action = act.SpawnTab("CurrentPaneDomain") },
-    { key = "w", mods = "SHIFT|CTRL", action = act.CloseCurrentTab({ confirm = true }) },
-
     -- change font size
     { key = "+", mods = "CTRL", action = act.IncreaseFontSize },
     { key = "+", mods = "SHIFT|CTRL", action = act.IncreaseFontSize },
@@ -152,7 +148,6 @@ return {
     { key = "_", mods = "CTRL", action = act.DecreaseFontSize },
     { key = "_", mods = "SHIFT|CTRL", action = act.DecreaseFontSize },
     { key = "0", mods = "CTRL", action = act.ResetFontSize },
-    { key = "0", mods = "ALT", action = act.ResetFontSize },
 
     -- copy/paste
     { key = "Copy", mods = "NONE", action = act.CopyTo("Clipboard") },
@@ -163,38 +158,7 @@ return {
     { key = "V", mods = "SHIFT|CTRL", action = act.PasteFrom("Clipboard") },
 
     -- scrolling
-    { key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-1) },
-    { key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(1) },
-
-    -- for pane control in tmux/nvim
-    { key = "h", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;72~") },
-    { key = "j", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;74~") },
-    { key = "k", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;75~") },
-    { key = "l", mods = "SHIFT|CTRL", action = act.SendString("\x1b[27;6;76~") },
-
-    -- for tab selection in nvim
-    { key = "1", mods = "CTRL", action = act.SendString("\x1b[27;5;49~") },
-    { key = "2", mods = "CTRL", action = act.SendString("\x1b[27;5;50~") },
-    { key = "3", mods = "CTRL", action = act.SendString("\x1b[27;5;51~") },
-    { key = "4", mods = "CTRL", action = act.SendString("\x1b[27;5;52~") },
-    { key = "5", mods = "CTRL", action = act.SendString("\x1b[27;5;53~") },
-    { key = "6", mods = "CTRL", action = act.SendString("\x1b[27;5;54~") },
-    { key = "7", mods = "CTRL", action = act.SendString("\x1b[27;5;55~") },
-    { key = "8", mods = "CTRL", action = act.SendString("\x1b[27;5;56~") },
-    { key = "9", mods = "CTRL", action = act.SendString("\x1b[27;5;57~") },
-    -- uncomment below to use for some terminal program
-    -- { key = "0", mods = "CTRL", action = act.SendString("\x1b[27;5;48~") },
-
-    -- -- for pane moving in tmux
-    -- { key = '1',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;49~") },
-    -- { key = '2',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;50~") },
-    -- { key = '3',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;51~") },
-    -- { key = '4',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;52~") },
-    -- { key = '5',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;53~") },
-    -- { key = '6',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;54~") },
-    -- { key = '7',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;55~") },
-    -- { key = '8',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;56~") },
-    -- { key = '9',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;57~") },
-    -- { key = '0',        mods = 'SHIFT|ALT',  action = act.SendString("\x1b[27;4;48~") },
+    { key = "PageUp", action = act.ScrollByPage(-1) },
+    { key = "PageDown", action = act.ScrollByPage(1) },
   },
 }
