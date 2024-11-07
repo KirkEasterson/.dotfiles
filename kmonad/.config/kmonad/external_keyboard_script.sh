e496ed9160d6a8c5caa3b33bf686fedb465a0b30
Kirk Easterson <kirk.easterson@gmail.com>
2024-11-07 19:52:43 +0100

    feat(kmonad): support multiple keyboards

--- kmonad/.config/kmonad/external_keyboard_script.sh
+++ kmonad/.config/kmonad/external_keyboard_script.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-device_name="platform-i8042-serio-0-event-kbd"
+laptop="by-path/platform-i8042-serio-0-event-kbd"
+dactyl="by-id/usb-izwan_zwactyl_50_vial:f64c2b3c-event-kbd"
 
-if [ -e "/dev/input/by-path/${device_name}" ]; then
-	kmonad "${HOME}/.config/kmonad/config.kbd"
+# This isn't an ideal solution. But the external keyboard gets high priority
+# than the laptop keyboard, so check for that first.
+if [ -e "/dev/input/${dactyl}" ]; then
+	kmonad "${HOME}/.config/kmonad/dactyl.kbd"
+elif [ -e "/dev/input/${laptop}" ]; then
+	kmonad "${HOME}/.config/kmonad/laptop.kbd"
 fi
