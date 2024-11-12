65c2a7c1079675c0524286e02c22c5f61b1f91a8
Kirk Easterson <kirk.easterson@gmail.com>
2024-11-12 12:47:53 +0100

    feat(kmonad): use one config file for all keyboards

--- kmonad/.config/kmonad/external_keyboard_script.sh
+++ kmonad/.config/kmonad/external_keyboard_script.sh
@@ -1,12 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-laptop="by-path/platform-i8042-serio-0-event-kbd"
-dactyl="by-id/usb-izwan_zwactyl_50_vial:f64c2b3c-event-kbd"
+laptop="/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+dactyl="/dev/input/by-id/usb-izwan_zwactyl_50_vial:f64c2b3c-event-kbd"
 
 # This isn't an ideal solution. But the external keyboard gets high priority
 # than the laptop keyboard, so check for that first.
-if [ -e "/dev/input/${dactyl}" ]; then
-	kmonad "${HOME}/.config/kmonad/dactyl.kbd"
-elif [ -e "/dev/input/${laptop}" ]; then
-	kmonad "${HOME}/.config/kmonad/laptop.kbd"
+if [ -e "${dactyl}" ]; then
+	echo "dactyl"
+	KBD_DEV=$dactyl
+elif [ -e "${laptop}" ]; then
+	echo "laptop"
+	KBD_DEV=$laptop
+else
+	echo "exiting"
+	exit 0
 fi
+
+export KBD_DEV
+KBDCFG=$(envsubst <"${HOME}/.config/kmonad/config.kbd")
+
+# TODO: make this line POSIX compliant
+kmonad <(echo "$KBDCFG")
