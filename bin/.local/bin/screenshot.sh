d67427ede2934f0f988a7df006afcdceb60c80d7
Kirk Easterson <kirk.easterson@gmail.com>
2024-11-14 12:24:22 +0100

    fix(flameshot): hack current desktop var so it works on river

--- bin/.local/bin/screenshot.sh
+++ bin/.local/bin/screenshot.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-gui () {
+# NOTE: Manually specifying the current wayland desktop to sway is intentional,
+# since flameshot does not yet support river. But it works fine on river ¯\_(ツ)_/¯
+
+gui() {
 	if [ -n "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
 		# grim -g "$(slurp -b 00000090 -s cc000000 -w 0)" "${output_file}"
-		flameshot gui
+		XDG_CURRENT_DESKTOP=sway flameshot gui
 	else
 		flameshot gui
 	fi
 }
 
-window () {
+window() {
 	if [ -n "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
 		# grim -g "$(swaymsg -t get_tree | jq -j '.. | select(.type?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')" "${output_file}"
-		flameshot full
+		XDG_CURRENT_DESKTOP=sway flameshot full
 	else
 		flameshot full
 	fi
 }
 
-fullscreen () {
+fullscreen() {
 	if [ -n "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
 		# grim "${output_file}"
-		flameshot full
+		XDG_CURRENT_DESKTOP=sway flameshot full
 	else
 		flameshot full
 	fi
 }
 
-sound () {
+sound() {
 	pw-play /usr/share/sounds/freedesktop/stereo/camera-shutter.oga
 }
 
@@ -36,17 +39,18 @@ screenshots_dir="$(xdg-user-dir PICTURES)/screenshots"
 mkdir -p "${screenshots_dir}"
 
 case "${1}" in
-	("gui")
-		gui
-		;;
-	("window")
-		window
-		;;
-	("fullscreen")
-		fullscreen
-		;;
-	(*)
-		exit 0
+"gui")
+	gui
+	;;
+"window")
+	window
+	;;
+"fullscreen")
+	fullscreen
+	;;
+*)
+	exit 0
+	;;
 esac
 
 sound
