4eab2eb14cd743d46a8d861016350076ea9d8e6f
Kirk Easterson <kirk.easterson@gmail.com>
2024-11-15 09:48:30 +0100

    docs(river): remove outdated comment

--- river/.config/river/modules/07-output.sh
+++ river/.config/river/modules/07-output.sh
@@ -7,9 +7,6 @@ riverctl map-switch locked lid close spawn "clamshell_or_lock.sh"
 riverctl map-switch locked lid open spawn "clamshell_or_lock.sh"
 riverctl spawn "clamshell.sh"
 
-# TODO: lock on lid close
-# riverctl map-switch normal lid close spawn "lock.sh"
-
 # lock after 5 minutes
 # turn off display after 10 minutes # TODO: implement this
 riverctl spawn "swayidle -w \
