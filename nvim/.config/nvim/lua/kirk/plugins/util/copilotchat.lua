b47bddd2f07b708dc311bfdd37315d156f4c4ce7
Kirk Easterson <kirk.easterson@gmail.com>
2024-12-07 14:08:47 +0100

    feat(nvim): add copilot chat

---
+++ nvim/.config/nvim/lua/kirk/plugins/util/copilotchat.lua
@@ -0,0 +1,58 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "github/copilot.vim",
+    "MeanderingProgrammer/render-markdown.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  cmd = {
+    "CopilotChat",
+    "CopilotChatAgents",
+    "CopilotChatClose",
+    "CopilotChatDebugInfo",
+    "CopilotChatLoad",
+    "CopilotChatModels",
+    "CopilotChatOpen",
+    "CopilotChatReset",
+    "CopilotChatSave",
+    "CopilotChatStop",
+    "CopilotChatToggle",
+  },
+  build = "make tiktoken",
+  keys = {
+    {
+      "<leader>cpt",
+      function()
+        require("CopilotChat").toggle()
+      end,
+      desc = "CopilotChat - toggle",
+    },
+    {
+      "<leader>cpp",
+      function()
+        local actions = require("CopilotChat.actions")
+        require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+      end,
+      desc = "CopilotChat - prompt actions",
+      mode = { "n", "v" },
+    },
+  },
+  opts = {
+    highlight_headers = false,
+    question_header = "  ",
+    answer_header = "  ",
+    error_header = "  ",
+  },
+  init = function()
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "copilot-*",
+      callback = function()
+        vim.opt_local.number = false
+        vim.opt_local.relativenumber = false
+        vim.opt_local.signcolumn = "no"
+        vim.opt_local.statuscolumn = ""
+      end,
+    })
+  end,
+}
