ff320987badd92a038ea37b7a098df3f4f21cf87
Kirk Easterson <kirk.easterson@gmail.com>
2024-12-20 09:37:44 +0100

    feat(nvim): add config for smear cursor

---
+++ nvim/.config/nvim/lua/kirk/plugins/ui/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  enabled = false,
+  event = "VimEnter",
+  opts = {
+    smear_between_buffers = false,
+    scroll_buffer_space = false, -- try toggling this
+  },
+}
