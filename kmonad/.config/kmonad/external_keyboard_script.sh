a2dc9998d1702094e82151becce7026af461ec9f
Kirk Easterson <kirk.easterson@gmail.com>
2024-12-22 19:04:53 +0100

    feat(kmonad): add oryal kludge keyboard to script

--- kmonad/.config/kmonad/external_keyboard_script.sh
+++ kmonad/.config/kmonad/external_keyboard_script.sh
@@ -2,16 +2,23 @@
 
 moergo="/dev/input/by-id/usb-MoErgo_Glove80_Left_moergo.com:GLV80-320B6C0561C725F1-event-kbd"
 dactyl="/dev/input/by-id/usb-izwan_zwactyl_50_vial:f64c2b3c-event-kbd"
+rk="/dev/input/by-id/usb-MosArt_USB_Keyboard-event-kbd"
 laptop="/dev/input/by-path/platform-i8042-serio-0-event-kbd"
 
 # This isn't an ideal solution. But the external keyboard gets high priority
 # than the laptop keyboard, so check for that first.
 if [ -e "${moergo}" ]; then
 	KBD_DEV=$moergo
+
 elif [ -e "${dactyl}" ]; then
 	KBD_DEV=$dactyl
+
+elif [ -e "${rk}" ]; then
+	KBD_DEV=$rk
+
 elif [ -e "${laptop}" ]; then
 	KBD_DEV=$laptop
+
 else
 	echo "exiting"
 	exit 0
