26b49d2e851d15839a6d8684654a9b5efa81eee2
Kirk Easterson <kirk.easterson@gmail.com>
2025-01-14 18:58:34 +0100

    feat(river): re-enable 1password silent

--- river/.config/river/modules/09-exec-fg.sh
+++ river/.config/river/modules/09-exec-fg.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# riverctl spawn "1password --silent"
+riverctl spawn "1password --silent"
 riverctl spawn "blueman-applet"
 riverctl spawn "nm-applet"
-# riverctl spawn "pcmanfm --daemon-mode"
