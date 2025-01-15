425a97f083546131a257631e6beed8033f50b00e
Kirk Easterson <kirk.easterson@gmail.com>
2025-01-15 08:32:04 +0100

    refactor(scripts): clean up `set_all_displays`

--- scripts/.local/scripts/set_all_displays.sh
+++ scripts/.local/scripts/set_all_displays.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-wlr-randr \
-	| grep -v "^ " \
-	| cut -d " " -f1 \
-	| xargs -I {} wlr-randr --output {} --$1
+set_display() {
+
+	if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
+		xrandr |
+			grep -v "^ " |
+			cut -d " " -f1 |
+			xargs -I {} xrandr --output {} --"$1"
+	else
+		wlr-randr |
+			grep -v "^ " |
+			cut -d " " -f1 |
+			xargs -I {} wlr-randr --output {} --"$1"
+	fi
+
+}
+
+case "${1}" in
+"on")
+	set_display "on"
+	;;
+"off")
+	set_display "off"
+	;;
+*)
+	exit 0
+	;;
+esac
