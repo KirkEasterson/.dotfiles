cb589780031288ce0fa063dc77df1a997a4aa522
Kirk Easterson <kirk.easterson@gmail.com>
2025-01-29 16:48:44 +0100

    feat(kmonad): add ergodox

--- kmonad/.config/kmonad/external_devices.sh
+++ kmonad/.config/kmonad/external_devices.sh
@@ -4,11 +4,15 @@ moergo="/dev/input/by-id/usb-MoErgo_Glove80_Left_moergo.com:GLV80-320B6C0561C725
 dactyl="/dev/input/by-id/usb-izwan_zwactyl_50_vial:f64c2b3c-event-kbd"
 royal_kludge="/dev/input/by-id/usb-MosArt_USB_Keyboard-event-kbd"
 laptop="/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+ergodox="/dev/input/by-id/usb-ZSA_Technology_Labs_ErgoDox_EZ_Glow-event-kbd"
 
 # This isn't an ideal solution. But the external keyboard gets high priority
 # than the laptop keyboard, so check for that first.
 if [ -e "${moergo}" ]; then
 	KBD_DEV=$moergo
+
+elif [ -e "${ergodox}" ]; then
+	KBD_DEV=$ergodox
 
 elif [ -e "${dactyl}" ]; then
 	KBD_DEV=$dactyl
