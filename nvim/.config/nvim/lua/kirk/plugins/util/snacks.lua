b094c3a4fdbe49665b0d32e4589c467a64b39d99
Kirk Easterson <kirk.easterson@gmail.com>
2025-02-04 05:56:03 +0100

    feat(nvim): replace fzf with snacks picker

---
+++ nvim/.config/nvim/lua/kirk/plugins/util/snacks.lua
@@ -0,0 +1,63 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require("snacks").picker.files({
+          layout = {
+            preset = "ivy_split",
+            preview = false,
+          },
+        })
+      end,
+      desc = "Find files",
+    },
+    {
+      "<leader>fb",
+      function()
+        require("snacks").picker.buffers()
+      end,
+      desc = "Search buffers",
+    },
+    {
+      "<leader>fB",
+      function()
+        require("snacks").picker.lines()
+      end,
+      desc = "Search current buffer",
+    },
+    {
+      "<leader>fg",
+      function()
+        require("snacks").picker.grep()
+      end,
+      desc = "Project search",
+    },
+    {
+      "<leader>fG",
+      function()
+        require("snacks").picker.grep_word()
+      end,
+      desc = "Search for current word",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>fr",
+      function()
+        require("snacks").picker.lsp_references()
+      end,
+      desc = "LSP references",
+    },
+  },
+  opts = {
+    picker = {
+      enabled = true,
+      prompt = " ",
+      filename_bonus = true,
+      cwd_bonus = true,
+    },
+  },
+}
