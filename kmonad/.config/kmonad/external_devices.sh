25dde101bd4bb87b2f187aec1620f065c575f84a
Kirk Easterson <kirk.easterson@gmail.com>
2025-02-23 20:04:32 +0100

    feat(kmonad): add more layers to base config

--- kmonad/.config/kmonad/external_devices.sh
+++ kmonad/.config/kmonad/external_devices.sh
@@ -6,22 +6,30 @@ royal_kludge="/dev/input/by-id/usb-MosArt_USB_Keyboard-event-kbd"
 laptop="/dev/input/by-path/platform-i8042-serio-0-event-kbd"
 ergodox="/dev/input/by-id/usb-ZSA_Technology_Labs_ErgoDox_EZ_Glow-event-kbd"
 
+regular_config="${HOME}/.config/kmonad/config.kbd"
+miryoku_config="${HOME}/.config/kmonad/miryoku.kbd"
+
 # This isn't an ideal solution. But the external keyboard gets high priority
 # than the laptop keyboard, so check for that first.
 if [ -e "${moergo}" ]; then
 	KBD_DEV=$moergo
+	KBD_TEMPLATE=$regular_config
 
 elif [ -e "${ergodox}" ]; then
 	KBD_DEV=$ergodox
+	KBD_TEMPLATE=$regular_config
 
 elif [ -e "${dactyl}" ]; then
 	KBD_DEV=$dactyl
+	KBD_TEMPLATE=$miryoku_config
 
 elif [ -e "${royal_kludge}" ]; then
 	KBD_DEV=$royal_kludge
+	KBD_TEMPLATE=$regular_config
 
 elif [ -e "${laptop}" ]; then
 	KBD_DEV=$laptop
+	KBD_TEMPLATE=$regular_config
 
 else
 	echo "exiting"
@@ -30,7 +38,7 @@ fi
 
 export KBD_DEV
 # NOTE: $XDG_CONFIG_HOME can't be used below
-KBDCFG=$(envsubst <"${HOME}/.config/kmonad/config.kbd")
+KBDCFG=$(envsubst <"$KBD_TEMPLATE")
 
 # TODO: make this line POSIX compliant
 kmonad <(echo "$KBDCFG")
