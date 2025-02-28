2ea414a6aa8f757b9fe5d810ccc6ae2cbc0b67c6
Kirk Easterson <kirk.easterson@gmail.com>
2025-02-28 11:12:03 +0100

    feat(kmonad): remove dactyl

--- kmonad/.config/kmonad/external_devices.sh
+++ kmonad/.config/kmonad/external_devices.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-dactyl="/dev/input/by-id/usb-izwan_zwactyl_50_vial:f64c2b3c-event-kbd"
 royal_kludge="/dev/input/by-id/usb-MosArt_USB_Keyboard-event-kbd"
 laptop="/dev/input/by-path/platform-i8042-serio-0-event-kbd"
 ergodox="/dev/input/by-id/usb-ZSA_Technology_Labs_ErgoDox_EZ_Glow-event-kbd"
@@ -13,10 +12,6 @@ miryoku_config="${HOME}/.config/kmonad/miryoku.kbd"
 if [ -e "${ergodox}" ]; then
 	KBD_DEV=$ergodox
 	KBD_TEMPLATE=$regular_config
-
-elif [ -e "${dactyl}" ]; then
-	KBD_DEV=$dactyl
-	KBD_TEMPLATE=$miryoku_config
 
 elif [ -e "${royal_kludge}" ]; then
 	KBD_DEV=$royal_kludge
