a5d83fc9abf6a3fad3a9309b8c68235b89998593
Kirk Easterson <kirk.easterson@gmail.com>
2025-03-29 10:46:14 +0100

    feat(nvim): add util func for winbar conf

--- nvim/.config/nvim/lua/util.lua
+++ nvim/.config/nvim/lua/util.lua
@@ -225,4 +225,13 @@ function M.git_default_branch()
   return result
 end
 
+function M.winbar()
+  local file_path = vim.api.nvim_eval_statusline("%f", {}).str
+  local modified = vim.api.nvim_eval_statusline("%M", {}).str == "+" and "()" or ""
+
+  file_path = file_path:gsub("/", "  ")
+
+  return "%#WinBarPath#" .. file_path .. "%*" .. " %#WinBarModified#" .. modified .. "%*"
+end
+
 return M
