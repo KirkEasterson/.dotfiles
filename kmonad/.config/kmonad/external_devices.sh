f0362b9a8e42b9b791b6b8d8d94c17555d7f8f62
Kirk Easterson <kirk.easterson@gmail.com>
2025-03-29 10:38:04 +0100

    feat(kmonad): remove ergodox

--- kmonad/.config/kmonad/external_devices.sh
+++ kmonad/.config/kmonad/external_devices.sh
@@ -2,18 +2,13 @@
 
 royal_kludge="/dev/input/by-id/usb-MosArt_USB_Keyboard-event-kbd"
 laptop="/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-ergodox="/dev/input/by-id/usb-ZSA_Technology_Labs_ErgoDox_EZ_Glow-event-kbd"
 
 regular_config="${HOME}/.config/kmonad/config.kbd"
 miryoku_config="${HOME}/.config/kmonad/miryoku.kbd"
 
 # This isn't an ideal solution. But external keyboards gets higher priority
 # than the laptop keyboard, so check for that first.
-if [ -e "${ergodox}" ]; then
-	KBD_DEV=$ergodox
-	KBD_TEMPLATE=$regular_config
-
-elif [ -e "${royal_kludge}" ]; then
+if [ -e "${royal_kludge}" ]; then
 	KBD_DEV=$royal_kludge
 	KBD_TEMPLATE=$regular_config
 
