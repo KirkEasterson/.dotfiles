cd7073c41a9a7420eef58e493a9d56ad872f42c1
Kirk Easterson <kirk.easterson@gmail.com>
2025-03-30 14:49:13 +0200

    feat(polybar): show on multiple monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,5 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar top &
+if type "xrandr"; then
+	for m in $(xrandr --listactivemonitors | sed 1,1d | cut -d" " -f6); do
+		MONITOR=$m polybar top &
+	done
+fi
