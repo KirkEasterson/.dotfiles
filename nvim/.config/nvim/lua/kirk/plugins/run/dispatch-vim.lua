5f018d09a011e745c577f8e3720b13ee7fb3de85
Kirk Easterson <kirk.easterson@gmail.com>
2025-04-13 03:36:16 +0200

    feat(nvim): add dispatch

---
+++ nvim/.config/nvim/lua/kirk/plugins/run/dispatch-vim.lua
@@ -0,0 +1,14 @@
+return {
+  "tpope/vim-dispatch",
+  enabled = false,
+  dependencies = {
+    "Konfekt/vim-compilers",
+  },
+  lazy = false,
+  cmd = {
+    "Make",
+    "Dispatch",
+    "Start",
+    "Spawn",
+  },
+}
