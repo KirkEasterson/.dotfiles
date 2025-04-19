1d64c241287d04de107d47af1440cbd7e7d7d3e3
Kirk Easterson <kirk.easterson@gmail.com>
2025-04-19 10:36:29 +0200

    feat(river): add gaps and smaller border

--- river/.config/river/modules/02-options.sh
+++ river/.config/river/modules/02-options.sh
@@ -3,4 +3,4 @@
 # Set the default layout generator to be rivertile and start it.
 # River will send the process group of the init executable SIGTERM on exit.
 riverctl default-layout rivertile
-rivertile -view-padding 0 -outer-padding 0 -main-ratio 0.5 &
+rivertile -view-padding 5 -outer-padding 5 -main-ratio 0.5 &
