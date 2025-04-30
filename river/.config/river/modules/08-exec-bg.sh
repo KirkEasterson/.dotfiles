302111e2a7bbc2562313ccd809a950f4bcc54d1c
Kirk Easterson <kirk.easterson@gmail.com>
2025-04-30 16:18:47 +0200

    fix(river): kanshi not starting in profile

--- river/.config/river/modules/08-exec-bg.sh
+++ river/.config/river/modules/08-exec-bg.sh
@@ -5,3 +5,6 @@ riverctl spawn "${XDG_CONFIG_HOME}/waybar/start.sh"
 riverctl spawn "systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
 riverctl spawn "hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=river"
 riverctl spawn "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+
+# TODO: find out why this isn't starting from profile
+riverctl spawn "kanshi"
