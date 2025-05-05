64ca54b0566b871e28e15d10522d7917c125815c
Kirk Easterson <kirk.easterson@gmail.com>
2025-05-05 21:59:27 +0200

    feat(waybar): add recording module

--- scripts/.local/scripts/record.sh
+++ scripts/.local/scripts/record.sh
@@ -4,80 +4,66 @@ send_signal() {
 	bar-signal.sh "recorder"
 }
 
-update_icon() {
-	echo "$1" >/tmp/recordingicon
-	# pkill -RTMIN+9 "${STATUSBAR:-dwmblocks}"
+status() {
+	recpid="$(cat /tmp/recordingpid 2>/dev/null)"
+	if [ -z "$recpid" ]; then
+		class="off"
+		text="Not recording"
+	else
+		class="$(cat /tmp/recordingtype)"
+		text="$(tr '_' ' ' </tmp/recordingtype)"
+	fi
+
+	printf '{"alt":"%s","class":"%s","tooltip":"%s"}\n' "$class" "$class" "$text"
 }
 
 kill_recording() {
 	recpid="$(cat /tmp/recordingpid)"
 	kill -15 "$recpid"
 	rm -f /tmp/recordingpid
-
-	# update_icon ""
-	class="off"
-	text="Not recording"
+	notify-send "Recording" "Recording ended"
 }
 
 screen() {
-	file="${screencasts_dir}/$(date '+%y-%m-%d_%H-%M-%S').${file_type}"
-	wf-recorder --file="${file}" &
+	wf-recorder --file="${filename}" &
 	echo $! >/tmp/recordingpid
-
-	# update_icon "⏺️🎙️"
-	class="screen"
-	text="Recording screen"
+	echo "screen" >/tmp/recordingtype
+	notify-send "Recording" "Recording screen"
 }
 
 screen_and_audio() {
-	file="${screencasts_dir}/$(date '+%y-%m-%d_%H-%M-%S').${file_type}"
-	wf-recorder --audio --file="${file}" &
+	wf-recorder --audio --file="${filename}" &
 	echo $! >/tmp/recordingpid
-
-	# update_icon "⏺️🎙️"
-	class="screen_and_audio"
-	text="Recording screen with audio"
+	echo "screen_and_audio" >/tmp/recordingtype
+	notify-send "Recording" "Recording screen and audio"
 }
 
 screen_selection() {
-	file="${screencasts_dir}/$(date '+%y-%m-%d_%H-%M-%S').${file_type}"
-	wf-recorder -g "$(slurp)" --file="${file}" &
+	wf-recorder -g "$(slurp)" --file="${filename}" &
 	echo $! >/tmp/recordingpid
-
-	# update_icon "⏺️🎙️"
-	class="screen_selection"
-	text="Recording screen selection"
+	echo "screen_selection" >/tmp/recordingtype
+	notify-send "Recording" "Recording screen selection"
 }
 
 webcam() {
-	file="${screencasts_dir}/$(date '+%y-%m-%d_%H-%M-%S')"
-
-	# mpv /dev/video0 \
-	# 	--profile=low-latency \
-	# 	--untimed \
-	# 	--no-osc \
-	# 	--title=webcam-on-screen &
-
 	ffmpeg \
 		-i /dev/video0 \
-		"${file}.${file_type}" &
+		"${filename}" &
 
 	echo $! >/tmp/recordingpid
-
-	# update_icon "🎥"
-	class="webcam"
-	text="Recording from webcam"
+	echo "webcam" >/tmp/recordingtype
+	notify-send "Recording" "Recording webcam"
 }
 
 prompt_end() {
-	response=$(printf "No\\nYes" |
-		$SEARCH -d -i -p "Recording still active. End recording?") &&
+	response=$(printf "Yes\\nNo" |
+		$SEARCH -dmenu -p "Recording still active. End recording?") &&
 		[ "$response" = "Yes" ] && kill_recording
 }
 
 prompt_recording() {
 	choice=$(printf "screen\\nscreen_and_audio\\nscreen_selection\\nwebcam" |
-		$SEARCH -d -i -p "Select recording style:")
+		$SEARCH -dmenu -p "Select recording:")
 
 	case "$choice" in
 	screen) screen ;;
@@ -85,29 +71,27 @@ prompt_recording() {
 	screen_selection) screen_selection ;;
 	# audio) audio;;
 	webcam) webcam ;;
-	*)
-		class="off"
-		text="Not recording"
-		;;
+	*) exit 1 ;;
 	esac
 }
 
-file_type="mp4"
 screencasts_dir="${HOME}/Videos/screen_recordings"
+file_type="mp4"
+filename="${screencasts_dir}/$(date '+%y-%m-%d_%H-%M-%S').${file_type}"
 
 if [ ! -d "${screencasts_dir}" ]; then
 	mkdir -p "${screencasts_dir}"
 fi
 
 case "$1" in
-screen) screen ;;
-screen_and_audio) screen_and_audio ;;
-screen_selection) screen_selection ;;
+"screen") screen ;;
+"screen_and_audio") screen_and_audio ;;
+"screen_selection") screen_selection ;;
 # audio) audio;;
-webcam) webcam ;;
-kill) kill_recording ;;
-*) ([ -f /tmp/recordingpid ] && prompt_end) || prompt_recording ;;
+"webcam") webcam ;;
+"kill") kill_recording ;;
+"prompt") ([ -f /tmp/recordingpid ] && prompt_end) || prompt_recording ;;
+*) status ;;
 esac
 
-printf '{"alt":"%s","tooltip":"%s"}\n' "$class" "$text"
 send_signal
