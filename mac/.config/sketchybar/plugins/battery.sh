fd198f1da5497622789a3816925ace995455768e
Kirk Easterson <kirk.easterson@gmail.com>
2025-10-01 09:39:07 +0200

    feat(sketchybar): reduce complexity of battery plugin

--- mac/.config/sketchybar/plugins/battery.sh
+++ mac/.config/sketchybar/plugins/battery.sh
@@ -7,87 +7,56 @@ if [ "$PERCENTAGE" = "" ]; then
   exit 0
 fi
 
-if [[ $CHARGING = "" ]]; then
-  case ${PERCENTAGE} in
-  100)
-    ICON="󰁹"
-    ;;
-  [9][0-9])
-    ICON="󰂂"
-    ;;
-  [8][0-9])
-    ICON="󰂁"
-    ;;
-  [7][0-9])
-    ICON="󰂀"
-    ;;
-  [6][0-9])
-    ICON="󰁿"
-    ;;
-  [5][0-9])
-    ICON="󰁾"
-    ;;
-  [4][0-9])
-    ICON="󰁽"
-    ;;
-  [3][0-9])
-    ICON="󰁼"
-    ;;
-  [2][0-9])
-    ICON="󰁻"
-    ;;
-  [1][0-9])
-    ICON="󰁺"
-    ;;
-  *) ICON="󱃍" ;;
-  esac
-fi
-
-if [[ $CHARGING != "" ]]; then
-  case ${PERCENTAGE} in
-  100)
-    ICON="󰂅"
-    ;;
-  [9][0-9])
-    ICON="󰂋"
-    ;;
-  [8][0-9])
-    ICON="󰂊"
-    ;;
-  [7][0-9])
-    ICON="󰢞"
-    ;;
-  [6][0-9])
-    ICON="󰂉"
-    ;;
-  [5][0-9])
-    ICON="󰢝"
-    ;;
-  [4][0-9])
-    ICON="󰂈"
-    ;;
-  [3][0-9])
-    ICON="󰂇"
-    ;;
-  [2][0-9])
-    ICON="󰂆"
-    ;;
-  [1][0-9])
-    ICON="󰢜"
-    ;;
-  *) ICON="󰢟" ;;
-  esac
-fi
+case ${PERCENTAGE} in
+100)
+  ICON="󰁹"
+  ;;
+[9][0-9])
+  ICON="󰂂"
+  ;;
+[8][0-9])
+  ICON="󰂁"
+  ;;
+[7][0-9])
+  ICON="󰂀"
+  ;;
+[6][0-9])
+  ICON="󰁿"
+  ;;
+[5][0-9])
+  ICON="󰁾"
+  ;;
+[4][0-9])
+  ICON="󰁽"
+  ;;
+[3][0-9])
+  ICON="󰁼"
+  ;;
+[2][0-9])
+  ICON="󰁻"
+  ;;
+[1][0-9])
+  ICON="󰁺"
+  ;;
+*) ICON="󰂎" ;;
+esac
 
 case ${PERCENTAGE} in
 [8-9][0-9] | 100)
-  HIGHLIGHT="0xffb8bb26"
+  HIGHLIGHT="0xff98971a"
   ;;
 [3-7][0-9])
   HIGHLIGHT="0xfffabd2f"
   ;;
-*) HIGHLIGHT="0xffcc241d" ;;
+*)
+  HIGHLIGHT="0xffcc241d"
+  ICON="󱈸${ICON}"
+  ;;
 esac
+
+if [[ $CHARGING != "" ]]; then
+  ICON="${ICON}󱐋"
+fi
 
 sketchybar --set "$NAME" icon="$ICON" \
   icon.color="$HIGHLIGHT" \
