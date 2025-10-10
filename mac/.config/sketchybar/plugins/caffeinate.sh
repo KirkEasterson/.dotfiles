add2ce98c1c367c02b6ddd30c2c0f1c0e6787440
Kirk Easterson <kirk.easterson@gmail.com>
2025-10-10 07:26:21 +0200

    fix(sketchybar): caffeinate shows correct icon

--- mac/.config/sketchybar/plugins/caffeinate.sh
+++ mac/.config/sketchybar/plugins/caffeinate.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 
-CAFFINATE_ID=$(pgrep caffeinate)
 SLEEP_ICON="󰒲"
 AWAKE_ICON="󰒳"
 
-toggle_caffeine() {
-	if [ -n "$CAFFINATE_ID" ]; then
-		pkill caffeinate
+caffeinate_id() {
+	pgrep caffeinate
+}
+
+start_caffeinate() {
+	### Copied from `man caffeinate`
+	#
+	# -d      Create an assertion to prevent the display from sleeping.
+	#
+	# -i      Create an assertion to prevent the system from idle sleeping.
+	#
+	# -m      Create an assertion to prevent the disk from idle sleeping.
+	#
+	# -s      Create an assertion to prevent the system from sleeping. This
+	#         assertion is valid only when system is running on AC power.
+	#
+	# -u      Create an assertion to declare that user is active. If the
+	#         display is off, this option turns the display on and prevents
+	#         the display from going into idle sleep. If a timeout is not
+	#         specified with '-t' option, then this assertion is taken with
+	#         a default of 5 second timeout.
+	caffeinate -dimsu &
+	disown
+}
+
+stop_caffeinate() {
+	pkill caffeinate
+}
+
+toggle_caffeinate() {
+	if [ -z "$(caffeinate_id)" ]; then
+		start_caffeinate
 	else
-		caffeinate -id &
-		disown
+		stop_caffeinate
 	fi
 }
 
-# if a mouse click, then toggle caffeine
+# if a mouse click, then toggle caffeinate
 if [ -n "$BUTTON" ]; then
-	toggle_caffeine
+	toggle_caffeinate
 fi
 
-# if caffeine is running
-if [ -n "$CAFFINATE_ID" ]; then
+# if caffeinate is running
+if [ -n "$(caffeinate_id)" ]; then
 	ICON="$AWAKE_ICON"
 else
 	ICON="$SLEEP_ICON"
