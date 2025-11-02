fc06f273a1c32fc2a710de264b46fcdb0e835ed1
Kirk Easterson <kirk.easterson@gmail.com>
2025-11-02 09:43:46 +0100

    feat(scripts): add clone script and opinionated dir layout for repos

---
+++ scripts/.local/scripts/clone.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# git clone a go project to $GOPATH/src
+if [ $# -eq 0 ]; then
+	>&2 echo "Provide a git repository"
+	return
+fi
+
+url=$1
+
+# magic regex: https://serverfault.com/a/917253
+re="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/(.+).git*$"
+if [[ $url =~ $re ]]; then
+	host=${BASH_REMATCH[3]}
+	team=${BASH_REMATCH[4]}
+	repo=${BASH_REMATCH[5]}
+else
+	>&2 echo "Provide a valid git repository"
+	return
+fi
+
+dest="${HOME}/dev/$host/$team/$repo"
+if ! [ -d "$dest" ]; then
+	mkdir -p "$dest"
+	git clone "$url" "$dest"
+else
+	>&2 echo "Repo already exists"
+fi
--- scripts/.local/scripts/tmux_sessionizer.sh
+++ scripts/.local/scripts/tmux_sessionizer.sh
@@ -7,9 +7,9 @@ if [ $# -eq 1 ]; then
 	selected=$1
 else
 	selected=$(
-		find ~/dev \
-			-mindepth 1 \
-			-maxdepth 4 \
+		find ~/dev/github.com/ \
+			-mindepth 2 \
+			-maxdepth 3 \
 			-type d \
 			-name .git \
 			-prune |
@@ -17,29 +17,20 @@ else
 			fzf
 	)
 	if [ -z "$selected" ]; then
-		exit 0
+		exit 1
 	fi
 fi
 
-selected_name=$(basename "$selected" | tr . _)
+selected_label=$(basename "$selected" | tr . _)
 
-# if inside tmux
-if [ -n "$TMUX" ]; then
-	# if the session doesn't exist, create it
-	if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-		tmux new-session -d -s "$selected_name" -c "$selected"
-	fi
+# ensure session exists
+is_tmux_running=$(pgrep tmux)
+if [ -z "$is_tmux_running" ] || ! tmux has-session -t="$selected_label" 2>/dev/null; then
+	tmux new-session -d -s "$selected_label" -c "$selected"
+fi
 
-	tmux switch-client -t "$selected_name"
-
-# if not inside tmux
-else
-	tmux_running=$(pgrep tmux)
-
-	# if tmux isn't running or the session doesn't exist, create it
-	if [ -z "$tmux_running" ] || ! tmux has-session -t="$selected_name" 2>/dev/null; then
-		tmux new-session -d -s "$selected_name" -c "$selected"
-	fi
-
-	tmux attach-session -t "$selected_name"
+if [ -n "$TMUX" ]; then # if inside tmux
+	tmux switch-client -t "$selected_label"
+else # if not inside tmux
+	tmux attach-session -t "$selected_label"
 fi
