092eac0c0ecb15d63bb695819b05ac57fde27007
Kirk Easterson <kirk.easterson@gmail.com>
2025-12-03 10:46:58 +0100

    feat(river): autostart dunst

--- river/.config/river/modules/08-exec-bg.sh
+++ river/.config/river/modules/08-exec-bg.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 riverctl spawn "swaybg --image ${XDG_CONFIG_HOME}/wallpaper"
+riverctl spawn "dunst"
 riverctl spawn "${XDG_CONFIG_HOME}/waybar/start.sh"
 riverctl spawn "systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
 riverctl spawn "hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=river"
