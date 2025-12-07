f03de90eac985e4f1871220560a3963f0405f756
Kirk Easterson <kirk.easterson@gmail.com>
2025-12-07 23:33:07 +0100

    refactor(scripts): cleanup wallpaper script

--- scripts/.local/scripts/wallpaper.sh
+++ scripts/.local/scripts/wallpaper.sh
@@ -5,16 +5,33 @@ file="$1"
 # check if file has been updated
 cmp --silent "${XDG_CONFIG_HOME}/wallpaper" "$file" && exit 0
 
+# set wallpaper
 cp "$file" "${XDG_CONFIG_HOME}/wallpaper"
-# feh --no-fehbg --bg-scale "${XDG_CONFIG_HOME}/wallpaper" # TODO. check for wayland/x11
-swaybg --image "${XDG_CONFIG_HOME}/wallpaper" &
-disown
 
-wal -e -n -q --saturate 0.7 -b black -i "$file"
-
+if [ -n "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
+	swaybg --image "${XDG_CONFIG_HOME}/wallpaper" &
+	disown
+else
+	feh --no-fehbg --bg-scale "${XDG_CONFIG_HOME}/wallpaper" &
+	disown
+fi
 notify-send "Wallpaper updated" "Updating themes"
+
+# create color palette
+wal \
+	-n \
+	-a 99 \
+	-b "#000000" \
+	--saturate 1.0 \
+	--contrast 21.0 \
+	--cols16 darken \
+	-i "$file"
 
 pgrep waybar >/dev/null && "${XDG_CONFIG_HOME}/waybar/start.sh" &
 disown
+
 pgrep qutebrowser >/dev/null && qutebrowser darken':config-source' &
+disown
+
+pgrep river >/dev/null && "${XDG_CONFIG_HOME}/river/modules/03-theme.sh" &
 disown
