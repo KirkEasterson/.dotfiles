524df871f7778335f2db523482b1927f0cbd633a
Kirk Easterson <kirk.easterson@gmail.com>
2025-12-08 11:07:09 +0100

    feat(nvim): remove gruvbox from octo

--- vim/.config/nvim/lua/kirk/plugins/git/octo.lua
+++ vim/.config/nvim/lua/kirk/plugins/git/octo.lua
@@ -5,35 +5,11 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
     "echasnovski/mini.icons",
-
-    "ellisonleao/gruvbox.nvim",
   },
-  config = function(_, _)
+  opts = {
+    default_merge_method = "squash",
+  },
+  init = function()
     vim.treesitter.language.register("markdown", "octo")
-
-    local gruvbox = require("gruvbox")
-    local opts = {
-      default_merge_method = "squash",
-      colors = {
-        white = gruvbox.palette.light0,
-        grey = gruvbox.palette.gray,
-        black = gruvbox.palette.dark0,
-
-        red = gruvbox.palette.light_red_hard,
-        dark_red = gruvbox.palette.dark_red,
-
-        green = gruvbox.palette.light_green_hard,
-        dark_green = gruvbox.palette.dark_green_hard,
-
-        yellow = gruvbox.palette.bright_yellow,
-        dark_yellow = gruvbox.palette.faded_yellow,
-
-        blue = gruvbox.palette.bright_blue,
-        dark_blue = gruvbox.palette.faded_blue,
-
-        purple = gruvbox.palette.bright_purple,
-      },
-    }
-    require("octo").setup(opts)
   end,
 }
