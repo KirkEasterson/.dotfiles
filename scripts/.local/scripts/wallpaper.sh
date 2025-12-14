19e2b0a80d9ad3d4d0a600dc18861007e20f80ac
Kirk Easterson <kirk.easterson@gmail.com>
2025-12-14 11:07:18 +0100

    feat(scripts): remove pywal from wallpaper.sh

--- scripts/.local/scripts/wallpaper.sh
+++ scripts/.local/scripts/wallpaper.sh
@@ -9,29 +9,10 @@ cmp --silent "${XDG_CONFIG_HOME}/wallpaper" "$file" && exit 0
 cp "$file" "${XDG_CONFIG_HOME}/wallpaper"
 
 if [ -n "$DISPLAY" ] && [ -n "$WAYLAND_DISPLAY" ]; then
-	swaybg --image "${XDG_CONFIG_HOME}/wallpaper" &
+	swaybg -m stretch --image "${XDG_CONFIG_HOME}/wallpaper" &
 	disown
 else
 	feh --no-fehbg --bg-scale "${XDG_CONFIG_HOME}/wallpaper" &
 	disown
 fi
-notify-send "Wallpaper updated" "Updating themes"
-
-# create color palette
-wal \
-	-n \
-	-a 99 \
-	-b "#000000" \
-	--saturate 1.0 \
-	--contrast 21.0 \
-	--cols16 darken \
-	-i "$file"
-
-pgrep waybar >/dev/null && "${XDG_CONFIG_HOME}/waybar/start.sh" &
-disown
-
-pgrep qutebrowser >/dev/null && qutebrowser darken':config-source' &
-disown
-
-pgrep river >/dev/null && "${XDG_CONFIG_HOME}/river/modules/03-theme.sh" &
-disown
+notify-send "Wallpaper updated"
