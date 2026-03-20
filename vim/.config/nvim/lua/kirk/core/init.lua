21e6c6e9fa94d9e2516a349f2ed7db81e5e66428
Kirk Easterson <kirk.easterson@gmail.com>
2026-03-20 17:57:59 +0100

    WIP of custom statusline

--- vim/.config/nvim/lua/kirk/core/init.lua
+++ vim/.config/nvim/lua/kirk/core/init.lua
@@ -1,3 +1,4 @@
 require("kirk.core.options")
 require("kirk.core.mappings")
 require("kirk.core.autocmds")
+-- require("kirk.core.statusline")
---
+++ vim/.config/nvim/lua/kirk/core/statusline.lua
@@ -0,0 +1,175 @@
+local modes = {
+  ["n"] = "NORMAL",
+  ["no"] = "NORMAL",
+  ["v"] = "VISUAL",
+  ["V"] = "VISUAL LINE",
+  [""] = "VISUAL BLOCK",
+  ["s"] = "SELECT",
+  ["S"] = "SELECT LINE",
+  [""] = "SELECT BLOCK",
+  ["i"] = "INSERT",
+  ["ic"] = "INSERT",
+  ["R"] = "REPLACE",
+  ["Rv"] = "VISUAL REPLACE",
+  ["c"] = "COMMAND",
+  ["cv"] = "VIM EX",
+  ["ce"] = "EX",
+  ["r"] = "PROMPT",
+  ["rm"] = "MOAR",
+  ["r?"] = "CONFIRM",
+  ["!"] = "SHELL",
+  ["t"] = "TERMINAL",
+}
+
+local mode = function()
+  local current_mode = vim.api.nvim_get_mode().mode
+  return string.format(" %s ", modes[current_mode]):upper()
+end
+
+local update_mode_colors = function()
+  local current_mode = vim.api.nvim_get_mode().mode
+  local mode_color = "%#StatusLineAccent#"
+  if current_mode == "n" then
+    mode_color = "%#StatuslineAccent#"
+  elseif current_mode == "i" or current_mode == "ic" then
+    mode_color = "%#StatuslineInsertAccent#"
+  elseif current_mode == "v" or current_mode == "V" or current_mode == "" then
+    mode_color = "%#StatuslineVisualAccent#"
+  elseif current_mode == "R" then
+    mode_color = "%#StatuslineReplaceAccent#"
+  elseif current_mode == "c" then
+    mode_color = "%#StatuslineCmdLineAccent#"
+  elseif current_mode == "t" then
+    mode_color = "%#StatuslineTerminalAccent#"
+  end
+  return mode_color
+end
+
+local function filename()
+  local fname = vim.fn.expand("%:t")
+  if fname == "" then
+    return ""
+  end
+  return fname .. " "
+end
+
+local lsp = function()
+  local count = {}
+  local levels = {
+    errors = "Error",
+    warnings = "Warn",
+    info = "Info",
+    hints = "Hint",
+  }
+
+  for k, level in pairs(levels) do
+    count[k] = vim.tbl_count(vim.diagnostic.get(0, { severity = level }))
+  end
+
+  local errors = ""
+  local warnings = ""
+  local hints = ""
+  local info = ""
+
+  if count["errors"] ~= 0 then
+    errors = " %#LspDiagnosticsSignError# " .. count["errors"]
+  end
+  if count["warnings"] ~= 0 then
+    warnings = " %#LspDiagnosticsSignWarning# " .. count["warnings"]
+  end
+  if count["hints"] ~= 0 then
+    hints = " %#LspDiagnosticsSignHint# " .. count["hints"]
+  end
+  if count["info"] ~= 0 then
+    info = " %#LspDiagnosticsSignInformation# " .. count["info"]
+  end
+
+  return errors .. warnings .. hints .. info .. "%#Normal#"
+end
+
+local macroinfo = function()
+  local reg = vim.fn.reg_recording()
+  if reg == "" then
+    return ""
+  end
+  return "recording @" .. reg
+end
+
+-- TODO: grapple module
+
+local lineinfo = function()
+  return " %P %l:%c "
+end
+
+local vcs = function()
+  local git_info = vim.b.gitsigns_status_dict
+  if not git_info or git_info.head == "" then
+    return ""
+  end
+  local added = git_info.added and ("%#GitSignsAdd#+" .. git_info.added .. " ") or ""
+  local changed = git_info.changed and ("%#GitSignsChange#~" .. git_info.changed .. " ") or ""
+  local removed = git_info.removed and ("%#GitSignsDelete#-" .. git_info.removed .. " ") or ""
+  if git_info.added == 0 then
+    added = ""
+  end
+  if git_info.changed == 0 then
+    changed = ""
+  end
+  if git_info.removed == 0 then
+    removed = ""
+  end
+  return table.concat({
+    " ",
+    added,
+    changed,
+    removed,
+    " ",
+    "%#GitSignsAdd# ",
+    git_info.head,
+    " %#Normal#",
+  })
+end
+
+Statusline = {}
+
+Statusline.active = function()
+  return table.concat({
+    "%#Statusline#",
+    update_mode_colors(),
+    mode(),
+    "%#Normal# ",
+    filename(),
+    "%#Normal#",
+    vcs(),
+    lsp(),
+    "%=%#StatusLineExtra#",
+    macroinfo(),
+    lineinfo(),
+  })
+end
+
+function Statusline.inactive()
+  return " %F"
+end
+
+function Statusline.short()
+  return "%#StatusLineNC#   NvimTree"
+end
+
+local group = vim.api.nvim_create_augroup("Statusline", { clear = true })
+
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  group = group,
+  desc = "Activate statusline on focus",
+  callback = function()
+    vim.opt_local.statusline = "%!v:lua.Statusline.active()"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+  group = group,
+  desc = "Deactivate statusline when unfocused",
+  callback = function()
+    vim.opt_local.statusline = "%!v:lua.Statusline.inactive()"
+  end,
+})
