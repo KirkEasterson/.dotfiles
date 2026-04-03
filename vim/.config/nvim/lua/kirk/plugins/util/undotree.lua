83d10c4342e6d2da69282873f323e42d5603a582
Kirk Easterson <kirk.easterson@gmail.com>
2026-04-03 14:21:04 +0200

    Update undotree symbols

--- vim/.config/nvim/lua/kirk/plugins/util/undotree.lua
+++ vim/.config/nvim/lua/kirk/plugins/util/undotree.lua
@@ -18,8 +18,11 @@ return {
   init = function()
     vim.g.undotree_ShortIndicators = 1
     vim.g.undotree_SetFocusWhenToggle = 1
-    vim.g.undotree_TreeNodeShape = ""
-    vim.g.undotree_TreeSplitShape = "/"
+    vim.g.undotree_HighlightChangedText = 1
     vim.g.undotree_HelpLine = 0
+    vim.g.undotree_TreeNodeShape = ""
+    vim.g.undotree_TreeVertShape = "│"
+    vim.g.undotree_TreeSplitShape = "╱"
+    vim.g.undotree_TreeReturnShape = "╱"
   end,
 }
