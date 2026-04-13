70c1095e9fbed6c56dc7db868e7b8308ff5b3c82
Kirk Easterson <kirk.easterson@gmail.com>
2026-04-13 09:59:51 +0200

    Move live-preview to ftplugin for markdown

--- vim/.config/nvim/ftplugin/markdown.lua
+++ vim/.config/nvim/ftplugin/markdown.lua
@@ -1,7 +1,12 @@
 vim.pack.add({
   "https://github.com/SCJangra/table-nvim",
+  "https://github.com/brianhuster/live-preview.nvim",
 })
 
 require("table-nvim").setup({
   mappings = false,
+})
+
+require("livepreview.config").set({
+  sync_scroll = false,
 })
--- vim/.config/nvim/plugin/live-preview.lua
+++
@@ -1,5 +0,0 @@
-vim.pack.add({
-  "https://github.com/brianhuster/live-preview.nvim",
-})
-
-require("livepreview.config").set()
