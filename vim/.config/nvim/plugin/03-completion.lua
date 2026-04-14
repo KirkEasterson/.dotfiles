d606098b6bac1a27333f06941be2d51f2cda6ae0
Kirk Easterson <kirk.easterson@gmail.com>
2026-04-14 22:51:38 +0200

    Add luasnip bindings

--- vim/.config/nvim/plugin/03-completion.lua
+++ vim/.config/nvim/plugin/03-completion.lua
@@ -110,3 +110,21 @@ require("blink.cmp").setup({
     },
   },
 })
+
+require("luasnip").setup()
+
+local ls = require("luasnip")
+vim.keymap.set({ "i" }, "<C-p>", function()
+  ls.expand()
+end, { desc = "Snippet - expand" })
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  ls.jump(1)
+end, { desc = "Snippet - next " })
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  ls.jump(-1)
+end, { desc = "Snippet - previous " })
+vim.keymap.set({ "i", "s" }, "<C-e>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, { desc = "Snippet - change choice" })
