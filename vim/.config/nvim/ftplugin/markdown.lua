5eac303b24fcd95a79c7c28e0df02849329163d7
Kirk Easterson <kirk.easterson@gmail.com>
2026-04-30 08:39:28 +0200

    Move nvim ftplugins to correct place

--- vim/.config/nvim/ftplugin/markdown.lua
+++ vim/.config/nvim/ftplugin/markdown.lua
@@ -3,6 +3,11 @@ vim.pack.add({
   "https://github.com/brianhuster/live-preview.nvim",
 })
 
+vim.opt.tabstop = 2 -- number of visual spaces per tab
+vim.opt.softtabstop = 2 -- number of spaces in tab when editing
+vim.opt.shiftwidth = 2 -- number of spaces when indenting with <> keys
+vim.opt.expandtab = true -- use spaces for tabs
+
 require("table-nvim").setup({
   mappings = false,
 })
