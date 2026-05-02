ee66014fe4346bf86645a106372ff89b1fd43116
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-02 06:22:59 +0200

    Fix linting issues in sway workspace script

--- scripts/.local/scripts/sway_switch workspace.sh
+++ scripts/.local/scripts/sway_switch workspace.sh
@@ -3,10 +3,9 @@
 
 DISPLAY_CONFIG=($(swaymsg -t get_outputs | jq -r '.[]|"\(.name):\(.current_workspace)"'))
 
-for ROW in "${DISPLAY_CONFIG[@]}"
-do
+for ROW in "${DISPLAY_CONFIG[@]}"; do
 	IFS=':'
-	read -ra CONFIG <<< "${ROW}"
+	read -ra CONFIG <<<"${ROW}"
 	if [ "${CONFIG[0]}" != "null" ] && [ "${CONFIG[1]}" != "null" ]; then
 		echo "moving ${CONFIG[1]} right..."
 		swaymsg -- workspace --no-auto-back-and-forth "${CONFIG[1]}"
