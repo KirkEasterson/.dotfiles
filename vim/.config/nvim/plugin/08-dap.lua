397eec812fd5d9d253f8f7b9a106891600818410
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-06 06:27:12 +0200

    Remove bash and python debuggers from dap ensure_installed

--- vim/.config/nvim/plugin/08-dap.lua
+++ vim/.config/nvim/plugin/08-dap.lua
@@ -28,10 +28,8 @@ require("mason-nvim-dap").setup({
   automatic_installation = true,
   automatic_setup = true,
   ensure_installed = {
-    "bash",
     "delve",
     "js",
-    "python",
   },
   handlers = {
     function(config)
