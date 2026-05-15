356218c1e20ad252b4078b4098a1f6c2e9916b77
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-15 06:07:35 +0200

    Add ftplugin for json

---
+++ vim/.config/nvim/ftplugin/json.lua
@@ -0,0 +1,4 @@
+vim.opt.tabstop = 2 -- number of visual spaces per tab
+vim.opt.softtabstop = 2 -- number of spaces in tab when editing
+vim.opt.shiftwidth = 2 -- number of spaces when indenting with <> keys
+vim.opt.expandtab = true -- use spaces for tabs
