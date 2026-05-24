84074d8b0366e4d381f4b7126c7ad0e99545e598
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-24 11:59:07 +0200

    Enable LSP folding in nvim

--- vim/.config/nvim/plugin/05-lsp.lua
+++ vim/.config/nvim/plugin/05-lsp.lua
@@ -64,6 +64,19 @@ require("mason-lspconfig").setup({
   },
 })
 
+-- LSP folding
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
+}
+local language_servers = vim.lsp.get_clients()
+for _, ls in ipairs(language_servers) do
+  require("lspconfig")[ls].setup({
+    capabilities = capabilities,
+  })
+end
+
 vim.keymap.set({ "n" }, "gd", function()
   vim.lsp.buf.definition()
 end, { desc = "Go to definition" })
