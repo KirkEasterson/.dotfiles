1b302f956f8536c0ee2ad42b31794bc383927d05
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-25 12:00:06 +0200

    Add formatting autocmds for notes

--- vim/.config/nvim-notes/lua/kirk/core/autocmds.lua
+++ vim/.config/nvim-notes/lua/kirk/core/autocmds.lua
@@ -14,3 +14,50 @@ autocmd("BufwritePost", {
 		fn.jobstart("notes.sh build " .. fn.expand("%%"))
 	end,
 })
+
+local trailing_space = augroup("TrailingSpace", {})
+autocmd({ "BufWritePre" }, {
+	desc = "Remove trailing-space",
+	group = trailing_space,
+	pattern = { "*" },
+	callback = function()
+		-- 'substitute' will move the cursor, so we save the position to restore
+		-- it. this causes the cursor to flash, which isn't ideal, but it's
+		-- unavoidable and not that big of a deal.
+		local pre_pos = vim.api.nvim_win_get_cursor(0)
+		vim.cmd([[%s/\s\+$//e]])
+		vim.api.nvim_win_set_cursor(0, pre_pos)
+	end,
+})
+autocmd({ "InsertEnter" }, {
+	desc = "Remove trailing space character for insert mode",
+	group = trailing_space,
+	pattern = { "*" },
+	-- command = "set listchars-=trail:·",
+	callback = function()
+		vim.opt.listchars:remove("trail")
+	end,
+})
+autocmd({ "InsertLeave" }, {
+	desc = "Enable trailing space character for non-insert mode",
+	group = trailing_space,
+	pattern = { "*" },
+	-- command = "set listchars+=trail:·",
+	callback = function()
+		vim.opt.listchars:append({
+			trail = "·",
+		})
+	end,
+})
+
+autocmd("TextYankPost", {
+	desc = "Highlight yanked text",
+	group = augroup("highlight_yank", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 100,
+		})
+	end,
+})
