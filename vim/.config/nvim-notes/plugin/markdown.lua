89d1e2b7423c87795877f476c095e41e97c90da3
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-25 12:35:13 +0200

    Move nvim-notes plugins outside of ftplugin

---
+++ vim/.config/nvim-notes/plugin/markdown.lua
@@ -0,0 +1,10 @@
+vim.pack.add({
+  "https://github.com/yousefhadder/markdown-plus.nvim",
+  "https://github.com/brianhuster/live-preview.nvim",
+})
+
+require("markdown-plus").setup()
+
+require("livepreview.config").set({
+  sync_scroll = false,
+})
