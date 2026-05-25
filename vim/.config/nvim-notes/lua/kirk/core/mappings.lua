b3095ce0c4033a9cc97ec3c451a84dcc4b9c5b23
Kirk Easterson <kirk.easterson@gmail.com>
2026-05-25 12:34:47 +0200

    Add nvim-notes undo mappings

--- vim/.config/nvim-notes/lua/kirk/core/mappings.lua
+++ vim/.config/nvim-notes/lua/kirk/core/mappings.lua
@@ -22,3 +22,10 @@ util.map({ "v" }, "<leader>x", "\"+x", { desc = "Cut to system clipboard" })
 util.map({ "n", "v" }, "<leader>y", "\"+y", { desc = "Yank to system clipboard" })
 util.map({ "n", "v" }, "<leader>p", "\"+p", { desc = "Paste from system clipboard" })
 util.map({ "n", "v" }, "<leader>P", "\"+P", { desc = "Paste from system clipboard" })
+
+-- easier undos
+util.map("i", ",", ",<c-g>u")
+util.map("i", ".", ".<c-g>u")
+util.map("i", "[", "[<c-g>u")
+util.map("i", "!", "!<c-g>u")
+util.map("i", "?", "?<c-g>u")
