e8e747da43a031543c59b34662d016df431c4d8b
Kirk Easterson <kirk.easterson@gmail.com>
2026-08-07 02:24:23 +0200

    Move nvim grep config to a separate file

---
+++ vim/.config/nvim/lua/kirk/core/grep.lua
@@ -0,0 +1,21 @@
+if vim.fn.executable("rg") == 1 then
+  vim.opt.grepprg = "rg --vimgrep --smart-case --hidden"
+  vim.opt.grepformat = "%f:%l:%c:%m"
+end
+
+-- taken from https://www.reddit.com/r/neovim/comments/1t86svd/how_to_make_find_fast/
+local findfunc
+if vim.fn.executable("fd") == 1 then
+  findfunc = "fd"
+elseif vim.fn.executable("fdfind") == 1 then
+  findfunc = "fdfind"
+else
+  vim.opt.path:append({ "**" })
+end
+
+function UseFd(cmdarg, _)
+  local param = vim.fn.getcwd() .. ".*" .. tostring(cmdarg)
+  return vim.fn.systemlist({ findfunc, "--type", "f", "--hidden", "--exclude", ".git", "--full-path", param })
+end
+
+vim.o.findfunc = "v:lua.UseFd"
--- vim/.config/nvim/lua/kirk/core/init.lua
+++ vim/.config/nvim/lua/kirk/core/init.lua
@@ -1,4 +1,5 @@
 require("kirk.core.options")
+require("kirk.core.grep")
 require("kirk.core.mappings")
 require("kirk.core.autocmds")
 require("kirk.core.statusline")
