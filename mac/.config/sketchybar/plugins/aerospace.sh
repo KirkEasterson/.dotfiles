97b0adba7579517dc59e9cd98a994dd8f2592171
Kirk Easterson <kirk.easterson@gmail.com>
2026-08-09 01:32:32 +0200

    Hide hidden workspaces in sketchybar for aeropsace

--- mac/.config/sketchybar/plugins/aerospace.sh
+++ mac/.config/sketchybar/plugins/aerospace.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+FOCUSED_WORKSPACE=$(<~/.local/share/aerospace/focused_workspace)
+EMPTY_WORKSPACES=$(<~/.local/share/aerospace/empty_workspaces)
+
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set "$NAME" background.drawing=on
+  sketchybar --set "$NAME" background.drawing=on drawing=on
+elif [[ $EMPTY_WORKSPACES != *"$1"* ]]; then
+  sketchybar --set "$NAME" background.drawing=off drawing=on
 else
-  sketchybar --set "$NAME" background.drawing=off
+  sketchybar --set "$NAME" drawing=off
 fi
