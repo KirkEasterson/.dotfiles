4a61e016726bdf0b9c9880d4aaaf48861f752202
Kirk Easterson <kirk.easterson@gmail.com>
2026-08-17 07:20:25 +0200

    Disable nvim spell plugin for markdown files

--- vim/.config/nvim/ftplugin/markdown.lua
+++ vim/.config/nvim/ftplugin/markdown.lua
@@ -16,6 +16,7 @@ vim.opt.tabstop = 2 -- number of visual spaces per tab
 vim.opt.softtabstop = 2 -- number of spaces in tab when editing
 vim.opt.shiftwidth = 2 -- number of spaces when indenting with <> keys
 vim.opt.expandtab = true -- use spaces for tabs
+vim.opt.spell = false
 
 -- easier undos
 local util = require("util")
