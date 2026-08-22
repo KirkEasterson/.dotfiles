33b02181c0df9f1c372b852141279ae5208c353a
Kirk Easterson <kirk.easterson@gmail.com>
2026-08-22 09:29:37 +0200

    Formatting for lock.sh

--- scripts/.local/scripts/lock.sh
+++ scripts/.local/scripts/lock.sh
@@ -10,17 +10,17 @@ if [ -n "$DISPLAY" ] && [ -z "$WAYLAND_DISPLAY" ]; then
 	magick "$LOCK_IMG" \
 		-gravity Center \
 		-background "$BGCOLOR" \
-		-resize "$SCREEN_RESOLUTION"! RGB:- \
-	| i3lock \
-		--ignore-empty-password \
-		--nofork \
-		--beep \
-		--raw "$SCREEN_RESOLUTION":rgb \
-		--image="/dev/stdin" \
-		--color="#000000" \
-		--tiling \
-		--show-failed-attempts \
-		--show-keyboard-layout
+		-resize "$SCREEN_RESOLUTION"! RGB:- |
+		i3lock \
+			--ignore-empty-password \
+			--nofork \
+			--beep \
+			--raw "$SCREEN_RESOLUTION":rgb \
+			--image="/dev/stdin" \
+			--color="#000000" \
+			--tiling \
+			--show-failed-attempts \
+			--show-keyboard-layout
 else
 	swaylock -f
 fi
