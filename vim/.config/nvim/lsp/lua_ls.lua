a6a78c87925944c285016f4679c10dd26ce7ff71
Kirk Easterson <kirk.easterson@gmail.com>
2026-09-06 09:56:24 +0200

    Add global vim diagnostics for lua_ls in nvim

--- vim/.config/nvim/lsp/lua_ls.lua
+++ vim/.config/nvim/lsp/lua_ls.lua
@@ -5,7 +5,13 @@ return {
       runtime = {
         version = "LuaJIT",
       },
+      diagnostics = {
+        globals = { "vim" },
+      },
       workspace = {
+        -- NOTE: This uses a lot of CPU since it indexes _every_ file. Use
+        -- _lazydev_ instead.
+        -- library = vim.api.nvim_get_runtime_file("", true),
         checkThirdParty = false,
       },
       telemetry = {
